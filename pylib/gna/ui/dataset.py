3a6701b47795f718c15d30e4ffec638f6841de12
Dmitriy Taychenachev <d@l2open.net>
2016-02-25 11:42:29 +0300

    update

---
+++ pylib/gna/ui/dataset.py
@@ -0,0 +1,26 @@
+from gna.ui import basecmd, append_typed, at_least
+import ROOT
+import numpy as np
+from gna.env import env
+from itertools import chain
+from gna.dataset import Dataset
+
+def getobservable(path):
+    nspath, name = path.split('/')
+    return env.ns(nspath).observables[name]
+
+class cmd(basecmd):
+    @classmethod
+    def initparser(cls, parser, env):
+        parser.add_argument('--name', required=True)
+        parser.add_argument('--asimov-data', nargs=2, action='append',
+                            metavar=('THEORY', 'DATA'),
+                            default=[])
+
+    def run(self):
+        dataset = Dataset(name=self.opts.name, desc=None)
+        for theory_path, data_path in self.opts.asimov_data:
+            dataset.assign(getobservable(theory_path),
+                           getobservable(data_path),
+                           getobservable(data_path))
+        self.env.parts.dataset[self.opts.name] = dataset
