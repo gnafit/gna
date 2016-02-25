17e5c87dd0b840872dfdbbe1e9d98014efacbdfe
Dmitriy Taychenachev <d@l2open.net>
2016-02-25 11:41:22 +0300

    update

---
+++ pylib/gna/ui/juno.py
@@ -0,0 +1,61 @@
+from gna.ui import basecmd
+
+from gna.exp.reactor import ReactorExperimentModel, Reactor, Detector
+import numpy as np
+import ROOT
+
+class juno(ReactorExperimentModel):
+    name = 'juno'
+
+    def makereactors(self):
+        reactors_opts = [
+            {'name': 'YJ1', 'location': 52.75, 'power': 2.9},
+            {'name': 'YJ2', 'location': 52.84, 'power': 2.9},
+            {'name': 'YJ3', 'location': 52.42, 'power': 2.9},
+            {'name': 'YJ4', 'location': 52.51, 'power': 2.9},
+            {'name': 'YJ5', 'location': 52.12, 'power': 2.9},
+            {'name': 'YJ6', 'location': 52.21, 'power': 2.9},
+
+            {'name': 'TS1', 'location': 52.76, 'power': 4.6},
+            {'name': 'TS2', 'location': 52.63, 'power': 4.6},
+            {'name': 'TS3', 'location': 52.32, 'power': 4.6},
+            {'name': 'TS4', 'location': 52.20, 'power': 4.6},
+
+            {'name': 'DYB', 'location': 215.0, 'power': 17.4},
+            {'name': 'HZ', 'location': 265.0, 'power': 17.4},
+        ]
+
+        reactors = []
+        for entry in reactors_opts:
+            entry.update({
+                'power_rate': [1.0],
+                'fission_fractions': {
+                    'Pu239': [0.60],
+                    'Pu241': [0.07],
+                    'U235': [0.27],
+                    'U238': [0.06],
+                }
+            })
+            reactors.append(Reactor(self.ns, **entry))
+
+        return reactors
+
+    def makedetectors(self):
+        detector = Detector(
+            self.ns,
+            name='AD1',
+            edges=np.linspace(1., 8., 100+1),
+            location=0,
+            protons=1.42e33,
+            livetime=[0.8*5*365*24*60*60.0]
+        )
+        return [detector]
+
+class cmd(basecmd):
+    @classmethod
+    def initparser(cls, parser, env):
+        super(cmd, cls).initparser(parser, env)
+        juno.initparser(parser, env)
+
+    def init(self):
+        juno(self.opts)
