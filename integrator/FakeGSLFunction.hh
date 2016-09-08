c40224dcddc2b9847170267684ec6179a2166cbf
Konstantin Treskov <morisrand@gmail.com>
2016-09-08 15:56:54 +0300

    Some changes in example

--- integrator/FakeGSLFunction.hh
+++ integrator/FakeGSLFunction.hh
@@ -9,24 +9,25 @@
  * http://stackoverflow.com/questions/13074756/how-to-avoid-static-member-function-when-using-gsl-with-c/18181494#18181494 */
 
 template< typename F >
-  class gsl_function_pp : public gsl_function {
+  class gsl_function_wrapper : public gsl_function {
   public:
-  gsl_function_pp(const F& func) : _func(func) {
-    function = &gsl_function_pp::invoke;
+  gsl_function_wrapper(const F& func) : _func(func) {
+    function = &gsl_function_wrapper::invoke;
     params=this;
   }
   private:
   const F& _func;
   static double invoke(double x, void *params) {
-    return static_cast<gsl_function_pp*>(params)->_func(x);
+    return static_cast<gsl_function_wrapper*>(params)->_func(x);
   }
 };
 
 /* Usage case :
  *  Class* ptr2 = this;
- *  auto ptr = [=](double x)->double{return ptr2->foo(x);};
- *  gsl_function_pp<decltype(ptr)> Fp(ptr);     
- *  gsl_function *F = static_cast<gsl_function*>(&Fp);   */
+ *  C++11: auto ptr = [=](double x)->double{return ptr2->foo(x);};
+ *  C++14: decltype(auto) ptr = [=](double x){return ptr2->foo(x);};
+ *  gsl_function_wrapper<decltype(ptr)> Fp(ptr);     
+ *  gsl_function F = static_cast<gsl_function>(Fp);   */
 /* Note that declaring every member function as static should also work as well, since static function location in memory is known even before instantiation of object of class */
 
 
