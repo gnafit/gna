457627e28ed799185392c7cadff68e7e1d8079d2
Konstantin Treskov <treskov@jinr.ru>
2017-04-25 18:40:29 +0300

    First variant for allowing minimizer to fetch all parameters from the given namespace. Perhaps some refactoring to be done later.

--- pylib/gna/ui/minimizer.py
+++ pylib/gna/ui/minimizer.py
@@ -1,3 +1,4 @@
+import ROOT
 from gna.ui import basecmd, set_typed
 from gna.minimizers import minimizers
 from gna.minimizers import spec
@@ -10,11 +11,29 @@ class cmd(basecmd):
         parser.add_argument('type', choices=minimizers.keys())
         parser.add_argument('statistic', action=set_typed(env.parts.statistic))
         parser.add_argument('par', nargs='*')
+        parser.add_argument('--par_namespace', nargs='*')
 
     def init(self):
         minimizer = minimizers[self.opts.type](self.opts.statistic)
 
-        minimizer.addpars([self.env.pars[pname] for pname in self.opts.par])
+        parameters = [self.env.pars[pname] for pname in self.opts.par]
+
+        # iterate over namespaces passed and extract parameters for minimization
+        # TODO: Refactor to handle nested namespaces
+        par_from_ns = []
+        for par_ns in self.opts.par_namespace:
+            for key in self.env.ns(par_ns).iterkeys():
+                par_name = par_ns + '.' + key
+                param = self.env.pars[par_name]
+                if isinstance(param, ROOT.GaussianParameter("double")):
+                    par_from_ns.append(param)
+
+        parameters.extend(par for par in par_from_ns if par not in parameters)
+        for _ in parameters:
+            print _.name()
+
+
+        minimizer.addpars(parameters)
 
         if self.opts.spec is not None:
             minimizer.spec = spec.parse(self.env, minimizer, self.opts.spec)
