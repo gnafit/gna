5184cbf8909e3b1a4c6574c5d4292abb06f66d9e
Dmitry V.Naumov <dmitryvnaumov@gmail.com>
2017-05-06 08:01:47 +0300

    adding an example of decoherence study with JUNO

---
+++ examples/juno_decoh.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+python gna -- ns --define common.Eres_b central=0.03 sigma=0.0 --push common \
+-- juno --name juno_stand --oscprob standard -- ns --value juno_stand.oscillation.Alpha normal \
+-- juno --name juno_decoh --oscprob decoh    -- ns --value juno_decoh.oscillation.Alpha normal \
+-- dataset --name asimov_stand --asimov-data juno_decoh/AD1 juno_stand/AD1 \
+-- analysis --name testdecoh -d asimov_stand -o juno_decoh/AD1 \
+-- chi2 chi2 testdecoh -- minimizer min minuit chi2 juno_decoh.oscillation.DeltaMSqEE juno_decoh.oscillation.SinSq12 juno_decoh.oscillation.SinSq13 \
+-- scan --lingrid juno_decoh.oscillation.SigmaDecohRel 0.001 0.01 20 --loggrid juno_decoh.oscillation.SigmaDecohRel 5e-17 1e-15 10  --minimizer min --verbose --output /tmp/out.hdf5 \
+-- contour --chi2 /tmp/out.hdf5 --minimizer min --plot chi2minplt --show
+#-- scan --lingrid juno_decoh.oscillation.SigmaDecohRel 0.001 0.1 10 --output /tmp/out.hdf5 
+#--minimizer min \
+#--output /tmp/out.hdf5 \
+#-- contour --chi2 /tmp/out.hdf5  --minimizer min 
+#--plot chi2minplt   --show
+#-- scan --lingrid juno_decoh.oscillation.SigmaDecohRel 0.001 0.01 20 --loggrid juno_decoh.oscillation.SigmaDecohRel 1e-17 1e-15 10  --minimizer min --verbose --output /tmp/out.hdf5 \
