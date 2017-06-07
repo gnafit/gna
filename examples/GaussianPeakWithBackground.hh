5f932fb543884274a69156c6a3df0a226e1810d6
Dmitry V.Naumov <dmitryvnaumov@gmail.com>
2017-06-07 15:03:14 +0300

    add possibility to use normalized gaussian with sigma-> sqrt{n}*sigma and mean -> n* sigma.

--- examples/GaussianPeakWithBackground.hh
+++ examples/GaussianPeakWithBackground.hh
@@ -5,15 +5,20 @@
 #include <cmath>
 
 #include "GNAObject.hh"
+#include "ParametricLazy.hpp"
 
 class GaussianPeakWithBackground: public GNAObject,
                                   public Transformation<GaussianPeakWithBackground> {
 public:
-  GaussianPeakWithBackground() {
+  GaussianPeakWithBackground(double n=1) {
     variable_(&m_b, "BackgroundRate");
     variable_(&m_mu, "Mu");
     variable_(&m_E0, "E0");
     variable_(&m_w, "Width");
+    using namespace ParametricLazyOps;
+
+    m_w_scaled = mkdep(m_w*std::sqrt( n )); 
+    m_E0_scaled = mkdep(m_E0*n);
 
     transformation_(this, "rate")
       .input("E")
@@ -26,10 +31,11 @@ public:
   void calcRate(Args args, Rets rets) {
     const double pi = boost::math::constants::pi<double>();
     const auto &E = args[0].arr;
-    rets[0].arr = m_b + m_mu*(1./std::sqrt(2*pi*m_w))*(-(E-m_E0).square()/(2*m_w*m_w)).exp();
+    rets[0].arr = m_b + m_mu*(1./std::sqrt(2*pi*m_w_scaled))*(-(E-m_E0_scaled).square()/(2*m_w_scaled*m_w_scaled)).exp();
   }
 protected:
   variable<double> m_b, m_mu, m_E0, m_w;
+  dependant<double> m_w_scaled, m_E0_scaled;
 };
 
 #endif // GAUSSIANPEAKWITHBACKGROUND_H
---
+++ pylib/gna/ui/PmtCharge.py
@@ -0,0 +1,53 @@
+from gna.ui import basecmd
+from gna.env import env
+import ROOT
+import numpy as np
+import scipy.misc
+from scipy.stats import poisson
+
+class cmd(basecmd):
+    @classmethod
+    def initparser(cls, parser, env):
+        parser.add_argument('--name', required=True)
+        parser.add_argument('--Emin', default=0, type=float)
+        parser.add_argument('--Emax', default=5, type=float)
+        parser.add_argument('--nbins', default=70, type=int)
+        parser.add_argument('--order', default=8)
+        parser.add_argument('--PoissonMean', default=0.2, type=float)
+        parser.add_argument('--PoissonOrder', default=4, type=int)
+
+    def init(self):
+        ns = env.ns(self.opts.name)
+        ns.reqparameter('BackgroundRate', central=0, sigma=0.1)
+        ns.reqparameter('Mu', central=1, sigma=1)
+        ns.reqparameter('E0', central=2, sigma=0.05)
+        ns.reqparameter('Width', central=0.2, sigma=0.005)
+
+        edges = np.linspace(self.opts.Emin, self.opts.Emax, self.opts.nbins+1)
+        orders = np.array([self.opts.order]*(len(edges)-1), dtype=int)
+        integrator = ROOT.GaussLegendre(edges, orders, len(orders))
+        hist = ROOT.GaussLegendreHist(integrator)
+        signal = ROOT.Sum()
+
+        n = self.opts.PoissonOrder
+        model = {}
+        #ff = np.arange(1,n+1)
+        #ff = 1/scipy.misc.factorial(ff)*np.exp(-self.opts.PoissionMean)
+        #ff_points = ROOT.Points(ff)
+        #print(ff, ff_points)
+        with ns:
+            for i in xrange(1,n+1):
+                print(i,n)
+                model[i] =  ROOT.GaussianPeakWithBackground(i) 
+                model[i].rate.E(integrator.points.x)
+         #       print(model[i].rate,model[i].rate.rate,ff_points[i])
+                prod = ROOT.Product()
+                prod.multiply(model[i].rate.rate)
+                poisson_factor = poisson.pmf(i, self.opts.PoissonMean)
+                poisson_factor_prod = ROOT.Points([poisson_factor])
+                print(type(model[i].rate), poisson_factor,poisson_factor_prod)
+                prod.multiply(poisson_factor_prod)
+                signal.add(prod)
+        hist.hist.f(signal)
+        ns.addobservable('spectrum', hist.hist)
+
