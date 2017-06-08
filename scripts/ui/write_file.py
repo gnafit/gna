3182b1d26a3f010acf9a63097a0f025d70a7be7f
Konstantin Treskov <treskov@jinr.ru>
2017-06-08 18:40:15 +0300

    Update

--- scripts/ui/write_file.py
+++ scripts/ui/write_file.py
@@ -1,11 +1,20 @@
 import numpy as np
 from gna.dataset import Dataset
 
+# 0 - which observable to take, 1 - file to write to, 2 - do poisson fluctuations
+# or no
+
 pred = self.env.get(self.opts.args[0])
-poisson_data = np.random.poisson(pred.data())
-print 'Fluctuated data', poisson_data
+data = None 
+if len(self.opts.args) > 2:
+    data = np.random.poisson(pred.data())
+    print 'Fluctuated data', data
+else:
+    data = pred.data()
+    print 'Data with no fluctuation {}'.format(data)
+
 print 'Write data to', self.opts.args[1]
-np.savez(self.opts.args[1], data=poisson_data)
+np.savez(self.opts.args[1], data=data)
 
 #  print self.opts.args
 #  with np.load('/tmp/ex.npz') as ex_file:
