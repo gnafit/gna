725983c8fd835ec8a638e8c2c08ea606f3a5b5d5
Konstantin Treskov <treskov@jinr.ru>
2017-06-15 20:28:42 +0300

    Now it works

--- neutrino/OscillationVariables.hh
+++ neutrino/OscillationVariables.hh
@@ -74,6 +74,9 @@ protected:
       .add(&DeltaMSqMM, {&DeltaMSqEE, &Alpha, &Theta12, &Delta, &DeltaMSq12, &Theta23, &Theta13}, [&](){
               return DeltaMSqEE - Alpha*sin(2*Theta12) 
                      + cos(Delta)*sin(Theta13)*sin(2*Theta12)*tan(Theta23)*DeltaMSq12;})
+      .add(&DeltaMSqEE, {&DeltaMSqMM, &Alpha, &Theta12, &Delta, &DeltaMSq12, &Theta23, &Theta13}, [&](){
+              return DeltaMSqMM + Alpha*sin(2*Theta12) 
+                     - cos(Delta)*sin(Theta13)*sin(2*Theta12)*tan(Theta23)*DeltaMSq12;})
       .add(&Theta12, {&SinSq12}, [&]() { return asin(sqrt(SinSq12)); })
       .add(&SinSq12, {&Theta12}, [&]() { return pow(sin(Theta12), 2); })
       .add(&Theta13, {&SinSq13}, [&]() { return asin(sqrt(SinSq13)); })
