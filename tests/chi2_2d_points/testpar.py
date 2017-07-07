c531b991bb1a848bcd0d81cd28a931dde8e39b44
Anna Fatkina <annacplus@yandex.ru>
2017-07-07 17:27:39 +0300

    Added args for linspace

--- tests/chi2_2d_points/testpar.py
+++ tests/chi2_2d_points/testpar.py
@@ -8,8 +8,7 @@ import itertools as I
 from matplotlib import pyplot as plt
 R.GNAObject
 
-def plotl( X, Y, Z, **kwargs ):
-    print(X.shape)
+def plotl( step, X, Y, Z, **kwargs ):
     levels = kwargs.pop( 'levels', [] )
     masks  = kwargs.pop( 'masks', [] )
     assert len(levels)==len(masks)
@@ -25,6 +24,8 @@ def plotl( X, Y, Z, **kwargs ):
     ax.contour( X, Y, Z, levels )
 
     markers = 'ov^s'
+    X=X[::step, ::step]
+    Y=Y[::step, ::step]
     for level, mask, marker in zip(levels, masks, I.cycle( markers ) ):
         ax.plot( X[mask], Y[mask], marker, label='L=%g'%level )
 
@@ -45,54 +46,32 @@ def points_to_mask( points, xsize, ysize ):
 def mask_to_points( mask ):
     return N.transpose(N.nonzero(mask))
 
-def invert_values( x ):
-    return True if x==False else False
-
-invert_ = N.vectorize(invert_values)
-
 def main( opts ):
-    xsize = 501;
-    ysize = 501;
-    ppp = N.matrix([[1, 3, 5], [2, 5, 2]])
-    print(mask_to_points(points_to_mask(ppp, 6, 6)))
-    x = N.linspace( -0., 6., xsize, dtype='d' )
-    y = N.linspace( -2., 4., ysize, dtype='d' )
+#TODO: make as module
+    print(opts.xlinspace)
+    xsize = int(opts.xlinspace[2])
+    ysize = int(opts.ylinspace[2])
+    x = N.linspace( opts.xlinspace[0], opts.xlinspace[1], xsize, dtype='d' )
+    y = N.linspace( opts.ylinspace[0], opts.ylinspace[1], ysize, dtype='d' )
     X, Y = N.meshgrid( x, y, indexing='ij' )
     Z = X**2 + Y**2
     Z1 = 2.0*(X-3.0)*(X-3.0) + 5.0*(Y-1.0)*(Y-1.0) + 3.0*(X-3.0)*(Y-1.0)
     Z2 = 0.05*X*X*X + 0.1*Y*Y*Y + Z1
-    x = N.zeros((10, 10), 'bool')
-    x[::3, ::3] = 1
-    x=invert_(x)
-    print(x)
-    p = R.Paraboloid(Z1.shape[0], Z1.shape[1], numpy_to_eigen( Z1 ), 6. / xsize, 6. / ysize, 1, 1., 0.1 )
+    p = R.Paraboloid(Z1.shape[0], Z1.shape[1], numpy_to_eigen( Z1 ), ( opts.xlinspace[1] - opts.xlinspace[0]) / xsize, (opts.ylinspace[1] - opts.ylinspace[0]) / ysize, 1, 1., 0.1 )
     masks = []
-    #invert_values = N.vectorize(invert_values)
     for level in opts.levels:
 	mat = R.Eigen.MatrixXd( xsize, ysize )
         p.GetCrossSectionExtendedAutoDev( mat, level )
         csc = eigen_to_numpy( mat, 'bool' ).copy()
-        #map(invert_values, csc)
-        print(csc)
-        print(csc.shape)
-        csc1 = invert_( csc )
-        print (csc1)
-        print(csc1.shape)
-        xslice = N.arange(0, xsize, 1)
-        xslice = N.delete(xslice, N.arange(0, xsize, 100))
-        #print(xslice)
-        yslice = N.arange(ysize)
-        #csc = csc.array()*
-        #csc[xslice, xslice] = 0
-        #csc = invert_values(csc)
-        masks.append( csc1 )
-        Zl=N.ma.array(Z, mask=~csc1)
+        csc_sparse = csc[:: opts.sparseness, :: opts.sparseness]
+        masks.append( csc_sparse )
+        Zl=N.ma.array(Z1, mask=~csc)
         Zvalues = Zl.compressed()
         if Zvalues.shape[0] != 0:
             print( 'Level %f (min/max): '%level, Zvalues.min(), Zvalues.max() )
 
-    print(X.shape)
-    plotl( X, Y, Z1, levels=opts.levels, masks=masks )
+    plotl( opts.sparseness, X, Y, Z1, levels=opts.levels, masks=masks )
+    
     plt.contour( X, Y, Z2, levels=opts.levels)
 
     if opts.output:
@@ -108,5 +87,8 @@ if __name__ == "__main__":
     parser.add_argument( '-s', '--show', action='store_true', help='show figures' )
     parser.add_argument( '-l', '--levels', nargs='+', default=[1.0, 4.0, 9.0], type=float, help='levels to process')
     parser.add_argument( '-o', '--output', help='figure output' )
-    parser.add_argument( '-sp', '--sparseness', nargs=1, default = 1, type=int, help='points sparseness')
+    parser.add_argument( '-sp', '--sparseness', type=int, default=1, help='points sparseness')
+    parser.add_argument( '--xlinspace', nargs=3, default=[0.0, 10.0, 501], type=float, help='parameters: BEGIN END NUM_OF_STEPS')
+    parser.add_argument( '--ylinspace', nargs=3, default=[0.0, 10.0, 501], type=float, help='parameters: BEGIN END NUM_OF_STEPS')
     main( parser.parse_args() )
+#TODO: add arguments for input in Paraboloid
