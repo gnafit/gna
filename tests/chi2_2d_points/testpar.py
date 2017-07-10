b52017a75bedb28ac690a2a5e494792930a35804
Anna Fatkina <annacplus@yandex.ru>
2017-07-10 11:11:31 +0300

    Image maker and new args from command line

--- tests/chi2_2d_points/testpar.py
+++ tests/chi2_2d_points/testpar.py
@@ -8,7 +8,7 @@ import itertools as I
 from matplotlib import pyplot as plt
 R.GNAObject
 
-def plotl( step, X, Y, Z, **kwargs ):
+def plotl( title, step, X, Y, Z, **kwargs ):
     levels = kwargs.pop( 'levels', [] )
     masks  = kwargs.pop( 'masks', [] )
     assert len(levels)==len(masks)
@@ -19,7 +19,7 @@ def plotl( step, X, Y, Z, **kwargs ):
     ax.grid()
     ax.set_xlabel( kwargs.pop( 'xlabel', 'axis x' ) )
     ax.set_ylabel( kwargs.pop('ylabel', 'axis y') )
-    ax.set_title( kwargs.pop('title', 'Paraboloid') )
+    ax.set_title( kwargs.pop('title', 'Paraboloid\n' + title) )
 
     ax.contour( X, Y, Z, levels )
 
@@ -30,6 +30,7 @@ def plotl( step, X, Y, Z, **kwargs ):
         ax.plot( X[mask], Y[mask], marker, label='L=%g'%level )
 
     ax.legend( loc='upper right' )
+    
 
 def eigen_to_numpy( eigen, dtype ):
     return N.frombuffer( eigen.data(), dtype='d', count=eigen.size() ).reshape( eigen.rows(), eigen.cols(), order='F' ).astype(dtype)
@@ -48,7 +49,6 @@ def mask_to_points( mask ):
 
 def main( opts ):
 #TODO: make as module
-    print(opts.xlinspace)
     xsize = int(opts.xlinspace[2])
     ysize = int(opts.ylinspace[2])
     x = N.linspace( opts.xlinspace[0], opts.xlinspace[1], xsize, dtype='d' )
@@ -57,8 +57,9 @@ def main( opts ):
     Z = X**2 + Y**2
     Z1 = 2.0*(X-3.0)*(X-3.0) + 5.0*(Y-1.0)*(Y-1.0) + 3.0*(X-3.0)*(Y-1.0)
     Z2 = 0.05*X*X*X + 0.1*Y*Y*Y + Z1
-    p = R.Paraboloid(Z1.shape[0], Z1.shape[1], numpy_to_eigen( Z1 ), ( opts.xlinspace[1] - opts.xlinspace[0]) / xsize, (opts.ylinspace[1] - opts.ylinspace[0]) / ysize, 1, 1., 0.1 )
+    p = R.Paraboloid(Z1.shape[0], Z1.shape[1], numpy_to_eigen( Z1 ), ( opts.xlinspace[1] - opts.xlinspace[0]) / xsize, (opts.ylinspace[1] - opts.ylinspace[0]) / ysize, opts.deviation, opts.gradinfluence, opts.tolerance )
     masks = []
+    title='deviation = ' + str(opts.deviation) + ', gradient influence = ' + str(opts.gradinfluence) + ',\ntolerance = ' + str(opts.tolerance) + ', points sparseness = ' + str(opts.sparseness) 
     for level in opts.levels:
 	mat = R.Eigen.MatrixXd( xsize, ysize )
         p.GetCrossSectionExtendedAutoDev( mat, level )
@@ -70,7 +71,7 @@ def main( opts ):
         if Zvalues.shape[0] != 0:
             print( 'Level %f (min/max): '%level, Zvalues.min(), Zvalues.max() )
 
-    plotl( opts.sparseness, X, Y, Z1, levels=opts.levels, masks=masks )
+    plotl( title, opts.sparseness, X, Y, Z1, levels=opts.levels, masks=masks )
     
     plt.contour( X, Y, Z2, levels=opts.levels)
 
@@ -88,7 +89,11 @@ if __name__ == "__main__":
     parser.add_argument( '-l', '--levels', nargs='+', default=[1.0, 4.0, 9.0], type=float, help='levels to process')
     parser.add_argument( '-o', '--output', help='figure output' )
     parser.add_argument( '-sp', '--sparseness', type=int, default=1, help='points sparseness')
+    parser.add_argument( '-tol', '--tolerance', type=float, default=0.05)
+    parser.add_argument( '-dev', '--deviation', type=int, default=1, help='deviation multiplier')
+    parser.add_argument( '-ginf', '--gradinfluence', type=float, default=1., help='multiplier for gradient')
     parser.add_argument( '--xlinspace', nargs=3, default=[0.0, 10.0, 501], type=float, help='parameters: BEGIN END NUM_OF_STEPS')
     parser.add_argument( '--ylinspace', nargs=3, default=[0.0, 10.0, 501], type=float, help='parameters: BEGIN END NUM_OF_STEPS')
+
     main( parser.parse_args() )
 #TODO: add arguments for input in Paraboloid
