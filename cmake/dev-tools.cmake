bf77b611c16b1d0a31c392f97ded43358b338c00
Maxim Gonchar <gmaxfl@gmail.com>
2017-07-13 14:07:42 +0300

    Fix tabs

--- cmake/dev-tools.cmake
+++ cmake/dev-tools.cmake
@@ -1,9 +1,3 @@
-if(DEBUG)
-    message(STATUS "Setting debug mode")
-    set(CMAKE_BUILD_TYPE Debug)
-    add_definitions(-DDEBUG_GRIDFILTER)
-endif()
-
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --std=c++14 -Wno-deprecated-declarations -Werror -Wall  -pedantic")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -pipe")
 
@@ -21,3 +15,10 @@ if(PARAM_DEBUG)
     message(STATUS "Enabling debug printing for parameters")
     add_definitions(-DDEBUG_PARAMETERS)
 endif()
+
+if(GRIDFILTER_DEBUG)
+    message(STATUS "Setting debug mode")
+    set(CMAKE_BUILD_TYPE Debug)
+    add_definitions(-DDEBUG_GRIDFILTER)
+endif()
+
