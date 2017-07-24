9f6718cb69f862a2d20f095027b5d19be3f43dd2
Anna Fatkina <annacplus@yandex.ru>
2017-07-24 11:30:49 +0300

    Revert "tests minor"
    This reverts commit 8897bc272650f825dfd336bf6b62ccbd52353a81.

--- tests/chi2_2d_points/paraboloid_test.sh
+++ tests/chi2_2d_points/paraboloid_test.sh
@@ -15,25 +15,52 @@ OUTPUT=output/chi2_2d_points
 test -d $OUTPUT && rm -r $OUTPUT
 mkdir -p $OUTPUT
 
+#sparseness
+: './tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 1  -tol 0.1 -dev 1 -ginf 1. -o $OUTPUT/sp1tol01dev1ginf1.pdf
 
-./tests/chi2_2d_points/testpar_irr.py -irr --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 1  -tol 0.1 -dev 1 -ginf 1 -o $OUTPUT/sp10tol01dev1ginf1_d.pdf
+./tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 5  -tol 0.1 -dev 1 -ginf 1. -o $OUTPUT/sp5tol01dev1ging1.pdf
 
-./tests/chi2_2d_points/testpar_irr.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 1  -tol 0.1 -dev 1 -ginf 1 -o $OUTPUT/sp10tol01dev1ginf1.pdf
+./tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 10  -tol 0.1 -dev 1 -ginf 1. -o $OUTPUT/sp10tol01dev1ginf1.pdf
 
-./tests/chi2_2d_points/testpar_irr.py -irr --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 1  -tol 0.1 -dev 1 -ginf 2 -o $OUTPUT/sp10tol01dev1ginf2_d.pdf
+pdfunite $OUTPUT/sp5tol01dev1ging1.pdf $OUTPUT/sp1tol01dev1ginf1.pdf $OUTPUT/sp10tol01dev1ginf1.pdf $OUTPUT/dif_sp.pdf
+'
+#tolerance
+: './tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 2  -tol 0.0 -dev 1 -ginf 1. -o $OUTPUT/sp2tol0dev1ginf1.pdf
 
-./tests/chi2_2d_points/testpar_irr.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 1  -tol 0.1 -dev 1 -ginf 2 -o $OUTPUT/sp10tol01dev1ginf2.pdf
+./tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 2  -tol 0.2 -dev 1 -ginf 1. -o $OUTPUT/sp2tol20dev1ginf1.pdf
 
-./tests/chi2_2d_points/testpar_irr.py -irr --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 1  -tol 0.1 -dev 2 -ginf 0.5 -o $OUTPUT/sp10tol01dev2ginf1_d.pdf
+./tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 2  -tol 0.5 -dev 1 -ginf 1. -o $OUTPUT/sp2tol05dev1ginf1.pdf
 
-./tests/chi2_2d_points/testpar_irr.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 1  -tol 0.1 -dev 2 -ginf 0.5 -o $OUTPUT/sp10tol01dev2ginf1.pdf
+./tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 2  -tol 1.0 -dev 1 -ginf 1. -o $OUTPUT/sp2tol1dev1ginf1.pdf
 
+./tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 2  -tol 2.0 -dev 1 -ginf 1. -o $OUTPUT/sp2tol2dev1ginf1.pdf
 
+pdfunite $OUTPUT/sp2tol0dev1ginf1.pdf $OUTPUT/sp2tol20dev1ginf1.pdf $OUTPUT/sp2tol05dev1ginf1.pdf $OUTPUT/sp2tol1dev1ginf1.pdf $OUTPUT/sp2tol2dev1ginf1.pdf $OUTPUT/dif_tol.pdf
+'
+#deviation
+: './tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 2  -tol 0.1 -dev 1 -ginf 1. -o $OUTPUT/sp2tol01dev1ginf1.pdf
+
+./tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 2  -tol 0.1 -dev 2 -ginf 1. -o $OUTPUT/sp2tol01dev2ginf1.pdf
+
+./tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 2  -tol 0.1 -dev 5 -ginf 1. -o $OUTPUT/sp2tol01dev5ginf1.pdf
+
+pdfunite $OUTPUT/sp2tol01dev1ginf1.pdf $OUTPUT/sp2tol01dev2ginf1.pdf $OUTPUT/sp2tol01dev5ginf1.pdf $OUTPUT/dif_dev.pdf
+'
+#gradient influence
+./tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 10  -tol 0.1 -dev 1 -ginf 0.5 -o $OUTPUT/sp10tol01dev1ginf05.pdf
+
+./tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 10  -tol 0.1 -dev 1 -ginf 1. -o $OUTPUT/sp10tol01dev1ginf1.pdf
+
+./tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 10  -tol 0.1 -dev 1 -ginf 1.5 -o $OUTPUT/sp10tol01dev1ginf15.pdf
+
+./tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 10  -tol 0.1 -dev 1 -ginf 2. -o $OUTPUT/sp10tol01dev1ginf2.pdf
+
+./tests/chi2_2d_points/testpar.py --xlinspace -2.0 6 501 --ylinspace -4.0 6 501 -sp 10  -tol 0.1 -dev 1 -ginf 3. -o $OUTPUT/sp10tol01dev1ginf3.pdf
 
 if which pdfunite >/dev/null
 then
     # test that 'pdfunite' executable exists in the system
-    pdfunite $OUTPUT/sp10tol01dev1ginf1_d.pdf $OUTPUT/sp10tol01dev1ginf1.pdf $OUTPUT/sp10tol01dev1ginf2_d.pdf $OUTPUT/sp10tol01dev1ginf2.pdf $OUTPUT/sp10tol01dev2ginf1_d.pdf  $OUTPUT/sp10tol01dev2ginf1.pdf $OUTPUT/dif.pdf
-    echo 'Output figure: ' $OUTPUT/dif.pdf
+    pdfunite $OUTPUT/sp10tol01dev1ginf05.pdf $OUTPUT/sp10tol01dev1ginf1.pdf $OUTPUT/sp10tol01dev1ginf15.pdf $OUTPUT/sp10tol01dev1ginf2.pdf $OUTPUT/sp10tol01dev1ginf3.pdf $OUTPUT/dif_ginf.pdf
+    echo 'Output figure: ' $OUTPUT/dif_ginf.pdf
 fi
 
