c46e80ff46e230692a073bc8aed2f529b8534cca
Anna Fatkina <annacplus@yandex.ru>
2017-08-29 12:57:19 +0300

    Safety commit

--- cuda/GNAcuMath.cu
+++ cuda/GNAcuMath.cu
@@ -19,7 +19,115 @@ __global__ void GenIdentity (int n, double * mat) {
   mat[x + n * y] = (x == y) ? 1.0 : 0.0; 
 }
 
+
+/**
+* cuBLAS multiplier wrapper for GNA 
+*/
+void cuMultiplyMat(int m, int n, int k, double* InA, double* InB, double* OutC) {
+  cudaSetDevice(0);
+  cublasHandle_t handle;
+  cublasStatus_t ret;
+  cudaError_t err;
+
+  cudaStream_t stream1, stream2;
+  cudaStreamCreate ( &stream1);
+  cudaStreamCreate ( &stream2);
+
+  ret = cublasCreate(&handle);
+  if(ret!=CUBLAS_STATUS_SUCCESS){
+    exit(EXIT_FAILURE);
+  }
+  double* devA; double* devB; double* devC;
+  cudaMalloc((void**)&devA, m * k * sizeof(double));
+  cudaMalloc((void**)&devB, k * n * sizeof(double));
+  cudaMalloc((void**)&devC, m * n * sizeof(double));
+  
+  cudaMemcpyAsync(devA, InA, m * k * sizeof(double), cudaMemcpyHostToDevice, stream1);
+  cudaMemcpyAsync(devB, InB, k * n * sizeof(double), cudaMemcpyHostToDevice, stream2);
+  cudaMemset(devC, 0, m * n * sizeof(double));
+  double alpha = 1, beta = 0;
+  cudaDeviceSynchronize();  
+  cublasDgemm(handle, 
+              CUBLAS_OP_N,  CUBLAS_OP_N,
+              m, n, k,
+              &alpha,
+              devA, m,
+              devB, k,
+              &beta,
+              devC, m );
+  
+  err = cudaMemcpyAsync(OutC, devC, m * n * sizeof(double), cudaMemcpyDeviceToHost, stream1);
+
+  if(err!=cudaSuccess) {
+    exit(EXIT_FAILURE);
+  }
+  cudaFree(devA);
+  cudaFree(devB);
+  cudaFree(devC);
+}
+
+
+/**
+* cuBLAS linear system solver wrapper for GNA. A is lower triangular.
+*/
+void cuSolveLowerLS(int m, int n, double* A, double* B) {
+printf("BEF fffff \n");
+//  cudaSetDevice(0);
+printf("AF set dev \n");
+
+  cublasHandle_t handle;
+  cublasStatus_t ret;
+  cudaError_t err;
+printf("BEF str\n");
+  cudaStream_t stream1, stream2;
+  cudaStreamCreate ( &stream1);
+  cudaStreamCreate ( &stream2);
+
+  ret = cublasCreate(&handle);
+  if(ret!=CUBLAS_STATUS_SUCCESS){
+    exit(EXIT_FAILURE);
+  }
+printf("BEF cumalloc \n");
+  double* devA;
+  double* devB;
+  cudaMalloc((void**)&devA,  m*m*sizeof(double));
+  cudaMalloc((void**)&devB,  m*n*sizeof(double));
+printf("AF cumalloc \n");
+
+  cudaMemcpyAsync(devA, A, m*m*sizeof(double), cudaMemcpyHostToDevice, stream1);
+  cudaMemcpyAsync(devB, B, m*n*sizeof(double), cudaMemcpyHostToDevice, stream2);
+  
+  double alpha = 1.0;
+/**
+  *  Solve A*x =  B.
+  */
+  ret = cublasDtrsm_v2(handle,
+          CUBLAS_SIDE_LEFT, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N, CUBLAS_DIAG_NON_UNIT,
+          m, n, &alpha, devA, m, devB, m);
+
+
+  cudaDeviceSynchronize();
+  if(ret!=CUBLAS_STATUS_SUCCESS) {
+    exit(EXIT_FAILURE);
+  }
+  
+  err = cudaMemcpyAsync(B, devB, m*n*sizeof(double), cudaMemcpyDeviceToHost, stream1);
+
+  if(err!=cudaSuccess) {
+    exit(EXIT_FAILURE);
+  }
+
+  cudaFree(devA);
+  cudaFree(devB);
+}
+
+
+/**
+* cuBLAS matrix invertor wrapper for GNA. Uses cuBLAS linear system solver.
+*/
 void cuInverseMat(int matSize, double* InMat, double* OutMat) {
+  const int blockSize = 16;
+  int copyableSize = matSize*matSize*sizeof(double);
   cudaSetDevice(0);
   cublasHandle_t handle;
   cublasStatus_t ret;
@@ -36,16 +144,15 @@ void cuInverseMat(int matSize, double* InMat, double* OutMat) {
 
   double* devInMat;
   double* devOutMat;
-  cudaMalloc((void**)&devInMat,  matSize*matSize*sizeof(double));
-  cudaMalloc((void**)&devOutMat,  matSize*matSize*sizeof(double));
+  cudaMalloc((void**)&devInMat,  copyableSize);
+  cudaMalloc((void**)&devOutMat,  copyableSize);
 
-  GenIdentity<<<dim3(matSize/32 + 1, matSize/32 + 1), dim3(32,32), 0, stream2>>>(matSize, devOutMat);
-  //cudaDeviceSynchronize();
-
-  err = cudaMemcpyAsync(devInMat, InMat, matSize*matSize*sizeof(double), cudaMemcpyHostToDevice, stream1);
+  err = cudaMemcpyAsync(devInMat, InMat, copyableSize, cudaMemcpyHostToDevice, stream1);
   if(err!=cudaSuccess) {
     exit(EXIT_FAILURE);
   }
+
+  GenIdentity<<<dim3(matSize/blockSize + 1, matSize/blockSize + 1), dim3(blockSize, blockSize), 0, stream2>>>(matSize, devOutMat);
 
   double alpha = 1.0;
 /**
@@ -54,12 +161,14 @@ void cuInverseMat(int matSize, double* InMat, double* OutMat) {
   ret = cublasDtrsm_v2(handle,
           CUBLAS_SIDE_LEFT, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N, CUBLAS_DIAG_NON_UNIT,
           matSize, matSize, &alpha, devInMat, matSize, devOutMat, matSize);
+
+
   cudaDeviceSynchronize();
   if(ret!=CUBLAS_STATUS_SUCCESS) {
     exit(EXIT_FAILURE);
   }
 
-  err = cudaMemcpyAsync(OutMat, devOutMat, matSize*matSize*sizeof(double), cudaMemcpyDeviceToHost, stream1);
+  err = cudaMemcpyAsync(OutMat, devOutMat, copyableSize, cudaMemcpyDeviceToHost, stream1);
 
   if(err!=cudaSuccess) {
     exit(EXIT_FAILURE);
@@ -70,40 +179,67 @@ void cuInverseMat(int matSize, double* InMat, double* OutMat) {
 }
 
 /*int main () {
-  int n = 200;
-  double* inM = new double[n*n];
-  double* outM = new double[n*n];
-  for (int i = 0; i < n; i++) {
+  int m = 20, n = 25;
+  double* A = new double[m*m];
+  //double* B = new double[k*n];
+std::cout << "!!!!!!!!!!!!!!!!!!!!" << std::endl;
+  double* C = new double[m*n];
+  for (int i = 0; i < m; i++) {
     for (int j = 0; j < n; j++) {
       if (i == j) {
-        inM[i + n*j] = i + j + 1;
-        outM[i + n*j] = 1;
+        C[i + m*j] = i + j + 1;
+        //C[i + n*j] = 1;
       }
       else {
-        inM[i + n*j] = 0;
-        outM[i + n*j] = 0;
+       // if (j < n) A[i + n*j] = 0;
+        C[i + m*j] = 0;
       }
     }
   }
 
-  for(int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      if (outM[i + j*n] != 0)
-      std::cout << inM[i + j*n] << " ";
+  for(int i = 0; i < m; i++) {
+    for (int j = 0; j < m; j++) {
+      if (i == j) A[i + j*m] = 1; else A[i + j*m] = 0;
+      std::cout << A[i + j*m] << " ";
     }
-  //  std::cout << std::endl;
+    std::cout << std::endl;
   }
-
-  cuInverseMat(n, inM,  outM);
-
-  for (int i = 0; i < n; i++) {
+std::cout << "BEFORE" <<std::endl;
+  cuSolveLowerLS(m, n, A, C);
+  //cuInverseMat(n, inM,  outM);
+std::cout << "AFTER" << std::endl;
+  for (int i = 0; i < m; i++) {
     for (int j = 0; j < n; j++) {
-      if (outM[i + j*n] != 0) 
-      std::cout << outM[i + j*n] << " ";
+      std::cout << C[i + j*m] << " ";
+    }
+    std::cout << std::endl;
+  }
+  return 0; 
+}
+*/
+/*  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < k; j++) {
+      A[i+m*j] = 1.0;
+      std::cout << A[i+m*j] << " ";
     }
     std::cout << std::endl;
   }
 
+  for(int i = 0; i < k; i++) {
+    for (int j = 0; j < n; j++) {
+      B[i+j*k] = 1.0;
+      std::cout << B[i+j*k] << " ";
+    }
+    std::cout << std::endl;
+  }
+  cuMultiplyMat(m, n, k, A, B, C);
+  
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      std::cout << C[i+m*j] << " ";
+    }
+    std::cout << std::endl;
+  }
   return 0;
 }
 */
--- cuda/GNAcuMath.h
+++ cuda/GNAcuMath.h
@@ -1,7 +1,14 @@
 /*************************************************
 Interfaces of CUDA implemetation of math methods
 **************************************************/
+#ifndef GNACUMATH_H
+#define GNACUMATH_H
+
 #include <cstdio>
 #include <math.h>
 
 extern "C" void cuInverseMat(int matSize, double* InMat, double* OutMat);
+extern "C" void cuMultiplyMat(int m, int n, int k, double* InA, double* InB, double* OutC);
+extern "C" void cuSolveLowerLS(int m, int n, double* A, double* B);
+
+#endif /* GNACUMATH_H */
