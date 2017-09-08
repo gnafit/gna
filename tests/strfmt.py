87beab1b97a9660bf7f45caa7801817a2cb867b8
Maxim Gonchar <gmaxfl@gmail.com>
2017-09-08 16:25:05 +0300

    minor

--- tests/strfmt.py
+++ tests/strfmt.py
@@ -3,7 +3,11 @@
 
 from __future__ import print_function
 from gna.labelfmt import formatter as L
+from gna.labelfmt import reg_dictionary
 from gna import labelfmt
+
+mydict = dict( logL2='logL2 replaced' )
+reg_dictionary( 'unfolding_corrected', mydict )
 
 print( L('As is: {logL1_label}') )
 print( L('Capitalize: {^logL1_label}') )
@@ -11,6 +15,8 @@ print( L('Indirect access to label: {$var}', var='logL1') )
 print( L('Capitalize indirect: {^$var}', var='logL1_label') )
 
 print( L('Unknown key: {unknown}') )
+
+print( L('Overridde key: {logL2}') )
 
 print(L('{^logL1_label}, {logL1}'))
 print(L('{^logL2_label}, {logL2}'))
