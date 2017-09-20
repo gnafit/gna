a403594eb6460e530c7b370f781a352047df118e
Konstantin Treskov <treskov_k@mail.ru>
2017-09-20 14:01:21 +0300

    Refactoring: check for namespace first. It allows for more elegant error handling

--- pylib/gna/parameter_loader.py
+++ pylib/gna/parameter_loader.py
@@ -10,24 +10,14 @@ def get_parameters(params):
     pars = []
     for candidate in params:
         try:
-            pars.append(env.pars[candidate])
-        except KeyError:
-            if cfg.debug_par_fetching: 
-                print("Parameter {0} not found, trying to use it as a namespace".format(candidate))
             par_namespace = env.ns(candidate)
-            #check that there is something in namespace
-            try:
-                par_namespace.walknames().next()
-            except StopIteration:
-                raise KeyError("Parameter {} can't be found or used as namespace".format(candidate))
+            par_namespace.walknames().next()
             independent_pars  = [par for _, par in par_namespace.walknames()
                                  if is_independent(par)]
             pars.extend(independent_pars)
+        except StopIteration:
+            if cfg.debug_par_fetching:
+                print("{0} is not a namespace, trying to use it as a parameter".format(candidate))
+            pars.append(env.pars[candidate])
+
     return pars
-
-            
-
-
-            
-
-
