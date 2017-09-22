f0ff6adf70dd8b3c64f04a8fcf14fb6c0363d26e
Maxim Gonchar <gmaxfl@gmail.com>
2017-09-22 17:06:43 +0300

    Add reference gaussian

--- tests/detector/test_eres.py
+++ tests/detector/test_eres.py
@@ -7,7 +7,14 @@ import numpy as N
 from load import ROOT as R
 from gna.env import env
 from gna.labelfmt import formatter as L
-from mpl_tools.helpers import savefig
+from mpl_tools.helpers import savefig, plot_hist
+from scipy.stats import norm
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument( '-o', '--output' )
+parser.add_argument( '-s', '--show', action='store_true' )
+opts = parser.parse_args()
 
 def axes( title, ylabel='' ):
     fig = P.figure()
@@ -31,15 +38,16 @@ env.defparameter( 'Eres_b',  central=0.081, relsigma=30*percent )
 env.defparameter( 'Eres_c',  central=0.026, relsigma=30*percent )
 
 binwidth=0.05
-edges = N.arange( 0.0, 12.0, binwidth )
+edges = N.arange( 0.0, 12.0001, binwidth )
+efine = N.arange( edges[0], edges[-1]+1.e-5, 0.005 )
 
 for eset in [
-    [ 1.0, 3.0, 6.0, 9.0 ],
-    [ ( 1.0, 5.0, 9.0 ) ],
-    [ ( 6.0, 7.0,  8.0, 8.8  ) ],
+    [ [1.025], [3.025], [6.025], [9.025] ],
+    [ [ 1.025, 5.025, 9.025 ] ],
+    [ [ 6.025, 7.025,  8.025, 8.825 ] ],
     ]:
     ax = axes( 'Energy resolution impact' )
-    for e in eset:
+    for i, e in enumerate(eset):
         phist = singularities( e, edges )
 
         hist = R.Histogram( phist.size, edges, phist )
@@ -49,9 +57,17 @@ for eset in [
         smeared = eres.smear.Nrec.data()
         print( 'Sum check for {} (diff): {}'.format( e, phist.sum()-smeared.sum() ) )
 
-        alpha = 0.7
-        bars = P.bar( edges[:-1], phist, binwidth, align='edge', alpha=alpha )
-        P.bar( edges[:-1], smeared, binwidth, align='edge', alpha=alpha, color=bars[0].get_facecolor() )
+        # bars = P.bar( edges[:-1], phist, binwidth, align='edge' )
+        lines = plot_hist( edges, smeared )
+        color = lines[0].get_color()
+        ax.vlines( e, 0.0, smeared.max(), linestyle='--', color=color )
+
+        if len(e)>1:
+            color='green'
+        for e in e:
+            ax.plot( efine, binwidth*norm.pdf( efine, loc=e, scale=eres.relativeSigma(e)*e ), linestyle='--', color=color )
+
+    savefig( opts.output, suffix='_test_%i'%i )
 
 ax = axes( 'Relative energy uncertainty', ylabel=L.u('eres_sigma_rel') )
 x = N.arange( 0.5, 12.0, 0.01 )
@@ -59,5 +75,8 @@ fcn = N.frompyfunc( eres.relativeSigma, 1, 1 )
 y = fcn( x )
 
 ax.plot( x, y*100. )
+savefig( opts.output, suffix='_sigma' )
 
-P.show()
+if opts.show:
+    P.show()
+
