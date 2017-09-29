012b0ef274bdbb158c36d190b7c5b73d9ad68d14
Maxim Gonchar <gmaxfl@gmail.com>
2017-09-29 12:44:18 +0300

    update

--- tests/detector/test_esmear.py
+++ tests/detector/test_esmear.py
@@ -36,40 +36,56 @@ def singularities( values, edges ):
 binwidth=0.05
 edges = N.arange( 0.0, 12.0001, binwidth )
 
+lower = True
+upper = False
+triangular = True
 n = 240
 mat = 0.0
-scales = N.geomspace( 1.0, 0.01, n )
-for i, scale in zip(range(n), scales):
-    mat += N.diag( N.full( n-i, scale ), i )
-mat = N.asfortranarray( mat )
+for i in range(n):
+    if i<4:
+        scale = 1.0 - i*0.01
+    else:
+        scale = 0.0001*(n-i)
+
+    print( i, scale )
+    if i:
+        if lower:
+            mat += N.diag( N.full( n-i, scale ),  i )
+        if upper:
+            mat += N.diag( N.full( n-i, scale ), -i )
+    else:
+        mat += N.diag( N.full( n-i, scale ), i )
+mat/=mat.sum( axis=0 )
+import IPython
+IPython.embed()
 
 for eset in [
     [ [1.025], [3.025], [6.025], [9.025] ],
     [ [ 1.025, 5.025, 9.025 ] ],
     [ [ 6.025, 7.025,  8.025, 8.825 ] ],
     ]:
-    ax = axes( 'Energy resolution impact' )
+    # ax = axes( 'Energy resolution impact' )
     for i, e in enumerate(eset):
-        phist = singularities( e, edges )
+        # phist = singularities( e, edges )
 
-        hist = R.Histogram( phist.size, edges, phist )
-        esmear = R.EnergySmear( mat.shape[0], mat.ravel(), True )
-        esmear.smear.Nvis( hist.hist )
+        # hist = R.Histogram( phist.size, edges, phist )
+        esmear = R.EnergySmear( mat.shape[0], mat.ravel( order='F' ), triangular )
+        # esmear.smear.inputs.Nvis( hist.hist )
 
-        smeared = esmear.smear.Nrec.data()
-        print( 'Sum check for {} (diff): {}'.format( e, phist.sum()-smeared.sum() ) )
+        # smeared = esmear.smear.Nrec.data()
+        # print( 'Sum check for {} (diff): {}'.format( e, phist.sum()-smeared.sum() ) )
 
-        # bars = P.bar( edges[:-1], phist, binwidth, align='edge' )
-        lines = plot_hist( edges, smeared )
-        color = lines[0].get_color()
-        ax.vlines( e, 0.0, smeared.max(), linestyle='--', color=color )
+        # # bars = P.bar( edges[:-1], phist, binwidth, align='edge' )
+        # lines = plot_hist( edges, smeared )
+        # color = lines[0].get_color()
+        # ax.vlines( e, 0.0, smeared.max(), linestyle='--', color=color )
 
-        if len(e)>1:
-            color='green'
-        for e in e:
-            ax.plot( efine, binwidth*norm.pdf( efine, loc=e, scale=esmear.relativeSigma(e)*e ), linestyle='--', color=color )
+        # if len(e)>1:
+            # color='green'
+        # for e in e:
+            # ax.plot( efine, binwidth*norm.pdf( efine, loc=e, scale=esmear.relativeSigma(e)*e ), linestyle='--', color=color )
 
-    savefig( opts.output, suffix='_test_%i'%i )
+    # savefig( opts.output, suffix='_test_%i'%i )
 
 fig = P.figure()
 ax = P.subplot( 111 )
@@ -79,6 +95,7 @@ ax.set_xlabel( '' )
 ax.set_ylabel( '' )
 ax.set_title( 'Synthetic energy leak matrix' )
 
+mat = convert(esmear.getMatrix(), 'matrix')
 mat = N.ma.array( mat, mask= mat==0.0 )
 c = ax.matshow( mat, extent=[ edges[0], edges[-1], edges[-1], edges[0] ] )
 add_colorbar( c )
