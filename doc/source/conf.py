05e86e9d725328ee653caaffa36850725a84d31c
Anna Fatkina <fatkina.a.i@gmail.com>
2017-10-13 12:37:02 +0300

    try uml

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -32,7 +32,8 @@ extensions = ['sphinx.ext.mathjax', 'sphinxcontrib.plantuml',
               'IPython.sphinxext.ipython_console_highlighting',
               'IPython.sphinxext.ipython_directive']
 
-plantuml = '/usr/bin/plantuml'
+#plantuml = '/usr/bin/plantuml'
+plantuml = 'java -Djava.awt.headless=true -jar /plantum.jar'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
