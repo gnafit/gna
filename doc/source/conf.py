gnafit/gna

commit 3a7081839ec56938e82c48c8d06d95c3f87d5920

Forgotten rubbish is deleted from conf

doc/source/conf.py
@@ -32,9 +32,7 @@ extensions = ['sphinx.ext.mathjax', 'sphinxcontrib.plantuml',
               'IPython.sphinxext.ipython_console_highlighting',
               'IPython.sphinxext.ipython_directive']
 
-#plantuml = '/usr/bin/plantuml'
 plantuml = 'java -Djava.awt.headless=true -jar /plantuml.jar'
-#plantuml = '~/plantuml'
 
 plantuml_output_format = 'svg'
 

commit d6720b2c96c8f4c4db109129620f6dd660743353

Fix plantuml config failure for local build introduced in 3a7081839ec56938e82c48c8d06d95c3f87d5920. Search for it in /usr/bin first.

doc/source/conf.py
@@ -32,7 +32,10 @@ extensions = ['sphinx.ext.mathjax', 'sphinxcontrib.plantuml',
               'IPython.sphinxext.ipython_console_highlighting',
               'IPython.sphinxext.ipython_directive']
 
-plantuml = 'java -Djava.awt.headless=true -jar /plantuml.jar'
+if 'plantuml' in os.listdir('/usr/bin'):
+    plantuml = '/usr/bin/plantuml'
+else:
+    plantuml = 'java -Djava.awt.headless=true -jar /plantuml.jar'
 
 plantuml_output_format = 'svg'
 