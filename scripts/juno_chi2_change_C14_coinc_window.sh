bd90bf0ae63ce6c6dac18f5df75d072902cda045
Konstantin Treskov <treskov_k@mail.ru>
2017-10-25 12:34:57 +0300

    Add possibility to vary coincidence window for C14 and additional script to study that possibility

--- scripts/juno_chi2_change_C14_coinc_window.sh
+++ scripts/juno_chi2_change_C14_coinc_window.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-python gna -- ns --define common.Eres_b central=0.03 sigma=0.0 --push common \
-           --define common.rho_C14 central=1e-19 sigma=0.0 --push common \
+python gna -- ns --define common.Eres_b central=0.03 sigma=0.0  \
+           --define common.rho_C14 central=1e-17 sigma=1e-19  \
+           --define common.CoincidenceWindow central=300 sigma=1 --push common \
 -- juno --with-C14 --name juno_ih --ibd first --binning AD1 1 10 700 \
 -- ns --value juno_ih.oscillation.Alpha inverted \
 -- juno --name juno_nh --with-C14 --ibd first --binning AD1 1 10 700 \
@@ -9,14 +10,12 @@ python gna -- ns --define common.Eres_b central=0.03 sigma=0.0 --push common \
 -- chi2 chi2 testhier \
 -- minimizer min minuit chi2 juno_nh.oscillation.DeltaMSqEE juno_nh.oscillation.SinSq12 \
    juno_nh.oscillation.SinSq13 juno_nh.oscillation.DeltaMSq12 \
--- contour --chi2 /tmp/out_C14.hdf5  --minimizer min --plot chi2minplt \
-   --no-shift --xlog --xlabel "\$ \rho_{rel} \$" --ylabel "\$ \Delta \chi^2 \$" \
-   --title "Coincidence window is 300 ns" \
-   --show
-   # --output chi2_C14.pdf
+-- scan  --lingrid common.CoincidenceWindow 300 1200 40 --minimizer min \
+  --verbose --output /tmp/out_c14_coinc.hdf5 \
+-- contour --chi2 /tmp/out_c14_coinc.hdf5  --minimizer min --plot chi2minplt \
+   --no-shift  --xlabel "Coincidence window, ns" --ylabel "\$ \Delta \chi^2 \$" \
+   --output chi2_C14_1e-17_coincidence_window.pdf  --title " \$ \rho_{rel} \$  is fixed to \$ 10^{-17}\$ "
 #--pullminimizer min
 # juno_nh.oscillation.DeltaMSqEE juno_nh.oscillation.SinSq12
 # juno_nh.oscillation.SinSq13 \
  # --lingrid common.Eres_b 0.001 0.1 10 --samples-type grid --single --minimizer min \
-# -- scan  --lingrid common.rho_C14 1e-19 1e-16 40 --minimizer min \
-   # --verbose --output /tmp/out_C14.hdf5 \
