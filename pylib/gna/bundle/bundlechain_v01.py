bb9c19dd929822ced62ad78d807ad33dca24bcce
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2017-12-26 17:35:50 +0300

    minor

--- pylib/gna/bundle/bundlechain_v01.py
+++ pylib/gna/bundle/bundlechain_v01.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-nfrom __future__ import print_function
+from __future__ import print_function
 from load import ROOT as R
 import numpy as N
 from gna.env import env, namespace
