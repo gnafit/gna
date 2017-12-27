667cc06f9dda1079df0ee48deca10621b871db0c
Anna Fatkina <fatkina.a.i@gmail.com>
2017-12-27 18:45:50 +0300

    gpu array wrapper for cpu side code -- not finished

---
+++ cuda/extra/GNAcuGpuArray.cu
@@ -0,0 +1,258 @@
+#include "GNAcuGpuArray.hh"
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <iostream>
+#include "GNAcuGpuMemStates.hh"
+
+template<typename T>
+__global__ void vecAdd (T* res, T*  inA, T* inB, size_t n) {
+        int x = blockDim.x * blockIdx.x + threadIdx.x;
+//	if (x >= n) return;
+	res[x] = inA[x] + inB[x];
+}
+
+template<typename T>
+__global__ void setByValueGPU (T* res, T val, size_t n) {
+        int x = blockDim.x * blockIdx.x + threadIdx.x;
+//	if (x >= n) return;
+        res[x] = val;
+}
+
+
+template <typename T>
+GNAcuGpuArray<T>::GNAcuGpuArray() {
+	std::cout <<  "I am created but not inited " << std::endl;
+        arrState = NotInitialized;
+}
+
+template <typename T>
+GNAcuGpuArray<T>::GNAcuGpuArray(T* inArrayPtr, size_t inSize) {
+	std::cout <<  "I am created by ptr " << std::endl; 
+	cudaSetDevice(0);
+	cudaError_t err;
+        arrSize = inSize;
+        size_t alloc_size = sizeof(T) * inSize;
+	err = cudaMalloc((void**)&arrayPtr, alloc_size);
+        if (err != cudaSuccess) {
+                printf("ERROR: unable to  allocate!\n");
+                std::cout << "err is " << cudaGetErrorString(err) << std::endl;
+                arrState = Crashed;
+        }
+	else {
+		arrState = InitializedOnly; 
+	}
+}
+
+template<typename T>
+GNAcuGpuArray<T>::~GNAcuGpuArray() {
+	cudaFree(arrayPtr);
+	cudaDeviceReset();
+}
+
+template<typename T>
+void GNAcuGpuArray<T>::resize(size_t newSize) {
+        cudaError_t err;
+	if (arrSize == newSize) return;
+	if (arrSize > newSize) {
+		arrSize = newSize;
+		//cudaFree()
+		// TODO: free the end of array
+		std::cerr << "WARNING! New array size is less then old size. Some data may be lost!" << std::endl; 
+	} else if (arrSize < newSize) {
+		// TODO: resizing without realloc
+		cudaFree(arrayPtr);
+		size_t alloc_size = sizeof(T) * newSize;
+		arrSize = newSize;
+		err = cudaMalloc((void**)&arrayPtr, alloc_size);
+	        if (err != cudaSuccess) {
+        	        printf("ERROR: unable to  allocate!\n");
+                	std::cerr << "err is " << cudaGetErrorString(err) << std::endl;
+        	        arrState = Crashed;
+       	 	}
+	}
+}
+
+template<typename T> 
+void GNAcuGpuArray<T>::setByHostArray(T* inHostArr) {
+        cudaError_t err;
+	err = cudaMemcpy((void**)&arrayPtr, inHostArr, sizeof(T) * arrSize, cudaMemcpyHostToDevice);
+	if (err != cudaSuccess) {
+                printf("ERROR: unable to set memory H2D!\n");
+                std::cout << "Err is: " << cudaGetErrorString(err) << std::endl;
+                arrState = Crashed;
+        }
+        else {
+                arrState = OnDevice;
+        }
+}
+
+template<typename T>
+void GNAcuGpuArray<T>::setByDeviceArray(T* inDeviceArr) {
+        cudaError_t err;
+        err = cudaMemcpy(arrayPtr, inDeviceArr, sizeof(T) * arrSize, cudaMemcpyDeviceToDevice);
+	if (err != cudaSuccess) {
+                printf("ERROR: unable to set memory D2D!\n");
+                std::cout << "Err is: " << cudaGetErrorString(err) << std::endl;
+                arrState = Crashed;
+        }
+        else {
+                arrState = OnDevice;
+        }
+}
+
+template <typename T>
+void GNAcuGpuArray<T>::setByValue(T value) {
+	cudaError_t err;
+	setByValueGPU<T><<<arrSize, 1>>> (arrayPtr, value, arrSize);
+        arrState = OnDevice;
+}
+
+template<typename T>
+void GNAcuGpuArray<T>::getContentToCPU(T* dst) {
+	cudaError_t err;
+	double* ttt = new double[arrSize];
+std::cout << "In getContent on cpu`: arrSize = " << arrSize << std::endl;
+//        err = cudaMemcpy(ttt, arrayPtr, sizeof(T) * arrSize, cudaMemcpyDeviceToHost);
+err = cudaMemcpy(ttt, arrayPtr, sizeof(T) * arrSize, cudaMemcpyDeviceToHost);
+
+        if (err != cudaSuccess) {
+                printf("ERROR: unable to get array values to host!\n");
+                std::cout << "Err is: " << cudaGetErrorString(err) << std::endl;
+                arrState = Crashed;
+        }
+        else {
+                arrState = OnHost;
+        }
+
+std::cout << std::endl;
+
+
+}
+
+template<typename T>
+void GNAcuGpuArray<T>::getContent(T* dst) {
+        cudaError_t err;
+std::cout << "In getContent: arrSize = " << arrSize << std::endl;
+        err = cudaMemcpy(dst,
+		 	arrayPtr, 
+			sizeof(T) * arrSize, 
+			cudaMemcpyDeviceToDevice);
+        if (err != cudaSuccess) {
+                printf("ERROR: unable to get array values!\n");
+                std::cout << "Err is: " << cudaGetErrorString(err) << std::endl;
+                arrState = Crashed;
+        }
+        else {
+                arrState = OnDevice;
+        }
+
+
+double * tmppp = new double[arrSize];
+//double * rhsTmp;
+//err =cudaMalloc((void**)&rhsTmp, sizeof(F) * rhs.getArraySize());
+
+//rhs.getContent(rhsTmp);
+err = cudaMemcpy(tmppp, arrayPtr, sizeof(T) * arrSize, cudaMemcpyDeviceToHost);
+if (err != cudaSuccess) {
+                printf("ERROR: unable to  allocate!\n");
+                std::cout << "err is " << cudaGetErrorString(err) << std::endl;
+                //res.arrState = Crashed;
+        }
+
+for (int i = 0; i < arrSize; i++ ) {
+        std::cout << tmppp[i] << " ";
+}
+std::cout << "END!" << std::endl;
+
+
+}
+
+
+template <typename F>
+GNAcuGpuArray<F> GNAcuGpuArray<F>::operator+(GNAcuGpuArray<F> rhs) {
+        F* resPtr;
+	size_t res_size = arrSize;
+        if(arrSize != rhs.getArraySize()) {
+                std::cerr << "ERROR: Sizes of lhs and rhs are different! The smallest will be used!" << std::endl;
+		if (arrSize > rhs.getArraySize()) res_size = rhs.getArraySize();
+        }
+        cudaError_t err;
+        err = cudaMalloc((void**)&resPtr, sizeof(F) * res_size);
+        if (err != cudaSuccess) {
+                printf("ERROR: unable to  allocate memory for add result!\n");
+                std::cerr << "err is " << cudaGetErrorString(err) << std::endl;
+        }
+
+//    GNAcuGpuArray<F> res(resPtr, res_size);
+        vecAdd<F><<< res_size, 1>>>(resPtr, arrayPtr, rhs.getArrayPtr(), res_size);
+	F* ttt;
+        GNAcuGpuArray<F> res(ttt, res_size);
+	res.setByDeviceArray(resPtr);
+        res.arrState = OnDevice;
+
+/*double* restmp = new double[res_size];
+std::cout << "restmp:" << std::endl;
+err = cudaMemcpy(restmp, res.getArrayPtr(), res_size * sizeof(F), cudaMemcpyDeviceToHost);
+for (int i = 0 ; i < res_size; i++) {
+std::cout << restmp[i] << " ";
+}
+std::cout << "ENDDDD" << std::endl;
+*/
+	return res;
+}
+
+/*template <typename T> 
+GNAcuGpuArray<T> operator+(GNAcuGpuArray<T> &lhs, GNAcuGpuArray<T> &rhs) {
+        T* resPtr;
+	GNAcuGpuArray<T> res(resPtr, lhs.getArraySize());
+	if(lhs.getArraySize() != rhs.getArraySize()) {
+		std::cerr << "ERROR: Unable to add! Sizes of lhs and rhs are different!" << std::endl;
+		return res;
+	}
+        cudaError_t err;
+	err = cudaMalloc((void**)resPtr, sizeof(T) * lhs.getArraySize());
+        if (err != cudaSuccess) {
+                printf("ERROR: unable to  allocate!\n");
+                std::cout << "err is " << cudaGetErrorString(err) << std::endl;
+                res.arrState = Crashed;
+        }
+	vecAdd<<<1, lhs.getArraySize()>>>(resPtr, lhs, rhs);
+	res.arrState = OnDevice;
+	return res;
+}
+*/
+template <typename T>
+GNAcuGpuArray<T>& GNAcuGpuArray<T>::operator= (GNAcuGpuArray<T> rhs) {
+        cudaError_t err;
+	T* tmpArr;
+//`GNAcuGpuArray<T> res(tmpArr, rhs.getArraySize());
+        err = cudaMalloc((void**)&tmpArr, sizeof(T) * rhs.getArraySize());
+	rhs.getContent(tmpArr);
+	resize(rhs.getArraySize());
+//	arrayPtr = tmpArr;
+        err = cudaMemcpy(arrayPtr, tmpArr, sizeof(T) * rhs.getArraySize(), cudaMemcpyDeviceToDevice);
+
+//res.setByDeviceArray(tmpArr);
+
+
+std::cout << "in Op = : size = " << arrSize;
+double * tmppp = new double[arrSize];
+err = cudaMemcpy(tmppp, arrayPtr, sizeof(T) * rhs.getArraySize(), cudaMemcpyDeviceToHost);
+if (err != cudaSuccess) {
+                printf("ERROR: unable to  allocate!\n");
+                std::cout << "err is " << cudaGetErrorString(err) << std::endl;
+                //res.arrState = Crashed;
+        }
+
+for (int i = 0; i < arrSize; i++ ) {
+	std::cout << tmppp[i] << " ";
+}
+std::cout << std::endl;
+	return *this;
+}
+
+template class GNAcuGpuArray<double>;
+//template class GNAcuGpuArray<float>;
+//template class GNAcuGpuArray<int>;
+//template class GNAcuGpuArray<bool>;
+
---
+++ cuda/extra/GNAcuGpuArray.hh
@@ -0,0 +1,38 @@
+#ifndef GNACUGPUARRAY_H
+#define GNACUGPUARRAY_H
+
+#include "GNAcuGpuMemStates.hh"
+#include <iostream>
+
+template <typename T>
+class GNAcuGpuArray {
+public: 
+	GpuMemoryState arrState;
+	GNAcuGpuArray();
+        GNAcuGpuArray(T* inArrayPtr, size_t inSize);
+	~GNAcuGpuArray();
+	void resize(size_t newSize);
+	void setByHostArray(T* inHostArr);
+        void setByDeviceArray(T* inDeviceArr);
+	void setByValue(T value);
+	void getContentToCPU(T* dst);
+	void getContent(T* dst);
+	T* getArrayPtr() { return arrayPtr; }
+	size_t getArraySize() { return arrSize; }
+	GNAcuGpuArray<T> operator+(GNAcuGpuArray<T> rhs);
+        GNAcuGpuArray<T>& operator=(GNAcuGpuArray<T> rhs);
+protected:
+	T* arrayPtr;
+        size_t arrSize;
+};
+
+//template <typename T>
+//GNAcuGpuArray<T> operator+(GNAcuGpuArray<T> &lhs, GNAcuGpuArray<T> &rhs) ;
+//extern "C" GNAcuGpuArray<double> operator+(GNAcuGpuArray<double> &lhs, GNAcuGpuArray<double> &rhs);
+
+
+//template <typename T>
+//void operator=(GNAcuGpuArray<T>& lhs, const GNAcuGpuArray<T>& rhs);
+
+
+#endif /* GNACUGPUARRAY_H */
