3e192260f53c7d42053b40624d89d821878b3c59
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2018-01-17 14:00:38 +0300

    Finalize grouping

--- pylib/gna/grouping.py
+++ pylib/gna/grouping.py
@@ -2,10 +2,19 @@
 
 from __future__ import print_function
 from collections import OrderedDict
+from itertools import chain
 
 class Groups(object):
+    """Helper class to manage groups with following abilities:
+        - keeps list of groups and their elements (each element may by only in one group)
+        - may determine the group of an item
+        - may return a list with items of a single group or all the items from all the groups
+
+        The class uses OrderedDict for storage and thus keeps the order.
+    """
     def __init__(self, groups):
-        self.groups = groups
+        """groups - dict like object with key : [item1, item2, ...] pairs"""
+        self.groups = OrderedDict(groups)
         self.match = OrderedDict()
         for groups, keylist in self.groups.items():
             for key in keylist:
@@ -14,26 +23,61 @@ class Groups(object):
     def __repr__(self):
         return self.groups.__repr__()
 
-    def group(self, item):
-        return self.match[item]
+    def keys(self, group=None):
+        """Returns iterable with items of a group or all the items of all the groups"""
+        return self.groups[group] if group else chain(*self.groups.values())
 
-    def items(self, group):
-        return self.groups[group]
+    def items(self):
+        """Unpack each group and iterate group, key pairs"""
+        for group, keys in self.groups.items():
+            for key in keys:
+                yield group, key
 
     def __contains__(self, item):
+        """Checks that item is known"""
         return item in self.match
 
+    def __getitem__(self, item):
+        """Returns a group of an item"""
+        return self.match[item]
+
+
 class GroupedDict(OrderedDict):
+    """OrderedDict implementation with:
+        - if key is present the behaviour is regular
+        - if key is missing, checks if key belongs to a group and uses group name instead of key"""
     def __init__(self, *args, **kwargs):
-        self.groups = Groups(kwargs.pop( 'groups' ))
+        """Obligatory argument: groups - Groups object or dict like object with key : [item1, item2, ...] pairs"""
+        self.groups = kwargs.pop( 'groups' )
+        if not isinstance(self.groups, Groups):
+            self.groups = Groups(self.groups)
 
         super(GroupedDict, self).__init__(*args, **kwargs)
 
     def __contains__(self, key):
+        """Checks if key or its group present in dictionary"""
         contains = super(OrderedDict, self).__contains__
         return contains(key) or key in self.groups and contains(self.groups.group(key))
 
-    def __getitem__(self, key, default)
+    def __missing__(self, key):
+        """Return value by group name instead of a key name"""
+        return self[self.groups[key]]
+
+    def subkeys(self, group=None):
+        """The same as key but replace each group by its elements"""
+        return self.groups.keys( group )
+
+    def subitems(self):
+        """The same as items but replace each group by its elements"""
+        for group, key in self.groups.items():
+            yield key, self[group]
+
+    def subvalues(self):
+        """The same as items but replace each group by its elements"""
+        for group, key in self.groups.items():
+            yield self[group]
+
+
 
 
 
--- tests/elementary/test_groups.py
+++ tests/elementary/test_groups.py
@@ -1,29 +1,62 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 from gna.grouping import *
 from collections import OrderedDict
 
-dets = OrderedDict( {
-    '11' : 1, '12' : 2,
-    '21' : 3, '22' : 8,
-    '31' : 4, '32' : 5,
-    '33' : 6, '33' : 7
-    } )
+dets = OrderedDict( [
+    ('11', 1), ('12', 2),
+    ('21', 3), ('22', 8),
+    ('31', 4), ('32', 5),
+    ('33', 6), ('33', 7)
+    ] )
 
-sites = OrderedDict( {
-    'EH1' : 'A',
-    'EH2' : 'B',
-    'EH3' : 'C'
-    } )
+sites = OrderedDict( [
+    ('EH1', 'A'),
+    ('EH2', 'B'),
+    ('EH3', 'C')
+    ] )
 
-groups = OrderedDict( {
-    'EH1' : ('11', '12'),
-    'EH2' : ('21', '22'),
-    'EH3' : ('31', '32', '33', '34'),
-    } )
+groups = OrderedDict( [
+    ( 'EH1', ('11', '12') ),
+    ( 'EH2', ('21', '22') ),
+    ( 'EH3', ('31', '32', '33', '34') ),
+    ] )
 
 gd = GroupedDict( sites, groups=groups )
 
-import IPython
-IPython.embed()
+print( 'Retrieve data by actual keys (sites):' )
+for k, v in gd.items():
+    print( '  ', k, v )
+print()
+
+print( 'Retrieve data by detectors' )
+for k, v in gd.subitems():
+    print( '  ', k, v )
+print()
+
+print( 'Keys' )
+print( list(gd.keys()) )
+print()
+
+print( 'Sub keys' )
+print( list(gd.subkeys()) )
+print()
+
+print( 'Items' )
+print( list(gd.items()) )
+print()
+
+print( 'Sub items' )
+print( list(gd.subitems()) )
+print()
+
+print( 'Values' )
+print( list(gd.values()) )
+print()
+
+print( 'Sub values' )
+print( list(gd.subvalues()) )
+print()
+
