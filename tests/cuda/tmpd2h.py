2cae81052cc33888202002da64e6a103a00ab7b0
Anna Fatkina <fatkina.a.i@gmail.com>
2018-01-26 17:20:18 +0300

    GPU tests

---
+++ tests/cuda/tmpd2h.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Test gpu"""
+
+from __future__ import print_function
+from matplotlib import pyplot as plt
+import numpy as N
+from load import ROOT as R
+from matplotlib.ticker import MaxNLocator
+
+def array_to_stdvector( array, dtype ):
+    """Convert an array to the std::vector<dtype>"""
+    ret = R.vector(dtype)( len( array ) )
+    for i, v in enumerate( array ):
+        ret[i] = v
+    return ret
+
+
+#
+# Create the matrix
+#
+mat = N.arange(12, dtype='d')
+
+# emat = R.Eigen.MatrixXd()
+
+print( 'Input matrix (numpy)' )
+print( mat )
+print()
+
+lmat = mat.ravel( order='F' )
+shape = array_to_stdvector(mat.shape, 'size_t')
+
+points = R.Points( lmat, shape )
+print( 'Input 0' )
+test = N.frombuffer( points.data(), count=mat.size )
+print( test )
+
+identitygpu = R.IdentityGPU()
+id2 = R.IdentityX2()
+#idGPU = R.IdentityGPU()
+identitygpu.identitygpu.source( points.points.points )
+id2.identityx2.source(identitygpu.identitygpu.target)
+#print( 'Eigen dump (C++)' )
+#identity.dump()
+#print()
+
+res = id2.identityx2.target.data()
+
+print( 'Input 1' )
+test = N.frombuffer( points.data(), count=mat.size )
+print( test )
+
+print( 'Input 2' )
+print(points.points.points.data())
+
+print( 'Eigen dump (C++)' )
+id2.dump()
+print( 'Result (C++ Data to numpy)' )
+print( res )
+print()
+
