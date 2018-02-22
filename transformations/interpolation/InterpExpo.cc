47879068e8ad768de9348576099b165ab23928f4
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2018-02-22 15:57:22 +0300

    minor

--- transformations/interpolation/InterpExpo.cc
+++ transformations/interpolation/InterpExpo.cc
@@ -1,14 +1,16 @@
 #include "InterpExpo.hh"
 #include "TypesFunctions.hh"
 
+#include <stdexcept>
+
 using std::next;
 using std::prev;
 
-InterpExpo::InterpExpo() : SegmentWise() {
+InterpExpo::InterpExpo(const std::string& underflow_strategy, const std::string& overflow_strategy) : SegmentWise() {
   transformation_("interp")
-    .input("points")
-    .input("edges")
-    .input("fcn")
+    .input("newx")
+    .input("x")
+    .input("y")
     .input("segments")
     .output("interp")
     .types(TypesFunctions::ifPoints<0>, TypesFunctions::if1d<0>)
@@ -18,21 +20,40 @@ InterpExpo::InterpExpo() : SegmentWise() {
     .types(TypesFunctions::pass<0,0>)
     .func(&InterpExpo::do_interpolate)
     ;
+
+  if(underflow_strategy.length()){
+    this->setUnderflowStrategy(underflow_strategy);
+  }
+  if(overflow_strategy.length()){
+    this->setOverflowStrategy(overflow_strategy);
+  }
+}
+
+void InterpExpo::interpolate(SingleOutput& x, SingleOutput& y, SingleOutput& newx){
+  auto segments = this->t_["segments"];
+  segments.inputs["x"](x)
+  segments.inputs["points"](newx)
+
+  auto interp = this->t_["interp"];
+  interp.inputs["points"](newx)
+  interp.inputs["x"](x)
+  interp.inputs["y"](y)
+  interp.inputs["segments"](segments.outputs["segments"])
 }
 
 void InterpExpo::do_interpolate(Args args, Rets rets){
-  auto& points_a=args[0].x;
-  auto& edges_a=args[1].x;
-  auto& fcn_a=args[2].x;
+  auto& newx_a=args[0].x;
+  auto& x_a=args[1].x;
+  auto& y_a=args[2].x;
 
-  auto nseg=edges_a.size()-1;
-  auto& ratio_a=fcn_a.head(nseg)/fcn_a.tail(nseg);
-  auto& width_a=edges_a.tail(nseg)-edges_a.head(nseg);
+  auto nseg=x_a.size()-1;
+  auto& ratio_a=y_a.head(nseg)/y_a.tail(nseg);
+  auto& width_a=x_a.tail(nseg)-x_a.head(nseg);
   auto& b_a=(ratio_a.log()/width_a).eval();
 
-  auto k_current=fcn_a.data();
+  auto k_current=y_a.data();
   auto b_current=b_a.data();
-  auto e0_current=edges_a.data();
+  auto e0_current=x_a.data();
 
   auto& ddx_d=args[3];
   auto  ddx_current=ddx_d.buffer;
@@ -67,7 +88,7 @@ void InterpExpo::do_interpolate(Args args, Rets rets){
     auto length=idx_next-idx_current;
 
     //printf("Fill %i->%i (%i): fcn\n", (int)idx_current, (int)idx_next, (int)length);
-    interp_a.segment(idx_current, length) = *k_current * ((*e0_current - points_a.segment(idx_current, length))*(*b_current)).exp();
+    interp_a.segment(idx_current, length) = *k_current * ((*e0_current - newx_a.segment(idx_current, length))*(*b_current)).exp();
 
     next_iter();
   }
@@ -77,4 +98,18 @@ void InterpExpo::do_interpolate(Args args, Rets rets){
     interp_a.tail(n-idx_current)=m_overflow;
     //printf("Fill %i->%i (%i): %g (tail)\n", (int)idx_current, (int)n, (int)(n-idx_current), m_overflow);
   }
+}
+
+InterpExpo::Strategy InterpExpo::getStrategy(const std::string& strategy){
+  if(strategy=="constant"){
+    return Constant;
+  }
+  else if(strategy=="extrapolate"){
+    return Extrapolate;
+  }
+  else{
+    throw std::runtime_error("Unknown underflow/overflow strategy");
+  }
+
+  return Constant;
 }
--- transformations/interpolation/InterpExpo.hh
+++ transformations/interpolation/InterpExpo.hh
@@ -1,20 +1,36 @@
 #pragma once
 
+#include <string>
 #include "SegmentWise.hh"
 
 class InterpExpo: public SegmentWise,
                   public TransformationBind<InterpExpo> {
 public:
+  enum Strategy {
+    Constant = 0,
+    Extrapolate
+  };
   using TransformationBind<InterpExpo>::transformation_;
 
-  InterpExpo();
+  InterpExpo(const std::string& underflow_strategy="", const std::string& overflow_strategy="");
 
   void do_interpolate(Args, Rets);
+  void interpolate(SingleOutput& x, SingleOutput& y, SingleOutput& newx);
 
   void setUnderflow(double value) { m_underflow = value; }
   void setOverflow(double value) { m_overflow = value; }
+
+  void setUnderflowStrategy(const std::string& strategy)  { m_underflow_strategy=getStrategy(strategy); }
+  void setOverflowStrategy(const std::string& strategy)   { m_overflow_strategy=getStrategy(strategy); }
+
+  void setUnderflowStrategy(Strategy strategy)  { m_underflow_strategy=strategy; }
+  void setOverflowStrategy(Strategy strategy)   { m_overflow_strategy=strategy; }
+
+  Strategy getStrategy(const std::string& strategy);
 protected:
   double m_underflow{0.0};
   double m_overflow{0.0};
 
+  Strategy m_underflow_strategy{Constant};
+  Strategy m_overflow_strategy{Constant};
 };
