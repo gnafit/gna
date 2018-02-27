bee46a696dbc7b992f2c490fcc1c6c9d5fac78e7
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2018-02-27 14:20:43 +0300

    Load Huber/Mueller antineutrino spectra

--- pylib/gna/bundle/reactor_anu_spectra_v01.py
+++ pylib/gna/bundle/reactor_anu_spectra_v01.py
@@ -6,10 +6,11 @@ import constructors as C
 import numpy as N
 from collections import OrderedDict
 from gna.bundle import *
+from scipy.interpolate import interp1d
 
 class reactor_anu_spectra_v01(TransformationBundle):
     short_names = dict( U5  = 'U235', U8  = 'U238', Pu9 = 'Pu239', Pu1 = 'Pu241' )
-    debug = True
+    debug = False
     def __init__(self, **kwargs):
         kwargs['namespaces'] = [self.short_names.get(s,s) for s in kwargs['cfg'].isotopes]
         super(reactor_anu_spectra_v01, self).__init__( **kwargs )
@@ -19,15 +20,25 @@ class reactor_anu_spectra_v01(TransformationBundle):
 
         model_edges_t = C.Points( self.model_edges )
         self.objects['edges'] = model_edges_t
-        for ns in self.namespaces:
-            print(ns.name)
 
-            # ie = R.InterpExpo(self.cfg.strategy['underflow'], self.cfg.strategy['overflow'])
-            # ie.interpolate(segments_t, fcn_t, points_t)
+        newx = self.shared.points
+        for ns in self.namespaces:
+            spectrum_t = C.Points( self.spectra[ns.name] )
+
+            ie = R.InterpExpo(self.cfg.strategy['underflow'], self.cfg.strategy['overflow'])
+            ie.interpolate(model_edges_t, spectrum_t.single(), newx)
+
+            """Store data"""
+            self.objects[('interp', ns.name)] = ie
+            self.transformations_out[ns.name] = ie.interp
+            self.outputs[ns.name] = ie.interp.interp
 
     def load_data(self):
-        self.spectra = OrderedDict()
+        """Read raw input spectra"""
+        self.spectra_raw = OrderedDict()
         dtype = [ ('enu', 'd'), ('yield', 'd') ]
+        if self.debug:
+            print('Load files:')
         for ns in self.namespaces:
             for format in self.cfg.filename:
                 fname = format.format(iso=ns.name)
@@ -39,14 +50,24 @@ class reactor_anu_spectra_v01(TransformationBundle):
                     if self.debug:
                         print( ns.name, fname )
                         print( data )
-                    self.spectra[ns.name] = data
+                    self.spectra_raw[ns.name] = data
                     break
             else:
                 raise Exception('Failed to load the spectrum for %s'%ns.name)
 
-        self.model_edges = N.ascontiguousarray( self.cfg.edges )
+        """Read parametrization edges"""
+        self.model_edges = N.ascontiguousarray( self.cfg.edges, dtype='d' )
         if self.debug:
-            print( 'Bin edges:', self.edges )
+            print( 'Bin edges:', self.model_edges )
+
+        """Compute the values of spectra on the parametrization"""
+        self.spectra=OrderedDict()
+        for name, (x, y) in self.spectra_raw.items():
+            f = interp1d( x, N.log(y), bounds_error=True )
+            model = N.exp(f(self.model_edges))
+            self.spectra[name] = model
+
+
 
 
 
--- tests/bundle/reactor_anu_spectra_v01.py
+++ tests/bundle/reactor_anu_spectra_v01.py
@@ -10,16 +10,42 @@ from gna.env import env, findname
 from matplotlib import pyplot as P
 from mpl_tools.helpers import plot_hist, plot_bar
 import constructors as C
+from gna.labelfmt import formatter as L
+
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument( '-l', '--log', action='store_true', help='logarithmic scale' )
+opts=parser.parse_args()
 
 cfg = NestedDict()
 cfg.bundle = 'reactor_anu_spectra_v01'
 cfg.isotopes = [ 'U5', 'U8', 'Pu9', 'Pu1' ]
 cfg.filename = ['data/Huber_smooth_extrap_{iso}_13MeV0.01MeVbin.dat', 'data/Mueller_smooth_extrap_{iso}_13MeV0.01MeVbin.dat']
-cfg.strategy = dict( underflow='fill', overflow='extrapolate' )
+cfg.strategy = dict( underflow='constant', overflow='extrapolate' )
 cfg.edges = N.concatenate( ( N.arange( 1.8, 8.7, 0.5 ), [ 12.3 ] ) )
 
 points = N.linspace( 0.0, 12.0, 241 )
 points_t = C.Points(points)
 
 shared=NestedDict( points=points_t.single() )
-b, = execute_bundle( cfg=cfg )
+b, = execute_bundle( cfg=cfg, shared=shared )
+
+fig = P.figure()
+ax = P.subplot( 111 )
+ax.minorticks_on()
+ax.grid()
+ax.set_xlabel( L.u('enu') )
+ax.set_ylabel( L.u('anu_yield') )
+ax.set_title( '' )
+
+ax.vlines(cfg.edges, 0.0, 2.5, linestyles='--', alpha=0.5, colors='blue')
+
+for name, output in b.outputs.items():
+    ax.plot( points, output.data(), label=L.s(name) )
+
+if opts.log:
+    ax.set_yscale('log')
+
+ax.legend( loc='upper right' )
+
+P.show()
