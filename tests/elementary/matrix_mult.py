1deafda4d52eb974228dce28b8d2bafe5ac8517a
Konstantin Treskov <treskov_k@mail.ru>
2018-03-28 15:48:01 +0800

    Multiplication of three matrices

--- tests/elementary/matrix_mult.py
+++ tests/elementary/matrix_mult.py
@@ -18,8 +18,21 @@ print("Second matrix\n", points2.points.data())
 prod = ROOT.MatrixProduct()
 prod.multiply(points1)
 prod.multiply(points2)
-print("From python\n", np.dot(mat1, mat2))
-print("From C++\n", prod.product.data())
-assert(np.allclose(np.dot(mat1, mat2), prod.product.data()), "The products
-don't coincide")
-print("Test passed")
+prod_of_two = np.dot(mat1, mat2)
+print("Product of two from python\n", prod_of_two )
+print("Product of two from C++\n", prod.product.data())
+assert np.allclose(prod_of_two, prod.product.data()), "The products of two matrices don't coincide"
+
+print("Add third matrix to product")
+
+prod2 = ROOT.MatrixProduct()
+mat3 = np.matrix((np.arange(10, 30).reshape((4,5))))
+points3 = convert(mat3, ROOT.Points)
+prod2.multiply(points1)
+prod2.multiply(points2)
+prod2.multiply(points3)
+prod_of_three = np.dot(prod_of_two, mat3)
+print("Third matrix\n", points3.points.data())
+print("Product of three from python\n", prod_of_three)
+print("Product of three from C++\n", prod.product.data())
+assert np.allclose(prod_of_three, prod2.product.data()), "The products of three matrices don't coincide"
