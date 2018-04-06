35bf4951fb3f395c3f4f544ff2eba81f66e82db3
Konstantin Treskov <treskov_k@mail.ru>
2018-04-06 20:30:11 +0800

    Add possibility to set random seed for generator from CLI for datasets

--- pylib/gna/ui/dataset.py
+++ pylib/gna/ui/dataset.py
@@ -20,8 +20,12 @@ class cmd(basecmd):
                             default=[])
         parser.add_argument('--error-type', choices=['pearson', 'neyman'],
                 default='pearson', help='The type of statistical errors to be used')
+        parser.add_argument('--random-seed', type=int, help='Set random seed of numpy random generator to given value')
 
     def run(self):
+        if self.opts.random_seed:
+            np.random.seed(self.opts.random_seed)
+
         dataset = Dataset(desc=None)
         if self.opts.pull:
             pull_pars = get_parameters(self.opts.pull)
