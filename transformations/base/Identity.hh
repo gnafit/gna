12f0a8fe68c7df9da57d53ab395875ce4e52bf94
Anna Fatkina <fatkina.a.i@gmail.com>
2018-08-13 10:33:36 +0300

    use example

--- transformations/base/Identity.hh
+++ transformations/base/Identity.hh
@@ -10,33 +10,26 @@
 class Identity: public GNASingleObject,
                 public TransformationBind<Identity> {
 public:
-  bool isgpu = false;
-  Identity(bool gpu = false) : isgpu(gpu) {
+  Identity() {
     transformation_("identity")
       .input("source")
       .output("target")
       .types(TypesFunctions::pass<0,0>)
-      .func([](FunctionArgs& fargs){ fargs.rets[0].x = fargs.args[0].x; })
-#ifdef GNA_CUDA_SUPPORT
-      .setEntryLocation(gpu? DataLocation::Device : DataLocation::Host)
+      .func([](FunctionArgs& fargs){ fargs.rets[0].x = fargs.args[0].x;})
+#ifdef GNA_CUDA_SUPPORT     //
+      .setEntryLocation(DataLocation::Device)
+      .func("GPU", &Identity::gpu_test)
 #endif
       ;
   };
 
-  void ident(Args args, Rets rets) {
 #ifdef GNA_CUDA_SUPPORT
-    if (isgpu) gpu_test(args, rets);
-    else 
-#endif
-      rets[0].x = 2*args[0].x;
-  }
-
-#ifdef GNA_CUDA_SUPPORT
-  void gpu_test (Args args, Rets rets) {
-    rets[0].gpuArr->setByDeviceArray(args[0].gpuArr->devicePtr);
-    *(rets[0].gpuArr) *=15;
+  void gpu_test (FunctionArgs& fargs) {
+    //this.setEntryLocation(DataLocation::Device);
+    fargs.rets[0].gpuArr->setByDeviceArray(fargs.args[0].gpuArr->devicePtr);
+    *(fargs.rets[0].gpuArr) *=15;
     std::cout << "Dump: ";
-    rets[0].gpuArr->dump();
+    fargs.rets[0].gpuArr->dump();
   }
 #endif
   void dump(){
