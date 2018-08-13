bd3cb86f1aba56b44fe07ed071608c6746408134
Anna Fatkina <fatkina.a.i@gmail.com>
2018-08-13 10:34:16 +0300

    use ex test (will be moved into tests dir)

---
+++ test.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Test gpu"""
+
+from __future__ import print_function
+from matplotlib import pyplot as plt
+import numpy as N
+from load import ROOT as R
+from matplotlib.ticker import MaxNLocator
+import IPython
+
+def array_to_stdvector( array, dtype ):
+    """Convert an array to the std::vector<dtype>"""
+    ret = R.vector(dtype)( len( array ) )
+    for i, v in enumerate( array ):
+        ret[i] = v
+    return ret
+
+
+mat = N.ones(100, dtype='d')
+
+print( 'Input matrix (numpy)' )
+print( mat )
+print()
+
+lmat = mat.ravel( order='F' )
+shape = array_to_stdvector(mat.shape, 'size_t')
+
+points = R.Points( lmat, shape )
+points2 = R.Points( lmat, shape )
+
+identity = R.Identity()
+id2 = R.Identity()
+identity.identity.switchFunction("GPU")
+id2.identity.switchFunction("GPU")
+
+#IPython.embed()
+identity.identity.source( points.points.points )
+id2.identity.source(identity.identity.target)
+#res = id2.identity.target.data()
+
+
+b_identity = R.Identity()
+b_id2 = R.Identity()
+b_identity.identity.switchFunction("GPU")
+b_id2.identity.switchFunction("GPU")
+
+b_identity.identity.source( points2.points.points )
+b_id2.identity.source(b_identity.identity.target)
+#res = id2.identity.target.data()
+
+
+susu = R.Sum(False)
+susu.add(id2.identity.target)
+susu.add(b_id2.identity.target)
+
+from gna.graphviz import GNADot
+#kwargs=dict(
+           # splines='ortho'
+#           )
+#ts = [ susu.sum ]
+graph = GNADot( susu.sum )
+graph.write("dotfile.dot")
+
+bres = susu.data()
+print( bres )
+#IPython.embed()
+#print( 'Result (C++ Data to numpy)' )
+#print( points.points.points.data() )
+#print( res )
+#print( id2.identity.target.data() )
+#print()
+
