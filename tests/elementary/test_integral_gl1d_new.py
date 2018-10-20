f23bc00635f33afed9a4896279b26f357a44999c
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2018-10-20 06:43:39 +0300

    Update integrator test

--- tests/elementary/test_integral_gl1d_new.py
+++ tests/elementary/test_integral_gl1d_new.py
@@ -19,7 +19,7 @@ from matplotlib import pyplot as P
 from mpl_tools.helpers import plot_bar
 import numpy as N
 from load import ROOT as R
-from constructors import Points, Histogram
+from constructors import Points, Histogram, stdvector
 from gna.env import env
 from argparse import ArgumentParser
 from gna.parameters.printer import print_parameters
@@ -28,35 +28,48 @@ from mpl_tools import bindings
 """Parse arguments"""
 parser = ArgumentParser()
 parser.add_argument( '-o', '--order', type=int, default=3, help='integration order' )
-parser.add_argument( '-g', '--gauss', type=float, nargs=2, default=[6.0, 1.0], help='gaussian mean and sigma' )
-parser.add_argument( '-m', '--mean', type=float, help='gaussian mean' )
+# parser.add_argument( '-g', '--gauss', type=float, nargs=2, default=[6.0, 1.0], help='gaussian mean and sigma' )
 parser.add_argument( '-b', '--bins', type=float, nargs=3, default=[ 0.0, 12.001, 0.05 ], help='Bins: arange arguments (min, max, step)' )
-parser.add_argument( '-l', '--legend', default='upper left', help='legend location' )
+parser.add_argument( '-l', '--legend', default='upper right', help='legend location' )
 parser.add_argument( '--input-edges', action='store_true', help='pass edges as input' )
 parser.add_argument( '-M', '--mode', default='gl', choices=['gl', 'rect_left', 'rect', 'rect_right', 'trap'], help='integration mode' )
 parser.add_argument( '-d', '--dump', action='store_true', help='dump integrator' )
 parser.add_argument( '--dot', help='write graphviz output' )
 opts = parser.parse_args()
 
-mean, sigma = opts.gauss
 
 """Initialize the function and environment"""
+# mean, sigma = opts.gauss
 ns = env.globalns
-ns.defparameter( 'BackgroundRate', central=0.0,   sigma=0.1 )
-ns.defparameter( 'Mu',             central=1.0,   sigma=0.1 )
-ns.defparameter( 'E0',             central=mean,  sigma=0.1 )
-ns.defparameter( 'Width',          central=sigma, sigma=0.1 )
+# ns.defparameter( 'BackgroundRate', central=0.0,   sigma=0.1 )
+# ns.defparameter( 'Mu',             central=1.0,   sigma=0.1 )
+# ns.defparameter( 'E0',             central=mean,  sigma=0.1 )
+# ns.defparameter( 'Width',          central=sigma, sigma=0.1 )
+ns.defparameter( 'alpha', central=-0.5, sigma=0.1 )
 print_parameters( ns )
 
 # Initialize the function of interest
-fcn = R.GaussianPeakWithBackground()
-fcn.rate.setLabel('Gaussian\n(function to integrate)')
-# keep its output as variable
-output=fcn.rate.rate
+# fcn = R.GaussianPeakWithBackground()
+# fcn.rate.setLabel('Gaussian\n(function to integrate)')
+# # keep its output as variable
+# fcn_output=fcn.rate.rate
+# fcn_input=fcn.rate.E
+
+names = stdvector(('alpha',))
+fcn_we = R.WeightedSum(names, names)
+fcn_input=fcn_we.sum.alpha
+
+fcn_e = R.Exp()
+fcn_e.exp.points(fcn_we.sum.sum)
+fcn_output = fcn_e.exp.result
 
 """Initialize the integrator"""
 # create array with bin edges
 edges = N.arange(*opts.bins, dtype='d')
+
+alpha=ns['alpha'].value()
+a, b = edges[0], edges[-1]
+integral = (N.exp(alpha*b) - N.exp(alpha*a))/alpha
 
 # create 1d integrator (sample points) for given edges and integration order
 if opts.input_edges:
@@ -76,9 +89,9 @@ widths = edges[1:]-edges[:-1]
 
 """Make fake gaussian data"""
 # pass sample points as input to the function 'energy'
-fcn.rate.E(integrator.points.x)
+fcn_input(integrator.points.x)
 # pass the function output to the histogram builder (integrator)
-integrator.hist.f( output )
+integrator.hist.f( fcn_output )
 # read the histogram contents
 hist_output = integrator.hist.hist
 hist_output.setLabel('output histogram')
@@ -97,11 +110,11 @@ ax.minorticks_on()
 ax.grid()
 ax.set_xlabel( 'x' )
 ax.set_ylabel( 'f(x)' )
-ax.set_title( 'Integrate Gaussian (%g, %g) with order %i'%( mean, sigma, opts.order ) )
+# ax.set_title( 'Integrate Gaussian (%g, %g) with order %i'%( mean, sigma, opts.order ) )
 
 baropts = dict(alpha=0.5)
 # plot function versus sample points
-ax.plot( integrator.points.x.data(), fcn.rate.rate.data(), '-', label='function' )
+ax.plot( integrator.points.x.data(), fcn_output.data(), '-', label='function' )
 # plot histogram using GNA to matplotlib interface
 hist_output.plot_bar(label='histogram (sum=%g)'%hist.sum(), **baropts)
 # plot histogram manually
@@ -112,8 +125,8 @@ ax.legend(loc=opts.legend)
 
 # Our function of interest is a guassian and should give 1 when integrated
 # Test it by summing the histogram bins
-diff = hist.sum()-1
-print( 'Integral-1:', diff )
+diff = hist.sum()-integral
+print( 'Integral - %g:'%integral, diff )
 print( N.fabs(diff)<1.e-8 and '\033[32mIntegration is OK!' or '\033[31mIntegration FAILED!', '\033[0m' )
 
 if opts.dot:
