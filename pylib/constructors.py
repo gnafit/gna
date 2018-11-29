6eb4e40ed3ec8d43e0152ea5662f4c27dfba6b4b
Konstantin Treskov <treskov@jinr.ru>
2018-11-29 17:56:33 +0300

    Fix ctors

--- pylib/constructors.py
+++ pylib/constructors.py
@@ -18,41 +18,42 @@ def wrap_constructor1(obj, dtype='d'):
         return R.SegmentWise(array.size, array, *args, **kwargs)
     return method
 
-"""Construct Points object from numpy array"""
 def Points( array, *args, **kwargs ):
     """Convert array to Points"""
-    if len(array.shape)>2:
-        raise Exception( 'Can convert only 1- and 2- dimensional arrays' )
+
     a = N.ascontiguousarray(array, dtype='d').ravel( order='F' )
-    s = array_to_stdvector_size_t( array.shape )
+    if len(a.shape)>2:
+        raise Exception( 'Can convert only 1- and 2- dimensional arrays' )
+    s = array_to_stdvector_size_t( a.shape )
     return R.Points( a, s, *args, **kwargs )
 
-"""Construct Sum object from list of SingleOutputs"""
 def Sum(outputs=None, *args, **kwargs):
+    """Construct Sum object from list of SingleOutputs"""
     if outputs is None:
         return R.Sum(*args, **kwargs)
 
     outputs = stdvector(outputs, 'OutputDescriptor*')
     return R.Sum(outputs, *args, **kwargs)
 
-"""Construct Product object from list of SingleOutputs"""
 def Product(outputs=None, *args, **kwargs):
+    """Construct Product object from list of SingleOutputs"""
     if outputs is None:
         return R.Product(*args, **kwargs)
 
     outputs = stdvector(outputs, 'OutputDescriptor*')
     return R.Product(outputs, *args, **kwargs)
 
-"""Construct Bins object from numpy array"""
 def Bins( array, *args, **kwargs ):
-    """Convert array to Points"""
-    if len(array.shape)!=1:
-        raise Exception( 'Edges should be 1d array' )
+    """Construct Bins object from numpy array"""
+
     a = N.ascontiguousarray(array, dtype='d')
+    if len(a.shape)!=1:
+        raise Exception( 'Edges should be 1d array' )
     return R.Bins( a, a.size-1, *args, **kwargs )
 
-"""Construct Histogram object from two arrays: edges and data"""
 def Histogram( edges, data, *args, **kwargs ):
+    """Construct Histogram object from two arrays: edges and data"""
+
     edges = N.ascontiguousarray(edges, dtype='d')
     data  = N.ascontiguousarray(data,  dtype='d')
     if (edges.size-1)!=data.size:
@@ -60,8 +61,9 @@ def Histogram( edges, data, *args, **kwargs ):
 
     return R.Histogram( data.size, edges, data, *args, **kwargs )
 
-"""Construct Histogram2d object from two arrays: edges and data"""
 def Histogram2d( xedges, yedges, data, *args, **kwargs ):
+    """Construct Histogram2d object from two arrays: edges and data"""
+
     xedges = N.ascontiguousarray(xedges, dtype='d')
     yedges = N.ascontiguousarray(yedges, dtype='d')
     data   = N.ascontiguousarray(data,   dtype='d').ravel(order='F')
@@ -70,15 +72,15 @@ def Histogram2d( xedges, yedges, data, *args, **kwargs ):
 
     return R.Histogram2d( xedges.size-1, xedges, yedges.size-1, yedges, data, *args, **kwargs )
 
-"""Construct the GaussLegendre transformation based on bin edges and order(s)"""
 def GaussLegendre(edges, orders, *args, **kwargs):
+    """Construct the GaussLegendre transformation based on bin edges and order(s)"""
     edges = N.ascontiguousarray(edges, dtype='d')
     if not isinstance(orders, int):
         orders = N.ascontiguousarray(orders, dtype='i')
     return R.GaussLegendre(edges, orders, edges.size-1, *args, **kwargs)
 
-"""Construct Rebin object from array with edges"""
 def Rebin( edges, rounding, *args, **kwargs ):
+    """Construct Rebin object from array with edges"""
     if not isinstance( rounding, int ):
         raise Exception('Rebin rounding should be an integer')
     edges = N.ascontiguousarray(edges, dtype='d')
