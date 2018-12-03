9d67a5860b359d9ab4a57c8fa3fab7292743c9c2
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2018-12-03 17:25:48 +0300

    Add WeightedSum tutorial

--- macro/tutorial/variables/04_weightedsum.py
+++ macro/tutorial/variables/04_weightedsum.py
@@ -13,25 +13,27 @@ from gna.bindings import common
 ns = env.globalns
 
 # Create a parameter in the global namespace
-w1 = ns.defparameter('w1', central=1.0, free=True, label='weight 1')
-w2 = ns.defparameter('w2', central=0.1, free=True, label='weight 2')
-w3 = ns.defparameter('w3', central=0.1, free=True, label='weight 3')
+w1 = ns.defparameter('a', central=1.0, free=True, label='weight 1')
+w2 = ns.defparameter('b', central=0.1, free=True, label='weight 2')
+w3 = ns.defparameter('c', central=0.1, free=True, label='weight 3')
 
 # Print the list of parameters
 ns.printparameters(labels=True)
 print()
 
-# Create x input
+# Create x  and several functions to be added with weights
 x = np.linspace(-1.0*np.pi, 1.0*np.pi, 500)
 a1 = C.Points(np.sin(x))
 a2 = C.Points(np.sin(16.0*x))
 a3 = C.Points(np.cos(16.0*x))
-outputs = [a.points.points for a in (a1, a2, a2)]
+outputs = [a.points.points for a in (a1, a2, a3)]
 
-weights = ['w1', 'w2', 'w3']
+# Initialize the WeightedSum with list of variables and list of outputs
+weights = ['a', 'b', 'c']
 wsum = C.WeightedSum(weights, outputs)
 wsum.print()
 
+# Do some plotting
 fig = plt.figure()
 ax = plt.subplot( 111 )
 ax.minorticks_on()
@@ -40,23 +42,29 @@ ax.set_xlabel( 'x' )
 ax.set_ylabel( 'f(x)' )
 ax.set_title( r'$a\sin(x)+b\sin(16x)+c\cos(16x)$' )
 
-label = 'a={}, b={}, c={}'.format(w1.value(), w2.value(), w2.value())
+label = 'a={}, b={}, c={}'.format(w1.value(), w2.value(), w3.value())
+wsum.sum.sum.plot_vs(x, label=label)
+
+w2.push(0.0)
+w3.push(0.0)
+label = 'a={}, b={}, c={}'.format(w1.value(), w2.value(), w3.value())
 wsum.sum.sum.plot_vs(x, label=label)
 
 w1.push(0.0)
-label = 'a={}, b={}, c={}'.format(w1.value(), w2.value(), w2.value())
-wsum.sum.sum.plot_vs(x, label=label)
-
-w1.pop()
-w2.push(0.0)
-label = 'a={}, b={}, c={}'.format(w1.value(), w2.value(), w2.value())
-wsum.sum.sum.plot_vs(x, label=label)
-
 w2.pop()
-w3.push(0.0)
-label = 'a={}, b={}, c={}'.format(w1.value(), w2.value(), w2.value())
+label = 'a={}, b={}, c={}'.format(w1.value(), w2.value(), w3.value())
 wsum.sum.sum.plot_vs(x, label=label)
 
-ax.legend(loc='upper right')
+w2.push(0.0)
+w3.pop()
+label = 'a={}, b={}, c={}'.format(w1.value(), w2.value(), w3.value())
+wsum.sum.sum.plot_vs(x, label=label)
+
+ax.legend(loc='lower right')
+
+from mpl_tools.helpers import savefig
+from sys import argv
+oname = argv[0].rsplit('/', 1).pop().replace('.py', '.png')
+savefig('output/tutorial/'+oname)
 
 plt.show()
