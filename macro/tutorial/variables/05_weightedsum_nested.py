73c6614f3fa12b577e56f0d89ffae6088bc85197
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2018-12-03 17:39:54 +0300

    minor

---
+++ macro/tutorial/variables/05_weightedsum_nested.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import load
+from gna.env import env
+import constructors as C
+import numpy as np
+from matplotlib import pyplot as plt
+from gna.bindings import common
+
+# Make a variable for global namespace
+ns = env.globalns
+
+# Create a parameter in the global namespace
+ns.defparameter('a', central=1.0,  free=True, label='weight 1 (global)')
+ns.defparameter('b', central=-0.1, free=True, label='weight 2 (global)')
+ns.defparameter('subst.a', central=0.5,  free=True, label='weight 1 (local)')
+ns.defparameter('subst.b', central=0.00, free=True, label='weight 2 (local)')
+ns.defparameter('subst.c', central=0.05, free=True, label='weight 3 (local)')
+
+# Print the list of parameters
+ns.printparameters(labels=True)
+print()
+
+# Create x  and several functions to be added with weights
+x = np.linspace(-1.0*np.pi, 1.0*np.pi, 500)
+a1 = C.Points(np.sin(x))
+a2 = C.Points(np.sin(16.0*x))
+a3 = C.Points(np.cos(16.0*x))
+outputs = [a.points.points for a in (a1, a2, a3)]
+
+# Initialize the WeightedSum with list of variables and list of outputs
+weights1 = ['a', 'b', 'subst.c']
+wsum1 = C.WeightedSum(weights1, outputs)
+
+weights2 = ['a', 'b', 'c']
+with ns('subst'):
+    wsum2 = C.WeightedSum(weights2, outputs)
+
+# Do some plotting
+fig = plt.figure()
+ax = plt.subplot( 111 )
+ax.minorticks_on()
+ax.grid()
+ax.set_xlabel( 'x' )
+ax.set_ylabel( 'f(x)' )
+ax.set_title( r'$a\sin(x)+b\sin(16x)+c\cos(16x)$' )
+
+wsum1.sum.sum.plot_vs(x, label='Weighted sum 1')
+wsum2.sum.sum.plot_vs(x, label='Weighted sum 2')
+
+ax.legend(loc='lower right')
+
+from mpl_tools.helpers import savefig
+from sys import argv
+oname = argv[0].rsplit('/', 1).pop().replace('.py', '.png')
+savefig('output/tutorial/'+oname)
+
+plt.show()
