d82511b53fd7c4f9974ac1ffa285abeae28a46fd
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2018-12-05 12:10:04 +0300

    minor

--- macro/tutorial/complex/01_integral1d.py
+++ macro/tutorial/complex/01_integral1d.py
@@ -7,6 +7,7 @@ from gna.env import env
 import constructors as C
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.gridspec as gridspec
 from gna.bindings import common
 import ROOT as R
 
@@ -14,8 +15,8 @@ import ROOT as R
 ns = env.globalns
 
 # Create a parameter in the global namespace
-p1 = ns.defparameter('a', central=1.0, free=True, label='weight 1')
-p2 = ns.defparameter('b', central=0.2, free=True, label='weight 2')
+pa = ns.defparameter('a', central=1.0, free=True, label='weight 1')
+pb = ns.defparameter('b', central=0.2, free=True, label='weight 2')
 pk = ns.defparameter('k', central=8.0, free=True, label='argument scale')
 
 # Print the list of parameters
@@ -59,22 +60,50 @@ sin_t.sin.setLabel('sin(x)')
 cos_t.cos.setLabel('cos(kx)')
 fcn.sum.setLabel('a sin(x) + b cos(kx)')
 
+# Compute the integral analytically as a cross check
+# int| a*sin(x) + b*cos(k*x) = a*cos(x) - b/k*sin(kx) + C
+a, b, k = (p.value() for p in (pa, pb, pk))
+F = - a*np.cos(x_edges) + (b/k)*np.sin(k*x_edges)
+integral_a = F[1:] - F[:-1]
+bar_width = (x_edges[1:]-x_edges[:-1])*0.5
+bar_left = x_edges[:-1]+bar_width
+
+integral_n = integrator.hist.hist.data()
+
 # Do some plotting
 fig = plt.figure()
-ax = plt.subplot( 111 )
+gs = gridspec.GridSpec(nrows=4, ncols=1, hspace=0.0)
+ax = plt.subplot(gs[:3,0])
 ax.minorticks_on()
 # ax.grid()
-ax.set_xlabel( 'x' )
 ax.set_ylabel( 'f(x)' )
 ax.set_title(r'$a\,\sin(x)+b\,\sin(kx)$')
 
-fcn.sum.sum.plot_vs(int_points, 'o-', label='function', alpha=0.5, markerfacecolor='none')
-integrator.hist.hist.plot_bar(label='hist')
+# Draw the function and integrals
+fcn.sum.sum.plot_vs(int_points, 'o-', label='function at integration points', alpha=0.5, markerfacecolor='none', markersize=4.0)
+integrator.hist.hist.plot_bar(label='integral: numerical', divide=2, shift=0)
+ax.bar(bar_left, integral_a, bar_width, label='integral: analytical', align='edge')
 
-ax.vlines(integrator.points.xedges.data(), -2, 2, linestyle='--', alpha=0.4, linewidth=0.5)
-ax.set_ylim(-1.5, 1.5)
+# Freeze axis limits and draw bin edges
+ax.autoscale(enable=False, axis='y')
+ymin, ymax = ax.get_ylim()
+ax.vlines(integrator.points.xedges.data(), ymin, ymax, linestyle='--', alpha=0.4, linewidth=0.5)
 
 ax.legend(loc='lower right')
+
+# Add difference
+ax = plt.subplot(gs[3,0], sharex=ax)
+ax.set_xlabel('x')
+diff_factor=1.e-7
+ax.set_ylabel(r'diff., $\times10^{-7}$')
+
+diff = integral_n-integral_a
+ax.bar(x_edges[:-1], diff/diff_factor, bar_width*2.0, align='edge')
+
+# Freeze axis limits and draw bin edges
+ax.autoscale(enable=False, axis='y')
+ymin, ymax = ax.get_ylim()
+ax.vlines(integrator.points.xedges.data(), ymin, ymax, linestyle='--', alpha=0.4, linewidth=0.5)
 
 from mpl_tools.helpers import savefig
 from sys import argv
