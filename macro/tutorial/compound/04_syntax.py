2e145af58a772147a2d02aad277957a731ec7e79
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2018-12-13 15:26:10 +0300

    Add tutorial on binding syntax

--- macro/tutorial/compound/04_syntax.py
+++ macro/tutorial/compound/04_syntax.py
@@ -15,45 +15,45 @@ print(p0.single())
 
 s1 = C.Sum()
 s1.add_input('first')
-print('Default binding syntax')
+# Default binding syntax
 s1.sum.inputs[0].connect(p0.points.outputs[0])
 
 s2 = C.Sum()
-print('Siplified access')
+# Siplified access
 s2.add_input('first')
 s2.single_input().connect(s1.single())
 
 s3 = C.Sum()
 s3.add_input('first')
-print('Even more simpler way')
+# Even more simpler way
 s2.sum.sum >> s3.sum.first
 
 s4 = C.Sum()
 s4.add_input('first')
-print('Even more simpler way')
-s3.sum >> s4.sum
+# The other direction
+s4.sum.first << s3.sum.sum
 
 s5 = C.Sum()
 s5.add_input('first')
-print('Even more simpler way')
+# Even more simpler way
+s4.sum >> s5.sum
+
+s5 = C.Sum()
+s5.add_input('first')
+# Even more simpler way
 s4 >> s5
-
-s5 = C.Sum()
-s5.add_input('first')
-print('The other direction')
-s5 << s4
 
 s6 = C.Sum()
 s6.add_input('first')
 s6.add_input('second')
-print('Bind a single output to two inputs')
+# Bind a single output to two inputs
 s5 >> s6.sum.inputs.values()
 
 s7 = C.Sum()
 s7.add_input('first')
 s7.add_input('second')
-print('The other direction')
-s6 >> (s6.sum.inputs.first, s6.sum.inputs.second)
+# Bind a single output to two inputs (explicity)
+s6 >> (s7.sum.inputs.first, s7.sum.inputs.second)
 
 result = s7.single().data()[0]
 
