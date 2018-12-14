822d422f7bd06ca4a5aa298723cd4762fa00b973
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2018-12-14 15:14:58 +0300

    Add RebinnerN that rebins histogram by joining each N bins
    fixes #31

---
+++ tests/elementary/test_rebinnern.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from matplotlib import pyplot as P
+import numpy as N
+from load import ROOT as R
+from gna.env import env
+from mpl_tools.helpers import savefig, plot_hist, add_colorbar
+from converters import convert
+from argparse import ArgumentParser
+import constructors as C
+
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument( '-n', '--njoin', type=int, default=2)
+args = parser.parse_args()
+
+edges   = N.linspace(0, 10, 17, dtype='d')
+
+ntrue = C.Histogram(edges, N.ones(edges.size-1) )
+rebin = R.RebinN(args.njoin)
+ntrue >> rebin
+
+olddata = ntrue.single().data()
+newdata = rebin.single().data()
+olddatatype = ntrue.single().datatype()
+newdatatype = rebin.single().datatype()
+
+print('Old data:', olddata, olddatatype)
+print('New data:', newdata, newdatatype)
