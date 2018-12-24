ba839325b8eeecd30f2bf5abefe5054273b499dd
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2018-12-24 15:26:18 +0300

    minor

--- tests/neutrino/OscillationVariables.py
+++ tests/neutrino/OscillationVariables.py
@@ -15,37 +15,49 @@ ns = env.globalns
 ns.defparameter('L', central=1, sigma=0.1)
 
 from gna.parameters.oscillation import reqparameters
+print('Initialize variables')
 reqparameters(ns)
 ns['Delta'].set(N.pi*1.5)
+print('Materialize expressions')
 env.globalns.materializeexpressions()
 
-print('Variables before:')
+print('Status:')
 env.globalns.printparameters(labels=True)
 print()
 
 if opts.pmnsc:
+    print('Initialize variables (C)')
+
     checkpmns = ns('checkpmns')
     exprs = R.PMNSExpressionsC(ns=checkpmns)
-    print('materialize expressions')
+    print('Materialize expressions')
     checkpmns.materializeexpressions()
 
 ns1 = ns('weights_ae_ae')
-ns1.materializeexpressions()
+print('Init OP ae-ae')
 with ns1:
     re1=R.OscProbPMNSExpressions(R.Neutrino.ae(), R.Neutrino.ae(), ns=ns1)
+print('Materialize expresions')
+ns1.materializeexpressions()
 # w12 = 2 sqr c12*s12*c13*c13
 # w13 = 2 sqr c12*c13*s13
 # w23 = 2 sqr s12*c13*s13
 
 ns2 = ns('weights_mu_e')
-ns2.materializeexpressions()
+print('Init OP mu-e')
 with ns2:
     re2=R.OscProbPMNSExpressions(R.Neutrino.mu(), R.Neutrino.e(), ns=ns2)
+print('Materialize expresions')
+ns2.materializeexpressions()
 
 ns3 = ns('weights_amu_ae')
-ns3.materializeexpressions()
+print('Init OP amu-ae')
 with ns3:
     re3=R.OscProbPMNSExpressions(R.Neutrino.amu(), R.Neutrino.ae(), ns=ns3)
+print('Materialize expresions')
+ns3.materializeexpressions()
 
+print()
+print('Print final')
 ns.printparameters(labels=True)
 
