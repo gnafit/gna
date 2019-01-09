a31797f977fdca0aa0e317b59ca2bf968589b2c5
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-01-09 17:34:08 +0300

    Split old and new bundles

--- config/gna/gnacfg.py
+++ config/gna/gnacfg.py
@@ -1,6 +1,6 @@
 # Paths to look for UI modules
 pkgpaths    = [ './pylib/gna/ui' ]
-bundlepaths = [ './pylib/gna/bundle' ]
+bundlepaths = [ './pylib/gna/bundles', './pylib/gna/bundles_legacy' ]
 covariance_path = ['./tests/covariances']
 
 # Verbosity options
