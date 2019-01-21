2d10e5d27c4f5d40028e8ab3be6070d18467f332
Anna Fatkina <fatkina.a.i@gmail.com>
2019-01-21 15:23:24 +0300

    minor

--- cuda/operations/cuOperations.cuh
+++ cuda/operations/cuOperations.cuh
@@ -5,6 +5,11 @@
 #include <stdio.h>
 
 
+
+/* Inverse template function.
+ * Float and double are availible.
+ * @return 1/x
+ */
 template <typename T> 
 __device__ void inverse(T* in, T* out);
 
@@ -12,12 +17,20 @@ __device__ void inverse(T* in, T* out);
 template<>
 __device__ void inverse <float> (float* in, float* out) {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	if (in[idx] != 0) out[idx] =  __frcp_rn(in[idx]); // TODO check if out == 0 by default
+	out[idx] =  __frcp_rn(in[idx]); // TODO check if out == 0 by default
 }
 
 template <>
 __device__ void inverse <double> (double* in, double* out) {
         int idx = blockIdx.x * blockDim.x + threadIdx.x;
-        if (in[idx] != 0) out[idx] =  __drcp_rn(in[idx]); // TODO check if out == 0 by default
+        __drcp_rn(in[idx]); // TODO check if out == 0 by default
 }
 
+/* ************************************ */
+
+
+template <typename T>
+__device__ void prodNumToVec(T k, T* x, T* res) {
+        int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	
+}
