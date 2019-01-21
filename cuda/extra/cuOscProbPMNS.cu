0e6ef4825c0f1eb368b320c7ae2ca4c974608e08
Anna Fatkina <fatkina.a.i@gmail.com>
2019-01-21 17:21:59 +0300

    minor

--- cuda/extra/cuOscProbPMNS.cu
+++ cuda/extra/cuOscProbPMNS.cu
@@ -10,32 +10,13 @@
 #include "cuOperations.cuh" 
 #include <cuda.h>
 
-/* TMP */
-
-#define m_L 1.1
-#define oscprobArgumentFactor 1.1
-
-template<>
-double OscProbPMNSBase::DeltaMSq<1,2>() const {return 0.1; }
-
-template<>
-double OscProbPMNSBase::DeltaMSq<1,3>() const {return 0.1; }
-
-template<>
-double OscProbPMNSBase::DeltaMSq<2,3>() const {return 0.1; }
-
-
-/* TMP END */
-
-
-
 
 template <int I, int J, typename T>
 __global__ void cuCalcComponent(T* xarg, T* xret, T* intern); 
 // TODO define inern as array of the same as xarg size from internals
 
 
-template <int I, int J>
+template <>
 __global__ void cuCalcComponent(float* xarg, float* xret, float* intern) { 
 	inverse(xarg, intern);
 	prodNumToVec (cosf(DeltaMSq<I,J>() * oscprobArgumentFactor * m_L * 0.5f), 
@@ -44,7 +25,7 @@ __global__ void cuCalcComponent(float* xarg, float* xret, float* intern) {
 }
 
 
-template <int I, int J>
+template <>
 __global__ void cuCalcComponent(double* xarg, double* xret, double* intern) { 
 	inverse(xarg, intern);
 	prodNumToVec (cosf(DeltaMSq<I,J>() * oscprobArgumentFactor * m_L * 0.5), 
