8582f113999ca7da196a7cee5edfa663c6490c6d
Anna Fatkina <fatkina.a.i@gmail.com>
2019-01-21 17:57:20 +0300

    sin funcs on vector

--- cuda/operations/cuOperations.cuh
+++ cuda/operations/cuOperations.cuh
@@ -37,3 +37,45 @@ __device__ void prodNumToVec(T k, T* x, T* res) {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	res[idx] = k*x[idx];
 }
+
+
+/* 
+ * Sin of k*x
+ *
+ */
+
+template <typename T>
+__device__ arr_sin (T k, T* x, T* out);
+
+template <>
+__device__ arr_sin (double k, double* x, double* out) {
+        int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	out[idx] = sin(k*x[idx]);
+} 
+
+template <>
+__device__ arr_sin (float k, float* x, float* out) {
+        int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	out[idx] = sinf(k*x[idx]);
+} 
+
+/*
+ * Multiply myself to sin(k*x)
+ */
+
+template <typename T>
+__device__ mult_by_arr_sin (T k, T* x, T* me);
+
+template <>
+__device__ mult_by_arr_sin (float k, float* x, float* me) {
+        int idx = blockIdx.x * blockDim.x + threadIdx.x;
+        out[idx] = me[idx] * sinf(k*x[idx]);
+}
+
+
+template <>
+__device__ mult_by_arr_sin (double k, double* x, double* me) {
+        int idx = blockIdx.x * blockDim.x + threadIdx.x;
+        out[idx] = me[idx] * sin(k*x[idx]);
+}
+
