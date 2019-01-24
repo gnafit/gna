24e86ae201f7009c45539ab946468952cb007593
Anna Fatkina <fatkina.a.i@gmail.com>
2019-01-24 17:14:05 +0300

    last

--- cuda/core/GpuBasics.hh
+++ cuda/core/GpuBasics.hh
@@ -3,10 +3,10 @@
 //extern "C" 
 
 template<typename T>
-void copyH2D(T** dst, T** src, int N);
+void copyH2D(T* dst, T* src, int N);
 
 template<typename T>
-void copyH2D_NOALL(T** dst, T** src, int N);
+void copyH2D_NOALL(T* dst, T* src, int N);
 
 template<typename T>
 void cuwr_free(T* ptr); 
