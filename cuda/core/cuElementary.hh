25c0b3d9d5d4b1d04d5dd8954658a83d379835a8
Anna Fatkina <fatkina.a.i@gmail.com>
2019-01-31 15:28:27 +0300

    minor

--- cuda/core/cuElementary.hh
+++ cuda/core/cuElementary.hh
@@ -2,3 +2,6 @@
 
 template<typename T>
 void cusum(T** in, T** out, unsigned int N, unsigned int M);
+
+template<typename T>
+void identity(T** in, T** out, unsigned int N);
--- tests/gpu/Sum_ongpu.py
+++ tests/gpu/Sum_ongpu.py
@@ -12,22 +12,26 @@ from gna.env import env
 """Initialize inpnuts"""
 arr1 = N.arange(1, 1000000)
 arr2 = arr1
+arr3 = arr1
 print( 'Data1:', arr1 )
 print( 'Data2:', arr2 )
+print( 'Data3:', arr3 )
 
-labels = [ 'arr1', 'arr2' ]
-weights = [ 'w1', 'w2' ]
+labels = [ 'arr1', 'arr2', 'arr3' ]
+weights = [ 'w1', 'w2', 'w3' ]
 
 """Initialize environment"""
 
 """Initialize transformations"""
 points1 = Points( arr1 )
 points2 = Points( arr2 )
+points3 = Points( arr3 )
 
 """Mode1: a1+a2"""
 ws = R.Sum()
 ws.add(points1.points)
 ws.add(points2.points)
+ws.add(points3.points)
 
 ws.sum.switchFunction("gpu")
 print( 'Mode1: ' )
