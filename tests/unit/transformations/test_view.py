ebf61b6e66adfc0691f69533efcd0e3a5c04f3dc
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-03-28 16:09:17 +0300

    Add view (histogram)

--- tests/unit/transformations/test_view.py
+++ tests/unit/transformations/test_view.py
@@ -32,6 +32,29 @@ def test_view_01():
         print()
         assert (res==pview).all()
 
+@floatcopy(globals())
+def test_view_02():
+    arr = N.arange(13, dtype='d')
+    points = C.Histogram(arr, arr[:-1])
+
+    ranges = [ (0, 3), (0, 12), (1, 3), (6, 6), (6, 1)]
+    for rng in ranges:
+        start, len = rng
+        pview  = arr[start:start+len]
+        pedges = arr[start:start+len+1]
+        view = C.View(points, start, len);
+
+        res = view.view.view.data()
+        edges = N.array(view.view.view.datatype().edges)
+        print('Range', rng)
+        print('Result', res)
+        print('Expect', pview)
+        print('Result (edges)', edges)
+        print('Expect (edges)', pedges)
+        print()
+        assert (res==pview).all()
+        assert (edges==pedges).all()
+
 if __name__ == "__main__":
     run_unittests(globals())
 
