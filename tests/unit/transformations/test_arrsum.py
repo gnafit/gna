ae4d12710b3a7078ccca81ccdc795d7a5953d874
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-03-28 12:34:36 +0300

    Make ArrSum a template

--- tests/unit/transformations/test_arrsum.py
+++ tests/unit/transformations/test_arrsum.py
@@ -6,27 +6,29 @@ import numpy as np
 from load import ROOT as R
 from gna.env import env
 from gna.parameters.printer import print_parameters
-from gna.converters import convert
+from gna import constructors as C
+from gna.unittest import *
 
-def test_arrsum():
+@floatcopy(globals(), addname=True)
+def test_arrsum(function_name):
     varname = 'out'
-    ns = env.globalns
+    ns = env.globalns(function_name)
     names = ["var1", "var2", "var3", "var4"]
     variables = [ns.reqparameter(name, central=float(i), relsigma=0.1)
                 for i, name in enumerate(names)]
 
-    cpp_names = convert(names, 'stdvector')
-    var_arr = R.VarArray(cpp_names)
+    with ns:
+        var_arr = C.VarArray(names)
     print("Input var array ", var_arr.vararray.points.data())
 
-    sum_arr = R.ArraySum(varname, var_arr, ns=ns)
+    sum_arr = C.ArraySum(varname, var_arr, ns=ns)
     # materialize variable
     ns[varname].get()
     output = var_arr.vararray.points
     print('Data:', output.data(), output.data().sum())
     print("Value of %s evaluable immediately after initialization "%varname, ns[varname].value(), sum_arr.arrsum.sum.data())
     print()
-    assert np.allclose(output.data().sum(), ns[varname].value())
+    assert (output.data().sum()==ns[varname].value()).all()
 
     #  sum_arr.arrsum.arr(var_arr.vararray)
     #  sum_arr.exposeEvaluable(var_arr.vararray)
@@ -37,10 +39,10 @@ def test_arrsum():
     ns[varname].dump()
     print("Sum should now be ", np.sum(var_arr.vararray.points.data()))
     print("Check the value %s of evaluable now: "%varname, ns['out'].value(), sum_arr.arrsum.sum.data())
-    assert np.allclose(output.data().sum(), ns[varname].value())
+    assert (output.data().sum()==ns[varname].value()).all()
     print()
 
     env.globalns.printparameters()
 
 if __name__ == "__main__":
-    test_arrsum()
+    run_unittests(globals())
