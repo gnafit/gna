a5dc8547bcbf8448c116eb5908b1050e3d11dcca
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-04-02 16:59:28 +0300

    Clean PartialSum

--- tests/unit/transformations/test_partial_sum.py
+++ tests/unit/transformations/test_partial_sum.py
@@ -4,19 +4,18 @@
 from __future__ import print_function
 from __future__ import division
 from matplotlib import pyplot as plt
-import numpy as np 
+import numpy as np
 from load import ROOT as R
 from matplotlib.ticker import MaxNLocator
 import gna.constructors as C
 from gna.bindings import DataType
+from gna.unittest import *
 
 def arithmetic_progression(sequence):
     assert np.allclose(sequence[1:] - sequence[:-1], sequence[1]-sequence[0])
     return np.array([(i+1)*(upper + sequence[0])/2 for i, upper in enumerate(sequence)])
-#
-# Create the matrix
-#
-def test_partial_sum_hist():
+
+def test_partial_sum_hist_01():
     arr = np.arange(1, 13, dtype='d')
     edges = np.arange(0, 13, dtype='d')
     print( 'Input sequence' )
@@ -25,7 +24,7 @@ def test_partial_sum_hist():
     # Create transformations
     #
     points = C.Histogram(edges, arr)
-    partial_sum = R.PartialSum(0.)
+    partial_sum = R.PartialSum()
     partial_sum.reduction << points
 
     res = partial_sum.reduction.data()
@@ -34,9 +33,29 @@ def test_partial_sum_hist():
     print(expected)
     print("Results from C++")
     print(res)
-    assert np.allclose(expected, res)
+    assert (expected==res).all()
 
-def test_partial_sum_hist_with_start():
+def test_partial_sum_hist_02():
+    arr = np.arange(1, 13, dtype='d')
+    edges = np.arange(0, 13, dtype='d')
+    print( 'Input sequence' )
+    print(arr)
+    #
+    # Create transformations
+    #
+    points = C.Histogram(edges, arr)
+    partial_sum = R.PartialSum(0)
+    partial_sum.reduction << points
+
+    res = partial_sum.reduction.data()
+    expected = np.hstack(([0.0], arithmetic_progression(arr)))
+    print("Expected")
+    print(expected)
+    print("Results from C++")
+    print(res)
+    assert (expected==res).all()
+
+def test_partial_sum_hist_03():
     arr = np.arange(1, 13, dtype='d')
     edges = np.arange(0, 13, dtype='d')
     validation = np.arange(3, 13, dtype='d')
@@ -46,7 +65,7 @@ def test_partial_sum_hist_with_start():
     # Create transformations
     #
     points = C.Histogram(edges, arr)
-    partial_sum = R.PartialSum(2.)
+    partial_sum = R.PartialSum(2., True)
     partial_sum.reduction << points
 
     res = partial_sum.reduction.data()
@@ -56,9 +75,9 @@ def test_partial_sum_hist_with_start():
     print(expected)
     print("Results from C++")
     print(res)
-    assert np.allclose(expected, res)
+    assert (expected==res).all()
 
-def test_partial_sum_points():
+def test_partial_sum_points_01():
     arr = np.arange(0, 13, dtype='d')
     validation = arr
     #  validation = np.arange(2, 13, dtype='d')
@@ -69,7 +88,7 @@ def test_partial_sum_points():
     # Create transformations
     #
     points = C.Points(arr)
-    partial_sum = R.PartialSum(0.)
+    partial_sum = R.PartialSum()
     partial_sum.reduction << points
 
     res = partial_sum.reduction.data()
@@ -78,32 +97,28 @@ def test_partial_sum_points():
     print(expected)
     print("Results from C++")
     print(res)
-    assert np.allclose(expected, res)
+    assert (expected==res).all()
 
-def test_partial_sum_points_with_start():
-    arr = np.arange(0, 13, dtype='d')
-    validation = np.arange(2, 13, dtype='d')
+def test_partial_sum_points_02():
+    arr = np.arange(1, 13, dtype='d')
     print( 'Input sequence' )
     print(arr)
     #
     # Create transformations
     #
     points = C.Points(arr)
-    partial_sum = R.PartialSum(2.)
+    partial_sum = R.PartialSum(-1)
     partial_sum.reduction << points
 
     res = partial_sum.reduction.data()
-    expected = arithmetic_progression(validation)
-    expected = np.concatenate((np.zeros(2), expected))
+    expected = arithmetic_progression(arr)
+    expected = np.concatenate(([-1], expected-1))
     print("Expected")
     print(expected)
     print("Results from C++")
     print(res)
-    assert np.allclose(expected, res)
+    assert (expected==res).all()
 
 if __name__ == "__main__":
-    glb = globals()
-    for fcn in sorted([name for name in glb.keys() if name.startswith('test_')]):
-        print('\nRun ', fcn)
-        glb[fcn]()
+    run_unittests(globals())
 
--- transformations/sum/PartialSum.cc
+++ transformations/sum/PartialSum.cc
@@ -1,11 +1,36 @@
 #include "PartialSum.hh"
+#include "TypeClasses.hh"
 #include <numeric>
 #include <fmt/ostream.h>
 
-PartialSum::PartialSum(double starting_value): m_starting_value{starting_value} {
+PartialSum::PartialSum() {
+    init();
+}
+
+PartialSum::PartialSum(double initial_value):
+m_initial_value(initial_value)
+{
+    init();
+}
+
+PartialSum::PartialSum(double initial_value, double threshold):
+m_threshold(threshold),
+m_initial_value(initial_value)
+{
+    init();
+}
+
+PartialSum::PartialSum(double threshold, bool):
+m_threshold(threshold)
+{
+    init();
+}
+
+void PartialSum::init() {
     transformation_("reduction")
         .input("inp")
         .output("out")
+        .types(new TypeClasses::CheckNdimT<double>(1))
         .types( &PartialSum::make_Points,
                 &PartialSum::findIdx
             )
@@ -13,40 +38,46 @@ PartialSum::PartialSum(double starting_value): m_starting_value{starting_value} 
 }
 
 void PartialSum::make_Points(TypesFunctionArgs targs) {
-    if (targs.args[0].kind == DataKind::Points) {
-        targs.rets[0] = targs.args[0];
-    } else {
-        auto size = targs.args[0].hist().bins();
-        targs.rets[0] = DataType().points().shape(size);
-    };
+    size_t size=targs.args[0].size();
+    if(m_initial_value){
+        ++size;
+    }
+    targs.rets[0] = DataType().points().shape(size);
 }
 
 void PartialSum::calc(FunctionArgs fargs) {
     auto& inputs = fargs.args[0];
     auto* input = inputs.buffer;
     auto  size = inputs.x.size();
-    auto* output = fargs.rets[0].buffer;
+    auto& ret=fargs.rets[0];
+    auto* output = ret.buffer;
 
-    if (__builtin_expect(m_idx<0, 0)) {
-        findStartingPoint(input, input + size);
+    if(m_initial_value)
+    {
+        std::fill(output, output+m_idx+1, m_initial_value.value());
+        std::partial_sum(input+m_idx, input+size, output+m_idx+1);
+        ret.x.tail(size-m_idx)+=m_initial_value.value();
     }
-
-    std::fill(output, output+m_idx, 0.);
-    std::partial_sum(input+m_idx, input+size, output+m_idx);
+    else{
+        std::fill(output, output+m_idx, 0.0);
+        std::partial_sum(input+m_idx, input+size, output+m_idx);
+    }
 }
 
 void PartialSum::findIdx(TypesFunctionArgs targs) {
     if (targs.args[0].kind == DataKind::Points) {
-        this->m_idx = -1;
+        this->m_idx = 0;
         return;
     }
     const auto& edges = targs.args[0].edges;
-    findStartingPoint(std::begin(edges), std::end(edges));
-} 
+    if(this->m_threshold){
+        findStartingPoint(std::begin(edges), std::end(edges));
+    }
+}
 
 template <typename InputIterator>
 void PartialSum::findStartingPoint(InputIterator start, InputIterator end) {
-    auto above_starting = std::lower_bound(start, end, m_starting_value);
+    auto above_starting = std::lower_bound(start, end, m_threshold.value());
     if (above_starting == end) {
         throw std::runtime_error("Starting value is larger then any bin in container");
     }
