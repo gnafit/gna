347e679dedc1086c6dc583303efa5e7113eaec6c
Anna Fatkina <fatkina.a.i@gmail.com>
2019-04-04 17:31:51 +0300

    minor

--- tests/gpu/OscProb_test.py
+++ tests/gpu/OscProb_test.py
@@ -34,35 +34,38 @@ with ns:
     #Vacuum neutrino (same antineutrino)
     oscprob = ROOT.OscProbPMNS(from_nu, to_nu)
     oscprob.comp12.inputs.Enu(E)
-#    oscprob.comp12.switchFunction("gpu")
+    oscprob.comp12.switchFunction("gpu")
     data_osc = oscprob.comp12.comp12
 
     oscprob.comp13.inputs.Enu(E)
- #   oscprob.comp13.switchFunction("gpu")
+    oscprob.comp13.switchFunction("gpu")
     data_osc2 = oscprob.comp13.comp13
 
     oscprob.comp23.inputs.Enu(E)
-  #  oscprob.comp23.switchFunction("gpu")
+    oscprob.comp23.switchFunction("gpu")
     data_osc3 = oscprob.comp23.comp23
 
-    oscprob.compCP.inputs.Enu(E)
+#    oscprob.compCP.inputs.Enu(E)
 #    oscprob.compCP.switchFunction("gpu")
-    data_osc4 = oscprob.compCP.compCP
+#    data_osc4 = oscprob.compCP.compCP
 
-    oscprob.probsum.inputs.comp12(oscprob.comp12.comp12)
-    oscprob.probsum.inputs.comp13(oscprob.comp13.comp13)
-    oscprob.probsum.inputs.comp23(oscprob.comp23.comp23)
-    oscprob.probsum.inputs.comp0(com.points.points)
-    datafin = oscprob.probsum.probsum
+#    oscprob.probsum.inputs.comp12(oscprob.comp12.comp12)
+#    oscprob.probsum.inputs.comp13(oscprob.comp13.comp13)
+#    oscprob.probsum.inputs.comp23(oscprob.comp23.comp23)
+#    oscprob.probsum.inputs.comp0(com.points.points)
+#    datafin = oscprob.probsum.probsum
     
-
+    
+#    oscprob = NestedDict(
+#              bundle = dict(name='oscprob', version='v02', major='rdc'),
+#              ),
 
 #print(data_osc)
-#plt.plot(E_arr*1e-3, data_osc.data())
-#plt.plot(E_arr*1e-3, data_osc2.data())
-#plt.plot(E_arr*1e-3, data_osc3.data())
+plt.plot(E_arr*1e-3, data_osc.data())
+plt.plot(E_arr*1e-3, data_osc2.data())
+plt.plot(E_arr*1e-3, data_osc3.data())
 #plt.plot(E_arr*1e-3, data_osc4.data())
-plt.plot(E_arr*1e-3, datafin.data())
+#plt.plot(E_arr*1e-3, datafin.data())
 plt.xlabel('$comp12$')
 plt.ylabel(r'$P_{\nu_{\mu} \to \nu_{e}}$')
 plt.grid()
--- transformations/neutrino/OscProbPMNS.hh
+++ transformations/neutrino/OscProbPMNS.hh
@@ -6,51 +6,58 @@
 #include "Neutrino.hh"
 #include "config_vars.h"
 
+
 class OscillationVariables;
 class PMNSVariables;
 class OscProbPMNSBase: public GNAObject,
                        public TransformationBind<OscProbPMNSBase> {
 protected:
   OscProbPMNSBase(Neutrino from, Neutrino to);
-
+    
   template <int I, int J>
   double DeltaMSq() const;
-
+  
   template <int I, int J>
   double weight() const;
-
+    
   double weightCP() const;
-
+    
   std::unique_ptr<OscillationVariables> m_param;
   std::unique_ptr<PMNSVariables> m_pmns;
-
+    
   int m_alpha, m_beta, m_lepton_charge;
 };
+    
 
-class OscProbPMNS: public OscProbPMNSBase,
-                   public TransformationBind<OscProbPMNS> {
-public:
-  using TransformationBind<OscProbPMNS>::transformation_;
+namespace GNA {
+  namespace GNAObjectTemplates {
+    class OscProbPMNST: public OscProbPMNSBase,
+                        public TransformationBind<OscProbPMNST<FloatType>, FloatType, FloatType> {
+    public:
+      using TransformationBind<OscProbPMNS>::transformation_;
+    
+      OscProbPMNS(Neutrino from, Neutrino to, std::string l_name="L");
+    
+      template <int I, int J>
+      void calcComponent(FunctionArgs fargs);
+      void calcComponentCP(FunctionArgs fargs);
+      void calcSum(FunctionArgs fargs);
+      void calcFullProb(FunctionArgs fargs);
+    #ifdef GNA_CUDA_SUPPORT
+      void calcFullProbGpu(FunctionArgs fargs);
+      template <int I, int J>
+      void gpuCalcComponent(FunctionArgs& fargs);
+      void gpuCalcComponentCP(FunctionArgs& fargs);
+      void gpuCalcSum(FunctionArgs& fargs);
+    #endif
+   
+    protected:
+      variable<double> m_L;
+    };
+  }
+}
 
-  OscProbPMNS(Neutrino from, Neutrino to, std::string l_name="L");
-
-  template <int I, int J>
-  void calcComponent(FunctionArgs fargs);
-  void calcComponentCP(FunctionArgs fargs);
-  void calcSum(FunctionArgs fargs);
-  void calcFullProb(FunctionArgs fargs);
-#ifdef GNA_CUDA_SUPPORT
-  void calcFullProbGpu(FunctionArgs fargs);
-  template <int I, int J>
-  void gpuCalcComponent(FunctionArgs& fargs);
-  void gpuCalcComponentCP(FunctionArgs& fargs);
-  void gpuCalcSum(FunctionArgs& fargs);
-#endif
-
-protected:
-  variable<double> m_L;
-};
-
+    
 class OscProbAveraged: public OscProbPMNSBase,
                        public TransformationBind<OscProbAveraged> {
 public:
