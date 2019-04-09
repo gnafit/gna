ad5bc998751ac53833792954777dfdc8da9bf94a
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-04-09 17:50:32 +0300

    Add HistEdgesOffset class to handle offset histogram edges

---
+++ tests/unit/core/test_typeclasses_kind.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from load import ROOT as R
+from gna.unittest import *
+from gna import constructors as C
+from gna import context
+import numpy as np
+
+def test_typeclass_kind_v01():
+    objects=[C.Histogram(np.arange(6), np.arange(5)), C.Histogram2d(np.arange(6), np.arange(7))]
+    outputs = [p.single() for p in objects]
+
+    obj = C.DummyType()
+    map(obj.add_input, outputs)
+
+    dt_points = R.TypeClasses.CheckKindT(context.current_precision())(1)
+    dt_points.dump(); print()
+
+    dt_hist = R.TypeClasses.CheckKindT(context.current_precision())(2)
+    dt_hist.dump(); print()
+
+    obj.add_typeclass(dt_hist)
+    res = obj.process_types();
+    assert res
+
+    obj.add_typeclass(dt_points)
+    print('Exception expected: ',end='')
+    res = obj.process_types();
+    assert not res
+
+def test_typeclass_kind_v02():
+    objects=[C.Points(np.arange(6)), C.Points(np.arange(6).reshape(3,2))]
+    outputs = [p.single() for p in objects]
+
+    obj = C.DummyType()
+    map(obj.add_input, outputs)
+
+    dt_points = R.TypeClasses.CheckKindT(context.current_precision())(1)
+    dt_points.dump(); print()
+
+    dt_hist = R.TypeClasses.CheckKindT(context.current_precision())(2)
+    dt_hist.dump(); print()
+
+    obj.add_typeclass(dt_points)
+    res = obj.process_types();
+    assert res
+
+    obj.add_typeclass(dt_hist)
+    print('Exception expected: ',end='')
+    res = obj.process_types();
+    assert not res
+
+if __name__ == "__main__":
+    run_unittests(globals())
+
---
+++ transformations/hist/HistEdgesOffset.cc
@@ -0,0 +1,119 @@
+#include "HistEdgesOffset.hh"
+#include "TypeClasses.hh"
+
+#include <algorithm>
+
+void HistEdgesOffset::init(){
+    auto trans = this->transformation_("histedges");
+    trans.input("hist", /*inactive*/true)
+         .output("points")                 // 0
+         .output("points_truncated")       // 1
+         .output("hist_truncated")         // 2
+         .types(new TypeClasses::CheckKindT<double>(DataKind::Hist), new TypeClasses::CheckNdimT<double>(1))
+         .types(&HistEdgesOffset::types)
+         .func(&HistEdgesOffset::func);
+         ;
+
+    if(m_threshold){
+        trans.output("hist_threshold")     // 3
+             .output("hist_offset")        // 4
+             .output("points_threshold")   // 5
+             .output("points_offset")      // 6
+             ;
+    }
+
+}
+
+void HistEdgesOffset::types(TypesFunctionArgs& fargs){
+    auto& hist_input = fargs.args[0];
+    auto& edges_input = hist_input.edges;
+
+    if(!m_offset){
+        auto it = std::upper_bound(edges_input.begin(), edges_input.end(), m_offset.value());
+        if( it==edges_input.end() || it==edges_input.begin() ){
+            throw fargs.args.error(hist_input, "Unable to find proper offset");
+        }
+
+        m_offset = *(it-1);
+    }
+
+    size_t size_in = hist_input.shape[0];
+    if(m_offset>size_in){
+        throw fargs.args.error(hist_input, "Input histogram has insufficient number of bins for given offset");
+    }
+    size_t newsize = size_in-m_offset.value();
+
+    auto& rets = fargs.rets;
+
+    /// Fill the regular outputs
+    auto& points           = rets[0];
+    auto& points_truncated = rets[1];
+    auto& hist_truncated   = rets[2];
+
+    points.points().shape(size_in+1);
+    points_truncated.points().shape(newsize+1);
+    hist_truncated.hist().edges(newsize+1, edges_input.data()+m_offset.value());
+
+    if(!m_threshold){
+        return;
+    }
+
+    /// Fill the offset/threshold outputs
+    auto& hist_threshold   = rets[3];
+    auto& hist_offset      = rets[4];
+    auto& points_threshold = rets[5];
+    auto& points_offset    = rets[6];
+
+    points_threshold.points().shape(newsize+1);
+    points_offset.points().shape(newsize+1);
+
+    auto edges_threshold=hist_truncated.edges;
+    if( m_threshold.value()<=edges_threshold[0] || m_threshold.value()>=edges_threshold[1] ){
+        throw fargs.args.error(hist_input, "Threshold is not located in a first bin of the truncated histogram (excluding edges)");
+    }
+    auto threshold = edges_threshold[0] = m_threshold.value();
+
+    auto edges_offset = edges_threshold;
+    std::transform(edges_offset.begin(), edges_offset.end(), edges_offset.begin(), [threshold](double v){ return v-threshold; });
+
+    hist_threshold.hist().edges(edges_threshold);
+    hist_offset.hist().edges(edges_offset);
+}
+
+void HistEdgesOffset::func(FunctionArgs& fargs){
+    auto& edges_input = fargs.args[0].type.edges;
+
+    auto& rets = fargs.rets;
+
+    /// Fill the regular outputs
+    auto& points           = rets[0];
+    auto& points_truncated = rets[1];
+    auto& hist_truncated   = rets[2];
+
+    auto& edges_truncated = hist_truncated.type.edges;
+
+    points.x           = Eigen::Map<const Eigen::ArrayXd>(edges_input.data(), edges_input.size());
+    points_truncated.x = Eigen::Map<const Eigen::ArrayXd>(edges_truncated.data(), edges_truncated.size());
+    hist_truncated.x   = 0.0;
+
+    if(!m_threshold){
+        return;
+    }
+
+    /// Fill the offset/threshold outputs
+    auto& hist_threshold   = rets[3];
+    auto& hist_offset      = rets[4];
+    auto& points_threshold = rets[5];
+    auto& points_offset    = rets[6];
+
+    auto& edges_threshold = hist_threshold.type.edges;
+    auto& edges_offset    = hist_offset.type.edges;
+
+    points_threshold.x = Eigen::Map<const Eigen::ArrayXd>(edges_threshold.data(), edges_threshold.size());
+    points_offset.x    = Eigen::Map<const Eigen::ArrayXd>(edges_offset.data(), edges_offset.size());
+    hist_threshold.x   = 0.0;
+    hist_offset.x      = 0.0;
+
+    rets.untaint();
+    rets.freeze();
+}
