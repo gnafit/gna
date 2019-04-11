cb1664a38a53cc37588d89cc0c35e57b622c5bc9
Konstantin Treskov <treskov@jinr.ru>
2019-04-11 17:42:39 +0300

    compare module improvements

--- pylib/gna/ui/compare.py
+++ pylib/gna/ui/compare.py
@@ -7,6 +7,8 @@ from gna.config import cfg
 import runpy
 import sys
 from gna.env import env
+from collections import defaultdict
+import matplotlib.pyplot as plt
 
 
 class cmd(basecmd):
@@ -16,29 +18,44 @@ class cmd(basecmd):
         parser.add_argument('--mapping', default=None, help='Mappings between names of observables')
         parser.add_argument('--ns', help='namespace')
         parser.add_argument('--output', help='Path where to save pics')
+        parser.add_argument('-s','--show', action='store_true', help='Show pics')
 
     def __init__(self, *args, **kwargs):
         basecmd.__init__(self, *args, **kwargs)
-        self.observables = dict() 
+        self.observables = defaultdict(lambda: defaultdict()) 
         if self.opts.mapping:
             self.load_mapping()
 
     def init(self):
         self.check_exps_correct()
         self.extract_observables()
+        #  import IPython; IPython.embed()
+        self.make_plots()
+        if self.opts.show:
+            plt.show()
+
+    def make_plots(self):
+        for title, obses in self.observables.items():
+            fig, (ax, ax_for_ratio) = plt.subplots(nrows=2, ncols=1, sharex=True)
+            fig.subplots_adjust(hspace=0.)
+
+            for label, obs in obses.items():
+                ax.plot(obs, label=label)
+            ax.set_title(title)
+            ax.legend(loc='best')
+
+            first_exp, second_exp = self.exps
+            ratio = obses[first_exp] / obses[second_exp]
+            ax_for_ratio.plot(ratio, label='/'.join(self.exps))
+            ax_for_ratio.legend(loc='best')
 
     def extract_observables(self):
-        idx_pairs = self.mapping['indices']
-        observables = self.mapping['observables']
-        for obs_1, obs_2 in observables:
-            for idx_1, idx_2 in idx_pairs:
-                first_obs = obs_1.format(idx_1)
-                second_obs = obs_2.format(idx_2)
-                exp_1, exp_2 = self.exps
-                print("Extracting {}".format(first_obs))
-                env.get('{0}/{1}'.format(exp_1, first_obs))
-                print("Extracting {}".format(second_obs))
-                env.get('{0}/{1}'.format(exp_2, second_obs))
+        for obs_pair, label in zip(self.mapping['observables'], self.mapping['labels']):
+            for idx_pair in self.mapping['indices']:
+                for obs_template, idx, exp in zip(obs_pair, idx_pair, self.exps):
+                    obs = obs_template.format(idx)
+                    print("Extracting {}".format(obs))
+                    self.observables[label][exp] = env.get('{0}/{1}'.format(exp, obs)).data()
 
     def load_mapping(self):
         map_path = os.path.abspath(self.opts.mapping)
@@ -50,5 +67,5 @@ class cmd(basecmd):
                 reverse = tuple(reversed(self.opts.experiments))
                 if reverse != self.exps:
                     raise Exception("Experiments from cli {0} and mapping "
-                                    "config  {1} doesn't match".format(self.opts.experiments, self.exps))
+                                    "config {1} doesn't match".format(self.opts.experiments, self.exps))
                 self.exps = reverse
