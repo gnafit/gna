184b1268639f12c339601a602c20cdfd40bc4016
Konstantin Treskov <treskov@jinr.ru>
2019-05-11 14:30:16 +0300

    minor

--- tests/detector/test_cherenkov.py
+++ tests/detector/test_cherenkov.py
@@ -13,6 +13,8 @@ from gna.converters import convert
 from argparse import ArgumentParser
 import gna.constructors as C
 
+P.rc('text', usetex=True)
+
 parser = ArgumentParser()
 parser.add_argument( '-o', '--output' )
 parser.add_argument( '-s', '--show', action='store_true' )
@@ -25,7 +27,7 @@ env.defparameter("p2",  central=-2.18044e+01, fixed=True)
 env.defparameter("p3",  central=1.44731e+01,  fixed=True)
 env.defparameter("p4",  central=3.22121e-02,  fixed=True)
 
-energy = N.linspace(0.1, 0.4, num=200)
+energy = N.linspace(0.1, 10, num=200)
 gna_energy = C.Points(energy)
 cherenkov = R.Cherenkov_Borexino()
 cherenkov.cherenkov.energy(gna_energy)
@@ -34,10 +36,17 @@ ch_response = cherenkov.cherenkov.ch_npe.data()
 fig = P.figure()
 ax = P.subplot( 111 )
 ax.plot(energy, ch_response)
-# ax.set_yscale("log")
+ax.set_yscale("log")
+ax.set_xlabel(r"E$_{e}$, MeV", fontsize=14)
+ax.set_ylabel(r"Number of p.e.", fontsize=14)
+ax.set_title("Cherenkov radiation contribution", fontsize=18)
+ax.grid(alpha=0.5)
 # ax.set_xscale("log")
 
 if opts.show:
     P.show()
 
-import IPython; IPython.embed()
+if opts.output:
+    P.savefig(opts.output)
+
+#  import IPython; IPython.embed()
