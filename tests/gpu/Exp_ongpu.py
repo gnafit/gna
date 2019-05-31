91ea126686ba2894b5f161922a17a0545da90ab4
Anna Fatkina <fatkina.a.i@gmail.com>
2019-05-31 11:55:43 +0300

    upd

---
+++ tests/gpu/Exp_ongpu.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Check the WeightedSum transformation"""
+
+from __future__ import print_function
+import numpy as N
+import gna.constructors as C
+from load import ROOT as R
+from gna.constructors import Points, stdvector
+from gna.env import env
+from gna import context, bindings
+import time
+
+"""Initialize inpnuts"""
+ndata = 1000000
+arr1 = N.arange(1, ndata)
+arr2 = arr1
+arr3 = arr1
+print( 'Data1:', arr1 )
+print( 'Data2:', arr2 )
+print( 'Data3:', arr3 )
+
+labels = [ 'arr1', 'arr2', 'arr3' ]
+weights = [ 'w1', 'w2', 'w3' ]
+
+"""Initialize environment"""
+
+#with C.precision('double'):
+
+with context.manager(ndata) as manager:
+  """Initialize transformations"""
+  points1 = Points( arr1 )
+  points2 = Points( arr2 )
+  points3 = Points( arr3 )
+    
+  """Mode1: a1+a2"""
+  ws = R.Exp()
+  ws.exp.points(points1.points)
+    
+#  ws.exp.switchFunction("gpu")
+print( 'Mode1: ' )
+print(  ws.exp.result.data() )
+print()
+
+   
+for x in range(0,20):
+    ws.exp.taint()
+    start_time = time.time()
+    ws.exp.result.data()
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(elapsed_time)
+    
