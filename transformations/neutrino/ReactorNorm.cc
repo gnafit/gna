c165dda6b3515b5e29b75e7c2cff900e1141f39f
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-06-10 15:54:54 +0300

    Fix ReactorNorm normalization
    Thermal power was divided by actual energy per fission for each isotope. Should be divided by average energy per fission for all isotopes

--- transformations/neutrino/ReactorNorm.cc
+++ transformations/neutrino/ReactorNorm.cc
@@ -63,7 +63,12 @@ void ReactorNorm::calcIsotopeNorms(FunctionArgs fargs) {
   static double conversionFactor = 1.0e-7/TMath::Qe();
   auto distanceWeight = (conversionFactor / (4*pi*std::pow(m_L, 2)));
   auto coeff = m_targetProtons*m_thermalPower*distanceWeight;
+
+  double ePerFission_avg=0.0;
   for (size_t i = 0; i < rets.size(); ++i) {
-    rets[i].x[0] = (coeff*livetime*power_rate*args[i].x/m_ePerFission[i]).sum();
+    ePerFission_avg+=args[i].x(0)*m_ePerFission[i].value();
+  }
+  for (size_t i = 0; i < rets.size(); ++i) {
+    rets[i].x[0] = (coeff*livetime*power_rate*args[i].x/ePerFission_avg).sum();
   }
 }
