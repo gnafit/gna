4e8fc02fcfb27c6834b7aa8f58d4c69f6495f3a6
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-06-13 17:39:19 +0300

    Add TransformationEntry context to store user defined Attrs

---
+++ core/transformation/TransformationContext.cc
@@ -0,0 +1,7 @@
+#include "TransformationContext.hh"
+
+using TransformationTypes::Attrs;
+using TransformationTypes::AttrsStack;
+
+Attrs TransformationTypes::TransformationContext::context={};
+AttrsStack TransformationTypes::TransformationContext::context_stack={};
