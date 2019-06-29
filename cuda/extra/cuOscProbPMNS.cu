19e21c38226937ed2b89c3f7ac8cf90a8c93b977
Anna Fatkina <fatkina.a.i@gmail.com>
2019-06-29 14:27:59 +0300

    osctob to template

--- cuda/extra/cuOscProbPMNS.cu
+++ cuda/extra/cuOscProbPMNS.cu
@@ -31,17 +31,18 @@ __global__ void cuCalcComponent(float** xarg, float** xret, float** intern) {
  * CUDA version of calcComponent function in OscProbPMNS::OscProbPMNS (doubles)
  * 
  */
-
-__global__ void d_cuCalcComponent(double** xarg, double** xret, double** intern, double** params,
-				unsigned int m, double oscprobArgumentFactor, double DeltaMSq, double m_L) { 
+template<typename T>
+__global__ void d_cuCalcComponent(T** xarg, T** xret, T** intern, T** params,
+				unsigned int m, T oscprobArgumentFactor, T DeltaMSq, T m_L) { 
 	inverse(xarg[0], intern[0], m);
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
         xret[0][idx] = cos( DeltaMSq * m_L * oscprobArgumentFactor * 0.5 * intern[0][idx]);      
 }
 
 
-void cuCalcComponent(double** xarg, double** xret, double** intern, double** params,
-		 unsigned int m, unsigned int n, double oscprobArgumentFactor, double DeltaMSq, double m_L) {
+template<typename T>
+void cuCalcComponent(T** xarg, T** xret, T** intern, T** params,
+		 unsigned int m, unsigned int n, T oscprobArgumentFactor, T DeltaMSq, T m_L) {
 	d_cuCalcComponent<<<m/CU_BLOCK_SIZE + 1, CU_BLOCK_SIZE>>>(xarg, xret, intern, params,
 								 m, oscprobArgumentFactor, DeltaMSq, m_L);
 	cudaDeviceSynchronize();
@@ -51,11 +52,9 @@ void cuCalcComponent(double** xarg, double** xret, double** intern, double** par
  * CUDA version of calcComponentCP function in OscProbPMNS::OscProbPMNS
  * 
  */
-
-
-//template <typename T>
-__global__ void d_cuCalcComponentCP(double** xarg, double** xret, double** intern, double** params, double m12, double m13, double m23, unsigned int m,
-					double oscprobArgumentFactor, double m_L) {
+template <typename T>
+__global__ void d_cuCalcComponentCP(T** xarg, T** xret, T** intern, T** params, T m12, T m13, T m23, T int m,
+					T oscprobArgumentFactor, T m_L) {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 //	xret[0][idx] = 1.0;
 	inverse(xarg[0], intern[0], m);
@@ -72,14 +71,16 @@ __global__ void d_cuCalcComponentCP(double** xarg, double** xret, double** inter
 //	mult_by_arr_sin(1.0, intern, xret);
 }
 
-void cuCalcComponentCP(double** xarg, double** xret, double** intern, double** params, double m12, double m13, double m23, 
-			unsigned int m, unsigned int n, double oscprobArgumentFactor, double m_L) {
+template<typename T>
+void cuCalcComponentCP(T** xarg, T** xret, T** intern, T** params, T m12, T m13, T m23, 
+			unsigned int m, unsigned int n, T oscprobArgumentFactor, T m_L) {
 	d_cuCalcComponentCP<<<m/CU_BLOCK_SIZE + 1, CU_BLOCK_SIZE>>>(xarg, xret, intern, params, m12, m13, m23, m, 
 								oscprobArgumentFactor, m_L);
 	cudaDeviceSynchronize();
 }
 
-__global__ void d_cuCalcSum(double** xarg, double** xret, double w12, double w13, double w23, double wcp, bool isSame) {
+template<typename T>
+__global__ void d_cuCalcSum(T** xarg, T** xret, T w12, T w13, T w23, T wcp, bool isSame) {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 /*	xret[0][idx]=2.0*(params[0][0]*xarg[0][idx]+params[0][1]*xarg[1][idx]+params[0][2]*xarg[2][idx])
 			+ (-2.0*(params[0][0]+params[0][1]+params[0][2]) 
@@ -92,7 +93,18 @@ __global__ void d_cuCalcSum(double** xarg, double** xret, double w12, double w13
 			+ 8.0*(!isSame)*wcp*xarg[4][idx];
 }
 
-void cuCalcSum(double** xarg, double** xret, double w12, double w13, double w23, double wcp, bool isSame, unsigned int m) {
+template<typename T>
+void cuCalcSum(T** xarg, T** xret, T w12, T w13, T w23, T wcp, bool isSame, unsigned int m) {
 	d_cuCalcSum<<<m/CU_BLOCK_SIZE + 1, CU_BLOCK_SIZE>>>(xarg, xret, w12, w13, w23, wcp, isSame);
 	cudaDeviceSynchronize();
 }
+
+template void cuCalcComponent<double>(double** xarg, double xret,  double w12, double w13, double w23, double wcp, bool isSame, unsigned int m);
+template void cuCalcComponent<float>(float** xarg, float xret,  float w12, float w13, float w23, float wcp, bool isSame, unsigned int m);
+
+template void cuCalcComponentCP<double>(double** xarg, double** xret, double** intern, double** params, double m12, double m13, double m23, unsigned int m, unsigned int n, double oscprobArgumentFactor, double m_L);
+template void cuCalcComponentCP<float>(float** xarg, float** xret, float** intern, float** params, float m12, float m13, float m23, unsigned int m, unsigned int n, float oscprobArgumentFactor, float m_L);
+
+template void cuCalcComponent<double>(double** xarg, double** xret, double** intern, double** params, unsigned int m, unsigned int n, double oscprobArgumentFactor, double DeltaMSq, double m_L);
+template void cuCalcComponent<float>(float** xarg, float** xret, float** intern, float** params, unsigned int m, unsigned int n, float oscprobArgumentFactor, float DeltaMSq, float m_L);
+
--- cuda/extra/cuOscProbPMNS.hh
+++ cuda/extra/cuOscProbPMNS.hh
@@ -1,5 +1,10 @@
 #pragma once
 
-void cuCalcComponent(double** xarg, double** xret, double** intern, double** params, unsigned int n, unsigned int m, double oscprobArgumentFactor, double DeltaMSq, double m_L);
-void cuCalcComponentCP(double** xarg, double** xret, double** intern, double** params, double m12, double m13, double m23, unsigned int n, unsigned int m, double oscprobArgumentFactor, double m_L);
-void cuCalcSum(double** xarg, double** xret, double w12, double w13, double w23, double wcp, bool isSame, unsigned int n);
+template<typename T>
+void cuCalcComponent(T** xarg, T** xret, T** intern, T** params, unsigned int n, unsigned int m, T oscprobArgumentFactor, T DeltaMSq, T m_L);
+
+template<typename T>
+void cuCalcComponentCP(T** xarg, T** xret, T** intern, T** params, T m12, T m13, T m23, unsigned int n, unsigned int m, T oscprobArgumentFactor, T m_L);
+
+template<typename T>
+void cuCalcSum(T** xarg, T** xret, T w12, T w13, T w23, T wcp, bool isSame, unsigned int n);
