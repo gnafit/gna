69fa22aa59fdc1728a6a6d69e2dc193a26162a8b
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-07-12 13:07:53 +0300

    Introduce new mode of handling pull parameters. Use VarArray and Points to store all the variables and covariance matrix in the same time

--- pylib/gna/ui/dataset.py
+++ pylib/gna/ui/dataset.py
@@ -15,10 +15,16 @@ from gna.dataset import Dataset
 from gna.parameters.parameter_loader import get_parameters
 
 class cmd(basecmd):
+    pull_vararray, pull_centrals, pull_sigmas2, pull_covariance = None, None, None, None
+
     @classmethod
     def initparser(cls, parser, env):
         parser.add_argument('--name', required=True, help='Dataset name', metavar='dataset')
-        parser.add_argument('--pull', action='append', help='Parameters to be added as pull terms')
+
+        pull = parser.add_mutually_exclusive_group()
+        pull.add_argument('--pull-legacy', action='append', help='Parameters to be added as pull terms (legacy code)')
+        pull.add_argument('--pull', action='append', help='Parameters to be added as pull terms')
+
         parser.add_argument('--asimov-data', nargs=2, action='append',
                             metavar=('THEORY', 'DATA'),
                             default=[])
@@ -38,13 +44,11 @@ class cmd(basecmd):
         verbose = self.opts.verbose
         if verbose:
             print('Adding pull parameters to dataset', self.opts.name)
-        if self.opts.pull:
-            pull_pars = get_parameters(self.opts.pull, drop_fixed=True, drop_free=True)
+        if self.opts.pull_legacy:
+            self.load_pulls_legacy(dataset)
 
-            for par in pull_pars:
-                dataset.assign(par, [par.central()], [par.sigma()**2])
-                if verbose:
-                    print (par, [par.central()], [par.sigma()**2])
+        if self.opts.pull:
+            self.load_pulls(dataset)
 
         if self.opts.asimov_data:
             for theory_path, data_path in self.opts.asimov_data:
@@ -70,3 +74,58 @@ class cmd(basecmd):
                                    # env.get(theory_path))
 
         self.env.parts.dataset[self.opts.name] = dataset
+
+    def load_pulls_legacy(self, dataset):
+        pull_pars = get_parameters(self.opts.pull_legacy, drop_fixed=True, drop_free=True)
+
+        for par in pull_pars:
+            dataset.assign(par, [par.central()], [par.sigma()**2])
+            if verbose:
+                print (par, [par.central()], [par.sigma()**2])
+
+    def load_pulls(self, dataset):
+        #
+        # Load nuisance parameters
+        #
+
+        # Get list of UncertainParameter objects, drop free and fixed
+        pull_pars = get_parameters(self.opts.pull, drop_fixed=True, drop_free=True)
+        npars = len(pull_pars)
+
+        variables, centrals, sigmas = [None]*npars, np.zeros(npars, dtype='d'), np.zeros(npars, dtype='d')
+        correlations = False
+
+        # Get variables, central values and sigmas, check that there are (no) correlations
+        for i, par in enumerate(pull_pars):
+            variables[i]=par.getVariable()
+            centrals[i]=par.central()
+            sigmas[i]=par.sigma()
+            correlations = correlations or par.isCorrelated()
+
+        from gna.constructors import VarArray, Points
+        # Create an array, representing pull parameter values
+        self.pull_vararray = VarArray(variables, labels='Nuisance: values')
+        # Create an array, representing pull parameter central values
+        self.pull_centrals = Points(centrals, labels='Nuiscance: central')
+
+        if correlations:
+            # In case there are correlations:
+            # - create covariance matrix
+            # - fill the diagonal it with the value of sigma**2
+            # - fill the off-diagonal elements with covarainces
+            # - create Points, representing the covariance matrix
+            covariance = np.diag(sigma**2)
+            for i in range(npars):
+                for j in range(i):
+                    pari, parj = pull_pars[i], pull_pars[j]
+                    cov = pari.getCovariance(parj)
+                    covariance[i,j]=covariance[j,i]=cov
+
+            self.pull_covariance = Points(covariance, labels='Nuiscance: covariance matrix')
+        else:
+            # If there are no correlations, store only the uncertainties
+            self.pull_sigmas2  = Points(sigmas**2, labels='Nuisance: sigma')
+
+        dataset.assign(self.pull_vararray, self.pull_centrals, self.pull_sigmas2)
+
+
