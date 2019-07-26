b8e1b710e44d134a2c522ebcd1dbe81de06af4ad
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-07-26 17:21:51 +0300

    Merge transformations/base/* from cugna

--- transformations/base/FillLike.hh
+++ transformations/base/FillLike.hh
@@ -3,41 +3,53 @@
 #include <string>
 
 #include "GNAObject.hh"
-#include "TypesFunctions.hh"
+#include "TypeClasses.hh"
+using namespace TypeClasses;
 
-/**
- * @brief FillLike transformation
- *
- * Fill an array with a number. The shape of an array is detemined by an input.
- *
- * Inputs and outputs:
- *   - `fill.inputs.a` -- the input array, needed to detemine the shape.
- *   - `fill.outputs.a` -- the output array.
- *
- * @author Dmitry Taychenachev
- * @date 02.2016
- */
-class FillLike: public GNASingleObject,
-                public TransformationBind<FillLike> {
-public:
-  /**
-   * @brief Constructor.
-   * @param value -- the value to be written to an array.
-   */
-  FillLike(double value)
-    : m_value(value)
-  {
-    transformation_("fill")                                // Initialize tranformation 'fill':
-      .input("a")                                          // - with single input 'a'
-      .output("a")                                         // - and single output 'a'
-      .types(TypesFunctions::passAll)                      // - the shape of the output is taken from the input
-      .func([](FillLike *obj, FunctionArgs& fargs) {       // - The implementation function:
-          auto& rets=fargs.rets;
-          rets[0].x.setConstant(obj->m_value);             //   set each element of a single output to m_value.
-          rets.untaint();
-          rets.freeze();
-        });
+namespace GNA{
+  namespace GNAObjectTemplates{
+    /**
+     * @brief FillLike transformation
+     *
+     * Fill an array with a number. The shape of an array is detemined by an input.
+     *
+     * Inputs and outputs:
+     *   - `fill.inputs.a` -- the input array, needed to detemine the shape.
+     *   - `fill.outputs.a` -- the output array.
+     *
+     * @author Dmitry Taychenachev
+     * @date 02.2016
+     */
+    template<typename FloatType>
+    class FillLikeT: public GNASingleObjectT<FloatType,FloatType>,
+                     public TransformationBind<FillLikeT<FloatType>,FloatType,FloatType> {
+    private:
+      using GNAObject = GNAObjectT<FloatType,FloatType>;
+      using FillLike = FillLikeT<FloatType>;
+      using typename GNAObject::FunctionArgs;
+    public:
+      /**
+       * @brief Constructor.
+       * @param value -- the value to be written to an array.
+       */
+      FillLikeT(FloatType value)
+        : m_value(value)
+      {
+        this->transformation_("fill")                          // Initialize tranformation 'fill':
+          .input("a")                                          // - with single input 'a'
+          .output("a")                                         // - and single output 'a'
+          .types(new PassTypeT<FloatType>(0,{0,-1}))           // - the shape of the output is taken from the input
+          .func([](FillLike *obj, FunctionArgs& fargs) {       // - The implementation function:
+              auto& rets=fargs.rets;
+              rets[0].x.setConstant(obj->m_value);             //   set each element of a single output to m_value.
+              rets.untaint();
+              rets.freeze();
+            });
+      }
+    protected:
+      FloatType m_value;  /// The value to be writte to input array.
+    };
   }
-protected:
-  double m_value;  /// The value to be writte to input array.
-};
+}
+
+using FillLike = GNA::GNAObjectTemplates::FillLikeT<double>;
--- transformations/base/Product.cc
+++ transformations/base/Product.cc
@@ -1,64 +1,81 @@
 #include "Product.hh"
-#include "TypesFunctions.hh"
+#include "GNAObject.hh"
+#include "TypeClasses.hh"
+using namespace TypeClasses;
 
-Product::Product() {
-  transformation_("product")
-    .output("product")
-    .types(TypesFunctions::ifSameShapeOrSingle, TypesFunctions::passNonSingle<0,0>)
-    .func([](FunctionArgs& fargs) {
-        auto& args=fargs.args;
-        auto& ret=fargs.rets[0].x;
-        double factor=1.0;
-        bool secondary=false;
-        for (size_t i = 0; i < args.size(); ++i) {
-          auto& data=args[i].x;
-          if (data.size()!=1) {
-            if (secondary) {
-              ret*=data;
-            } else {
-              ret=data;
-              secondary=true;
+#include "config_vars.h"
+#ifdef GNA_CUDA_SUPPORT
+#include "cuElementary.hh"
+#include "DataLocation.hh"
+#endif
+
+namespace GNA {
+  namespace GNAObjectTemplates {
+    template<typename FloatType>
+    ProductT<FloatType>::ProductT() {
+      this->transformation_("product")
+        .output("product")
+        .types(new CheckSameTypesT<FloatType>({0,-1}), new PassTypeT<FloatType>(0, {0,0}))
+        .func([](typename GNAObjectT<FloatType,FloatType>::FunctionArgs& fargs) {
+            auto& args=fargs.args;
+            auto& ret=fargs.rets[0].x;
+            ret = args[0].x;
+            for (size_t i = 1; i < args.size(); ++i) {
+                ret*=args[i].x;
             }
-          }
-          else{
-            factor*=data(0);
-          }
-        }
-        if(!secondary){
-          ret=factor;
-        }
-        else if(factor!=1){
-          ret*=factor;
-        }
-      });
-}
+          })
+#ifdef GNA_CUDA_SUPPORT
+        .func("gpu", &ProductT<FloatType>::product_ongpu, DataLocation::Device)
+#endif
+    	;
+    }
 
-/**
- * @brief Construct Product from vector of SingleOutput instances
- */
-Product::Product(const OutputDescriptor::OutputDescriptors& outputs) : Product(){
-  for(auto& output : outputs){
-    this->multiply(*output);
+    /**
+     * @brief Construct Product from vector of SingleOutput instances
+     */
+    template<typename FloatType>
+    ProductT<FloatType>::ProductT(const typename GNAObjectT<FloatType,FloatType>::OutputDescriptor::OutputDescriptors& outputs) : ProductT<FloatType>(){
+      for(auto& output : outputs){
+        this->multiply(*output);
+      }
+    }
+
+    /**
+     * @brief Add an input and connect it to the output.
+     *
+     * The input name is derived from the output name.
+     *
+     * @param out -- a SingleOutput instance.
+     * @return InputDescriptor instance for the newly created input.
+     */
+    template<typename FloatType>
+    InputDescriptorT<FloatType,FloatType> ProductT<FloatType>::multiply(typename GNAObjectT<FloatType,FloatType>::SingleOutput &out) {
+      return InputDescriptorT<FloatType,FloatType>(this->t_[0].input(out));
+    }
+
+    /**
+     * @brief Add an input by name and leave unconnected.
+     * @param name -- a name for the new input.
+     * @return InputDescriptor instance for the newly created input.
+     */
+    template<typename FloatType>
+    InputDescriptorT<FloatType,FloatType> ProductT<FloatType>::add_input(const char* name) {
+      return InputDescriptorT<FloatType,FloatType>(this->t_[0].input(name));
+    }
+
+#ifdef GNA_CUDA_SUPPORT
+    template<typename FloatType>
+    void ProductT<FloatType>::product_ongpu(FunctionArgs& fargs) {
+    	fargs.args.touch();
+    	auto& gpuargs=fargs.gpu;
+    	auto** source=gpuargs->args;
+        auto** dest  =gpuargs->rets;
+    	cuproduct(source, dest, gpuargs->nargs, fargs.args[0].arr.size());
+    }
+#endif
   }
 }
-
-/**
- * @brief Add an input and connect it to the output.
- *
- * The input name is derived from the output name.
- *
- * @param out -- a SingleOutput instance.
- * @return InputDescriptor instance for the newly created input.
- */
-InputDescriptor Product::multiply(SingleOutput &out) {
-  return InputDescriptor(t_[0].input(out));
-}
-
-/**
- * @brief Add an input by name and leave unconnected.
- * @param name -- a name for the new input.
- * @return InputDescriptor instance for the newly created input.
- */
-InputDescriptor Product::add_input(const char* name) {
-  return InputDescriptor(t_[0].input(name));
-}
+template class GNA::GNAObjectTemplates::ProductT<double>;
+#ifdef PROVIDE_SINGLE_PRECISION
+  template class GNA::GNAObjectTemplates::ProductT<float>;
+#endif
