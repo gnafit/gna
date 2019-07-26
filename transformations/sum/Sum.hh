6f001923dc2c30de30ebdedbd3d999e819fbe8fb
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-07-26 17:26:05 +0300

    Merge transformations/sum/Sum* from cugna

--- transformations/sum/Sum.hh
+++ transformations/sum/Sum.hh
@@ -2,27 +2,45 @@
 
 #include "GNAObject.hh"
 
-/**
- * @brief Calculate the element-wise sum of the inputs.
- *
- * Outputs:
- *   - `sum.sum` -- the result of a sum.
- *
- * @author Dmitry Taychenachev
- * @date 2015
- */
-class Sum: public GNASingleObject,
-           public TransformationBind<Sum> {
-public:
-  Sum();                                                   ///< Constructor.
-  Sum(const OutputDescriptor::OutputDescriptors& outputs); ///< Construct Sum from vector of outputs
+namespace GNA {
+  namespace GNAObjectTemplates {
+    /**
+     * @brief Calculate the element-wise sum of the inputs.
+     *
+     * Outputs:
+     *   - `sum.sum` -- the result of a sum.
+     *
+     * @author Dmitry Taychenachev
+     * @date 2015
+     */
+    template<typename FloatType>
+    class SumT: public GNASingleObjectT<FloatType,FloatType>,
+               public TransformationBind<SumT<FloatType>, FloatType, FloatType> {
+    private:
+      using BaseClass = GNAObjectT<FloatType,FloatType>;
+    public:
+      using typename BaseClass::FunctionArgs;
+      using typename BaseClass::TypesFunctionArgs;
+      using typename BaseClass::SingleOutput;
+      using typename BaseClass::OutputDescriptor;
 
-  InputDescriptor add_input(const char* name);  ///< Add an input by name and leave unconnected.
+      SumT();                                                            ///< Constructor.
+      SumT(const typename OutputDescriptor::OutputDescriptors& outputs); ///< Construct Sum from vector of outputs
 
-  /** @brief Add an input by name and leave unconnected. */
-  InputDescriptor add(const char* name){
-    return add_input(name);
+      InputDescriptorT<FloatType, FloatType> add_input(const char* name);  ///< Add an input by name and leave unconnected.
+
+      /** @brief Add an input by name and leave unconnected. */
+      InputDescriptorT<FloatType,FloatType> add(const char* name){
+        return add_input(name);
+      }
+
+      InputDescriptorT<FloatType,FloatType> add(SingleOutput &data);   ///< Add an input and connect it to the output.
+
+#ifdef GNA_CUDA_SUPPORT
+      void sum_ongpu(FunctionArgs& fargs);
+#endif
+    };
   }
+}
 
-  InputDescriptor add(SingleOutput &data);   ///< Add an input and connect it to the output.
-};
+using Sum = GNA::GNAObjectTemplates::SumT<double>;
