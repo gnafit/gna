Fix compilation on clang with single-precision floats by removing explicit specialization of std::complex.
Introduce pytest-style parametrization for some tests.

tests/unit/transformations/test_ProductBC.py
@@ -5,6 +5,8 @@ from __future__ import print_function
 from load import ROOT as R
 import gna.constructors as C
 import numpy as np
+import pytest
+
 from gna.unittest import *
 
 product_broadcast_mat_m11 = np.ones((1,), dtype='d')
@@ -14,7 +16,24 @@ product_broadcast_mat_m33 = np.ones((3,3), dtype='d')
 product_broadcast_rup   = np.arange(12.0, dtype='d').reshape(3,4)
 product_broadcast_rdown = np.arange(12.0, dtype='d')[::-1].reshape(3,4)
 
-def check_product_broadcast(arrays):
+
+@pytest.mark.parametrize('arrays',
+        [
+            (product_broadcast_mat_m11,),
+            (product_broadcast_mat_m11*2, product_broadcast_mat_m11*3, product_broadcast_mat_m11*4,),
+            (product_broadcast_mat_m11*2, product_broadcast_mat_m11*0,),
+            (product_broadcast_mat_m34,),
+            (product_broadcast_mat_m34, product_broadcast_mat_m34*0,),
+            (2.0*product_broadcast_mat_m11, 3.0*product_broadcast_mat_m34,),
+            (3.0*product_broadcast_mat_m34, 2.0*product_broadcast_mat_m11,),
+            (2.0*product_broadcast_mat_m11, 3.0*product_broadcast_mat_m34, 4.0*product_broadcast_mat_m34,),
+            (3.0*product_broadcast_mat_m34, 2.0*product_broadcast_mat_m11, 4.0*product_broadcast_mat_m34,),
+            (3.0*product_broadcast_mat_m34, 4.0*product_broadcast_mat_m34, 2.0*product_broadcast_mat_m11,),
+            (3.0*product_broadcast_mat_m34, 4.0*product_broadcast_mat_m34, 2.0*product_broadcast_mat_m34,),
+            (product_broadcast_rup, product_broadcast_rdown,),
+        ]
+        )
+def test_product_broadcast(arrays):
     print('Test ', len(arrays), ':', sep='')
     for array in arrays:
         print(array)
@@ -36,42 +55,6 @@ def check_product_broadcast(arrays):
     print('Result', calc, end='\n\n')
 
     assert (calc==truth).all()
-
-def test_product_broadcast_01():
-    check_product_broadcast([product_broadcast_mat_m11])
-
-def test_product_broadcast_01a():
-    check_product_broadcast([product_broadcast_mat_m11*2, product_broadcast_mat_m11*3, product_broadcast_mat_m11*4])
-
-def test_product_broadcast_01b():
-    check_product_broadcast([product_broadcast_mat_m11*2, product_broadcast_mat_m11*0])
-
-def test_product_broadcast_02():
-    check_product_broadcast([product_broadcast_mat_m34])
-
-def test_product_broadcast_02b():
-    check_product_broadcast([product_broadcast_mat_m34, product_broadcast_mat_m34*0])
-
-def test_product_broadcast_03():
-    check_product_broadcast([2.0*product_broadcast_mat_m11, 3.0*product_broadcast_mat_m34])
-
-def test_product_broadcast_04():
-    check_product_broadcast([3.0*product_broadcast_mat_m34, 2.0*product_broadcast_mat_m11])
-
-def test_product_broadcast_03():
-    check_product_broadcast([2.0*product_broadcast_mat_m11, 3.0*product_broadcast_mat_m34, 4.0*product_broadcast_mat_m34])
-
-def test_product_broadcast_04():
-    check_product_broadcast([3.0*product_broadcast_mat_m34, 2.0*product_broadcast_mat_m11, 4.0*product_broadcast_mat_m34])
-
-def test_product_broadcast_05():
-    check_product_broadcast([3.0*product_broadcast_mat_m34, 4.0*product_broadcast_mat_m34, 2.0*product_broadcast_mat_m11])
-
-def test_product_broadcast_06():
-    check_product_broadcast([3.0*product_broadcast_mat_m34, 4.0*product_broadcast_mat_m34, 2.0*product_broadcast_mat_m34])
-
-def test_product_broadcast_07():
-    check_product_broadcast([product_broadcast_rup, product_broadcast_rdown])
 
 if __name__ == "__main__":
     run_unittests(globals())