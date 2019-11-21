0291a5ac125131a09247d6c36898b3c4c186b7f8
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-11-21 15:18:34 +0300

    Add plot_lines function for matplotlib

---
+++ tests/python/plot_table.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from matplotlib import pyplot as plt
+from mpl_tools.plot_lines import plot_lines
+
+fig = plt.figure()
+ax = plt.subplot( 111 )
+ax.minorticks_on()
+ax.grid()
+ax.set_xlabel( '' )
+ax.set_ylabel( '' )
+ax.set_title( '' )
+
+lines = (
+        'first line with formula $e=mc^2$',
+        'second line with number: 240'
+        )
+plot_lines('text', loc='upper right')
+
+fig = plt.figure()
+ax = plt.subplot( 111 )
+ax.minorticks_on()
+ax.grid()
+ax.set_xlabel( '' )
+ax.set_ylabel( '' )
+ax.set_title( '' )
+plt.subplots_adjust(right=0.6)
+
+lines = (
+        'first line with formula $e=mc^2$',
+        'second line with number: 240'
+        )
+plot_lines(lines, loc='upper right', outside=[1.02, 1.0])
+
+plt.show()
