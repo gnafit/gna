83dff98d870118147609774463c2086a07969059
Konstantin Treskov <treskov@jinr.ru>
2019-12-03 11:49:43 +0300

    Add missing test for interpolation

---
+++ tests/unit/transformations/test_interpolation.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import numpy as N
+import os
+import matplotlib as mpl
+from matplotlib import pyplot as P
+from mpl_tools.helpers import savefig
+import pytest
+import allure
+
+from load import ROOT as R
+from gna.constructors import Points
+
+@pytest.mark.parametrize('strategy', [R.GNA.Interpolation.Strategy.Extrapolate, R.GNA.Interpolation.Strategy.Constant])
+@pytest.mark.parametrize('interpolator', [R.InterpLinear, R.InterpExpo, R.InterpLog, R.InterpLogx])
+def test_interpolators(interpolator, strategy, tmp_path):
+    '''Test various interpolators'''
+
+    segments   = N.arange(1.0, 10.1, 1.5, dtype='d')
+    segments_t = Points(segments)
+
+    points   = N.stack([N.linspace(0.0+i, 12.+i, 61, dtype='d') for i in [0, -0.1, 0.1, 0.3, 0.5]]).T
+    points_t = Points(points)
+
+    fcn = N.exp( -(segments-segments[0])*0.5 )
+    fcn = N.exp(segments**(-0.5))
+    fcn_t = Points(fcn)
+
+    ie = interpolator()
+
+    ie.set_underflow_strategy(strategy)
+    ie.set_overflow_strategy(strategy)
+
+    ie.interpolate(segments_t, fcn_t, points_t)
+    seg_idx = ie.insegment.insegment.data()
+
+    res = ie.interp.interp.data()
+
+    fig = P.figure()
+    ax = P.subplot(111)
+    ax.minorticks_on()
+    ax.grid()
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    strategy_name = "Constant" if strategy is R.GNA.Interpolation.Strategy.Constant else "Extrapolate"
+    title = ie.__class__.__name__.strip('Interp') + ", " + "Strategy: {}".format(strategy_name)
+    ax.set_title(title)
+
+    ax.plot( segments, fcn, 'o-', markerfacecolor='none', label='coarse function', linewidth=0.5, alpha=0.5 )
+
+    markers='os*^vh'
+    for i, (p, r) in enumerate(zip(points.T, res.T)):
+        ax.plot( p, r, '.', label='interpolation, col %i'%i, marker=markers[i], markersize=1.5 )
+        break
+
+    ax.legend(loc='upper right')
+    path = os.path.join(str(tmp_path), '{}_{}.png'.format(ie.__class__.__name__, strategy_name))
+    savefig(path, dpi=300)
+    allure.attach.file(path, attachment_type=allure.attachment_type.PNG)
