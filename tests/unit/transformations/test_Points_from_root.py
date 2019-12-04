c4e36121cb995df0edabe88a0e2fc1c66aebfc19
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-12-04 14:00:21 +0300

    misc

--- tests/unit/transformations/test_Points_from_root.py
+++ tests/unit/transformations/test_Points_from_root.py
@@ -26,8 +26,14 @@ def test_points_v01_TH1D():
     assert np.all(buf==res)
 
 def test_points_v02_TH2D():
-    hist = R.TH2D('testhist', 'testhist', 20, -5, 5, 24, -6, 6)
-    hist.FillRandom('gaus', 10000)
+    hist = R.TH2D('testhist', 'testhist', 20, 0, 10, 24, -6, 6)
+
+    xyg=R.TF2("xyg","exp([0]*x)*exp([1]*y)", 0, 10, 0, 12)
+    xyg.SetParameter(0, -1/2.)
+    xyg.SetParameter(1, -1/8.)
+    R.gDirectory.Add( xyg )
+
+    hist.FillRandom('xyg', 10000)
 
     p = C.Points(hist)
 
@@ -39,7 +45,7 @@ def test_points_v02_TH2D():
 def test_points_v03_TMatrixD():
     n1, n2 = 3, 4
     mat = R.TMatrixD(n1, n2)
-    for i, (i1, i2) in enumerate(I.product(range(1, n1+1), range(1, n2+1))):
+    for i, (i1, i2) in enumerate(I.product(range(n1), range(n2))):
         mat[i1, i2] = i
 
     p = C.Points(mat)
