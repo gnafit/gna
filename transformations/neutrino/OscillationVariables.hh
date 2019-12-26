f61bd2b5542c3f2f9f97ba8f28f76164f75bb5f6
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-12-26 17:35:08 +0300

    Add labels to oscillation parameters

--- transformations/neutrino/OscillationVariables.hh
+++ transformations/neutrino/OscillationVariables.hh
@@ -109,58 +109,64 @@ namespace GNA {
           .add(&DeltaMSq13,
                {&DeltaMSq23, &Alpha, &DeltaMSq12}, [&]() {
                  return DeltaMSq23.value() + Alpha.value()*DeltaMSq12.value();
-               }, "Δm²(13)=Δm²(23) + αΔm²(12)")
+               }, "|Δm²₁₃|=|Δm²₂₃| + α|Δm²₁₂|")
           .add(&DeltaMSq23,
                {&DeltaMSq13, &Alpha, &DeltaMSq12}, [&]() {
                  return DeltaMSq13.value() - Alpha.value()*DeltaMSq12.value();
-               }, "Δm²(23)=Δm²(13) - αΔm²(12)")
+               }, "|Δm²₂₃|=|Δm²₁₃| - α|Δm²₁₂|")
           .add(&DeltaMSq23,
                {&DeltaMSqEE, &Alpha, &SinSq12, &DeltaMSq12}, [&]() {
                  return DeltaMSqEE.value() + Alpha.value()*(SinSq12.value() - 1)*DeltaMSq12.value();
-               }, "Δm²(23)=Δm²(ee) + α w(12) Δm²(12)")
+               }, "|Δm²₂₃|=|Δm²(ee)| + α w₁₂ |Δm₁₂²|")
           .add(&DeltaMSqEE,
                {&DeltaMSq23, &Alpha, &SinSq12, &DeltaMSq12}, [&]() {
                  return DeltaMSq23.value() - Alpha.value()*(SinSq12.value() - 1)*DeltaMSq12.value();
-               }, "Δm²(ee) =Δm²(23) - α w(12) Δm²(12)")
+               }, "|Δm²(ee)|=|Δm²₂₃| - α w₁₂ |Δm₁₂²|")
           .add(&DeltaMSqMM, {&DeltaMSqEE, &Alpha, &Theta12, &Delta, &DeltaMSq12, &Theta23, &Theta13}, [&](){
                   return DeltaMSqEE.value() - Alpha.value()*sin(2*Theta12.value())
                          + cos(Delta.value())*sin(Theta13.value())*sin(2*Theta12.value())*tan(Theta23.value())*DeltaMSq12.value();},
-               "Δm²(mm) = f(Δm²(ee), ...)" )
+               "|Δm²(μμ)|=f(|Δm²(ee)|, θ)" )
           .add(&DeltaMSqEE, {&DeltaMSqMM, &Alpha, &Theta12, &Delta, &DeltaMSq12, &Theta23, &Theta13}, [&](){
                   return DeltaMSqMM.value() + Alpha.value()*sin(2*Theta12.value())
                          - cos(Delta.value())*sin(Theta13.value())*sin(2*Theta12.value())*tan(Theta23.value())*DeltaMSq12.value();},
-              "Δm²(ee) = f(Δm²(μμ), ...)")
+              "|Δm²(ee)|=f(|Δm²(μ|μ), θ)")
           // Squared sine and squared cos from cos of double angle
-          .add(&SinSq12, {&CosDouble12}, [&]() { return 0.5*(1.0-CosDouble12.value()); }, "sin²θ(12)=(1-cos2θ(12))/2")
-          .add(&SinSq13, {&CosDouble13}, [&]() { return 0.5*(1.0-CosDouble13.value()); }, "sin²θ(13)=(1-cos2θ(13))/2")
-          .add(&CosSq12, {&CosDouble12}, [&]() { return 0.5*(1.0+CosDouble12.value()); }, "cos²θ(12)=(1+cos2θ(12))/2")
-          .add(&CosSq13, {&CosDouble13}, [&]() { return 0.5*(1.0+CosDouble13.value()); }, "cos²θ(13)=(1+cos2θ(13))/2")
+          .add(&SinSq12, {&CosDouble12}, [&]() { return 0.5*(1.0-CosDouble12.value()); }, "sin²θ₁₂=(1-cos2θ₁₂)/2")
+          .add(&SinSq13, {&CosDouble13}, [&]() { return 0.5*(1.0-CosDouble13.value()); }, "sin²θ₁₃=(1-cos2θ₁₃)/2")
+          .add(&CosSq12, {&CosDouble12}, [&]() { return 0.5*(1.0+CosDouble12.value()); }, "cos²θ₁₂=(1+cos2θ₁₂)/2")
+          .add(&CosSq13, {&CosDouble13}, [&]() { return 0.5*(1.0+CosDouble13.value()); }, "cos²θ₁₃=(1+cos2θ₁₃)/2")
+          // Cos of double angle from squared sine of double angle
+          .add(&CosDouble12, {&SinSqDouble12}, [&]() { return sqrt(1.0-SinSqDouble12.value()); }, "cos2θ₁₂=√(1-sin²2θ₁₂)")
+          .add(&CosDouble13, {&SinSqDouble13}, [&]() { return sqrt(1.0-SinSqDouble13.value()); }, "cos2θ₁₃=√(1-sin²2θ₁₃)")
+          // Squared sine of double angle from squared sine
+          .add(&SinSqDouble12, {&SinSq12}, [&]() { auto v=SinSq12.value(); return 4*v*(1.0-v); }, "sin2θ₁₂=4sin²θ₁₂(1-sin²θ₁₂)")
+          .add(&SinSqDouble13, {&SinSq13}, [&]() { auto v=SinSq13.value(); return 4*v*(1.0-v); }, "sin2θ₁₃=4sin²θ₁₃(1-sin²θ₁₃)")
           // Squared sine from angle
-          .add(&SinSq12, {&Theta12}, [&]() { return pow(sin(Theta12.value()), 2); }, "sin²θ(12)")
-          .add(&SinSq13, {&Theta13}, [&]() { return pow(sin(Theta13.value()), 2); }, "sin²θ(13)")
-          .add(&SinSq23, {&Theta23}, [&]() { return pow(sin(Theta23.value()), 2); }, "sin²θ(23)")
+          .add(&SinSq12, {&Theta12}, [&]() { return pow(sin(Theta12.value()), 2); }, "sin²θ₁₂")
+          .add(&SinSq13, {&Theta13}, [&]() { return pow(sin(Theta13.value()), 2); }, "sin²θ₁₃")
+          .add(&SinSq23, {&Theta23}, [&]() { return pow(sin(Theta23.value()), 2); }, "sin²θ₂₃")
           // Squared sine from squared cos
-          .add(&SinSq12, {&CosSq12}, [&]() { return 1.0-CosSq12.value(); }, "cos²θ(12)=1-sin²θ(12)")
-          .add(&SinSq13, {&CosSq13}, [&]() { return 1.0-CosSq13.value(); }, "cos²θ(13)=1-sin²θ(13)")
-          .add(&SinSq23, {&CosSq23}, [&]() { return 1.0-CosSq23.value(); }, "cos²θ(23)=1-sin²θ(23)")
+          .add(&SinSq12, {&CosSq12}, [&]() { return 1.0-CosSq12.value(); }, "sin²θ₁₂=1-cos²θ₁₂")
+          .add(&SinSq13, {&CosSq13}, [&]() { return 1.0-CosSq13.value(); }, "sin²θ₁₃=1-cos²θ₁₃")
+          .add(&SinSq23, {&CosSq23}, [&]() { return 1.0-CosSq23.value(); }, "sin²θ₂₃=1-cos²θ₂₃")
           // Sine and cos
-          .add(&Sin12, {&SinSq12}, [&]() { return sqrt(SinSq12.value()); }, "sinθ(12)=sqrt sin²θ(12)")
-          .add(&Sin13, {&SinSq13}, [&]() { return sqrt(SinSq13.value()); }, "sinθ(13)=sqrt sin²θ(13)")
-          .add(&Sin23, {&SinSq23}, [&]() { return sqrt(SinSq23.value()); }, "sinθ(23)=sqrt sin²θ(23)")
-          .add(&Cos12, {&CosSq12}, [&]() { return sqrt(CosSq12.value()); }, "cosθ(12)=sqrt cos²θ(12)")
-          .add(&Cos13, {&CosSq13}, [&]() { return sqrt(CosSq13.value()); }, "cosθ(13)=sqrt cos²θ(13)")
-          .add(&Cos23, {&CosSq23}, [&]() { return sqrt(CosSq23.value()); }, "cosθ(23)=sqrt cos²θ(23)")
+          .add(&Sin12, {&SinSq12}, [&]() { return sqrt(SinSq12.value()); }, "sinθ₁₂=√sin²θ₁₂")
+          .add(&Sin13, {&SinSq13}, [&]() { return sqrt(SinSq13.value()); }, "sinθ₁₃=√sin²θ₁₃")
+          .add(&Sin23, {&SinSq23}, [&]() { return sqrt(SinSq23.value()); }, "sinθ₂₃=√sin²θ₂₃")
+          .add(&Cos12, {&CosSq12}, [&]() { return sqrt(CosSq12.value()); }, "cosθ₁₂=√cos²θ₁₂")
+          .add(&Cos13, {&CosSq13}, [&]() { return sqrt(CosSq13.value()); }, "cosθ₁₃=√cos²θ₁₃")
+          .add(&Cos23, {&CosSq23}, [&]() { return sqrt(CosSq23.value()); }, "cosθ₂₃=√cos²θ₂₃")
           // Angles
-          .add(&Theta12, {&Sin12}, [&]() { return asin(Sin12.value()); }, "θ(12)=asin sinθ(12)")
-          .add(&Theta13, {&Sin13}, [&]() { return asin(Sin13.value()); }, "θ(13)=asin sinθ(13)")
-          .add(&Theta23, {&Sin23}, [&]() { return asin(Sin23.value()); }, "θ(23)=asin sinθ(23)")
+          .add(&Theta12, {&Sin12}, [&]() { return asin(Sin12.value()); }, "θ₁₂=asin sinθ₁₂")
+          .add(&Theta13, {&Sin13}, [&]() { return asin(Sin13.value()); }, "θ₁₃=asin sinθ₁₃")
+          .add(&Theta23, {&Sin23}, [&]() { return asin(Sin23.value()); }, "θ₂₃=asin sinθ₂₃")
           // CP
           .add(&Phase, {&Delta}, [&](arrayview<FloatType>& ret) {
               ret.complex() = exp(-std::complex<FloatType>(0, Delta.value()));
-            }, 2, "-exp(iδ)")
+            }, 2, "exp(-iδ)")
           .add(&PhaseC, {&Delta}, [&](arrayview<FloatType>& ret) { //conjugate
               ret.complex() = exp(+std::complex<FloatType>(0, Delta.value()));
-            }, 2, "+exp(iδ)")
+            }, 2, "exp(+iδ)")
           ;
       }
     };
