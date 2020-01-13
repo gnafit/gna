826d63c4bcb72935144ddae43feb9d382b01a0df
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-01-13 23:16:12 +0300

    Update plots

--- macro/plot/plot-sensitivity-nmo-yaml.py
+++ macro/plot/plot-sensitivity-nmo-yaml.py
@@ -41,12 +41,15 @@ class NMOSensPlotter(object):
         ax.minorticks_on()
         ax.grid(axis='x')
 
-        plt.subplots_adjust(left=0.2)
+        plt.subplots_adjust(left=0.3)
+        plt.tick_params(axis='y', which='minor', left=False)
 
         formatter = ax.xaxis.get_major_formatter()
         formatter.set_useOffset(False)
         formatter.set_powerlimits((-2,2))
         formatter.useMathText=True
+
+        plt.subplots_adjust(left=0.2)
 
         return ax
 
@@ -59,29 +62,33 @@ class NMOSensPlotter(object):
 
         chi2 = self.get_chi2()
         shift = np.zeros_like(chi2)
+        shift[0] = chi2[0]
         shift[1:] = chi2[1:] - chi2[:-1]
+        facecolors = [s>0 and 'green' or 'red' for s in shift]
 
         ax=self.figure(r'$\chi^2$')
-        ax.barh(self.info, chi2)
+        ax.barh(self.info, chi2, color=facecolors)
         ax.set_ylim(reversed(ax.get_ylim()))
+        ax.axvline(chi2[0], color='blue', linewidth=1, alpha=0.5)
 
         self.savefig(suffix+('rel', ))
 
         ax=self.figure(r'$\chi^2$')
 
         prev = 0
-        for i, (label, c) in enumerate(zip(self.info, chi2)):
-            ax.broken_barh([(prev, c-prev)], (-i, 0.9))
+        for i, (label, c, color) in enumerate(zip(self.info, chi2, facecolors)):
+            offset = c-prev
+            ax.broken_barh([(prev, offset)], (-i+0.4, -0.8), facecolor=color)
             prev = c
+        ax.axvline(chi2[0], color='blue', linewidth=1, alpha=0.5, linestyle='--')
 
-        ax.set_ylim(-len(self.info), 0)
-        ax.set_yticks(range(-len(self.info), 0))
-        ax.set_yticklabels(reversed(self.info))
+        ax.set_ylim(-len(self.info)+0.5, 0.5)
+        plt.yticks(range(-len(self.info)+1, 1), reversed(self.info))
 
         self.savefig(suffix+('rel1', ))
 
         ax=self.figure(r'$\Delta\chi^2$')
-        ax.barh(self.info, shift)
+        ax.barh(self.info, shift, color=facecolors)
         ax.set_ylim(reversed(ax.get_ylim()))
 
         self.savefig(suffix+('relshift', ))
