a9a5455b06d91a8a63be0b407b8ea6a7f7f4702f
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-01-14 13:17:57 +0300

    Update plots

--- macro/plot/plot-sensitivity-nmo-yaml.py
+++ macro/plot/plot-sensitivity-nmo-yaml.py
@@ -20,7 +20,91 @@ class NMOSensPlotter(object):
         self.arange = np.arange(self.size)
         self.opts = opts
 
-        self.info = [data.label for data in opts.files]
+        self.load_data()
+
+    def load_data(self):
+        # Labels
+        self.info = [data.label for data in self.opts.files]
+
+        # Chi2 values
+        self.chi2_full = np.zeros(self.size+1, dtype='d')
+        for i, data in enumerate(self.opts.files):
+            self.chi2_full[i+1] = data.fun
+
+        self.chi2 = self.chi2_full[1:]
+
+        # Bar Y
+        self.width=0.9
+        self.hwidth=self.width*0.5
+
+        self.yc_full = np.arange(1, -len(self.info), -1)
+        self.ytop_full = self.yc_full+self.hwidth
+        self.ybottom_full = self.yc_full-self.hwidth
+        self.ywidth_full = self.ybottom_full-self.ytop_full
+
+        self.yc = self.yc_full[1:]
+        self.ytop = self.ytop_full[1:]
+        self.ybottom = self.ybottom_full[1:]
+        self.ywidth = self.ywidth_full[1:]
+
+        # Previous idx
+        self.idx_prev = np.arange(0, len(self.chi2))
+        print('idx prev before', self.idx_prev)
+        for cur, prev in self.opts.previous:
+            assert cur!=0
+            self.idx_prev[cur]=prev+1
+        print('idx prev after', self.idx_prev)
+
+        # Previous step data
+        self.chi2_prev = self.chi2_full[self.idx_prev]
+        self.shift = self.chi2 - self.chi2_prev
+        self.facecolors = [s>0 and 'green' or 'red' for s in self.shift]
+
+        self.ytop_prev = self.ytop_full[self.idx_prev]
+
+        print('ytop', self.ytop)
+        print('ytop_prev', self.ytop_prev)
+        print('ybottom', self.ybottom)
+
+    def plot_nmo_sensitivity(self):
+        suffix = ('nmo', 'sens')
+        print('Plot NMO sensitivity')
+
+        ax=self.figure(r'$\Delta\chi^2$')
+        ax.barh(self.yc, self.chi2, self.ywidth, color=self.facecolors)
+        ax.axvline(self.chi2[0], color='blue', linewidth=1, alpha=0.5)
+        self.patch_yticklabels()
+
+        self.savefig(suffix+('rel', ))
+
+        #
+        #
+        #
+        ax=self.figure(r'$\Delta\chi^2$')
+
+        for i in range(len(self.chi2)):
+            chi2_prev = self.chi2_prev[i]
+            shift = self.shift[i]
+            ytop, ybottom, ytop_prev = self.ytop[i], self.ybottom[i], self.ytop_prev[i]
+            ax.broken_barh([(chi2_prev, shift)], (ytop, ybottom-ytop), facecolor=self.facecolors[i], alpha=0.7)
+            ax.vlines(chi2_prev, ytop_prev, ybottom, color='black', linewidth=1.5, linestyle='-')
+        else:
+            ax.vlines(self.chi2[-1], self.ytop[-1], self.ybottom[-1], color='black', linewidth=1.5, linestyle='-')
+
+        self.patch_yticklabels()
+
+        self.savefig(suffix+('rel1', ))
+
+        ax.set_xlim(left=self.chi2.min()-3)
+        self.savefig(suffix+('rel1', ))
+
+        # #
+        # #
+        # #
+        # ax=self.figure(r'$\Delta\chi^2$')
+        # ax.barh(self.info, self.shift, color=self.facecolors)
+        # ax.set_ylim(reversed(ax.get_ylim()))
+        # self.savefig(suffix+('relshift', ))
 
     def plot(self):
         self.plot_nmo_sensitivity()
@@ -28,25 +112,14 @@ class NMOSensPlotter(object):
         if self.opts.show:
             plt.show()
 
-    def get_chi2(self):
-        if self.chi2 is not None:
-            return self.chi2
-
-        self.chi2 = np.zeros(self.size, dtype='d')
-
-        for i, data in enumerate(self.opts.files):
-            self.chi2[i] = data.fun
-
-        return self.chi2
-
     def figure(self, ylabel):
         fig = plt.figure()
         ax = plt.subplot(111, xlabel=ylabel, title='NMO sensitivity')
         ax.minorticks_on()
+        ax.tick_params(axis='y', direction='inout', which='minor', left=False, right=False, length=0.0)
         ax.grid(axis='x')
 
         plt.subplots_adjust(left=0.3)
-        plt.tick_params(axis='y', which='minor', left=False)
 
         formatter = ax.xaxis.get_major_formatter()
         formatter.set_useOffset(False)
@@ -59,7 +132,10 @@ class NMOSensPlotter(object):
 
     def patch_yticklabels(self, ax=None):
         ax = ax or plt.gca()
-        plt.tick_params(axis='y', direction='in', pad=-10)
+        ax.set_ylim(-len(self.info)+0.5, 0.5)
+        ax.tick_params(axis='y', which='major', direction='in', pad=-10)
+        plt.yticks(sorted(range(0, -len(self.info), -1)))
+        labels = ax.set_yticklabels(reversed(self.info))
         ax1 = ax.twinx()
         ax1.set_ylabel(r'$\Delta\chi^2$')
         plt.tick_params(axis='y', direction='in', pad=-7)
@@ -75,76 +151,21 @@ class NMOSensPlotter(object):
         ax1.set_ylim(*ax.get_ylim())
         ax1.set_yticks(ax.get_yticks())
         ax1.set_yticks(ax.get_yticks())
-        labels = ax1.set_yticklabels(['{:.2f}'.format(c) for c in reversed(self.get_chi2())])
+        labels = ax1.set_yticklabels(['{:.2f}'.format(c) for c in reversed(self.chi2)])
 
         for label in labels:
             label.set_bbox(bbox_right)
             label.set_ha('right')
 
+        if self.opts.lines:
+            xlim = ax.get_xlim()
+            linesy = -np.array(self.opts.lines)+0.5
+            ax.hlines(linesy, xlim[0], xlim[1], linestyle='dashed', linewidth=1.0, alpha=0.6)
+            ax.set_xlim(*xlim)
+
     def savefig(self, suffix):
         savefig(self.opts.output, dpi=300, suffix=suffix)
 
-    def plot_nmo_sensitivity(self):
-        suffix = ('nmo', 'sens')
-        print('Plot NMO sensitivity')
-
-        chi2 = self.get_chi2()
-        shift = np.zeros_like(chi2)
-        shift[0] = chi2[0]
-        shift[1:] = chi2[1:] - chi2[:-1]
-        facecolors = [s>0 and 'green' or 'red' for s in shift]
-
-        ax=self.figure(r'$\Delta\chi^2$')
-        ax.barh(self.info, chi2, color=facecolors)
-        ax.set_ylim(reversed(ax.get_ylim()))
-        ax.axvline(chi2[0], color='blue', linewidth=1, alpha=0.5)
-        self.patch_yticklabels()
-
-        self.savefig(suffix+('rel', ))
-
-        #
-        #
-        #
-        ax=self.figure(r'$\Delta\chi^2$')
-
-        prev, y_prev = 0, 0.0
-        markwidth = 0.10
-        for i, (label, c, color) in enumerate(zip(self.info, chi2, facecolors)):
-            offset = c-prev
-            y1, h = -i+0.4, -0.8
-            y2 = y1+h
-            ax.broken_barh([(prev, offset)], (y1, h), facecolor=color, alpha=0.7) #, alpha=0.5)
-            # if offset>0:
-                # ax.broken_barh([(c-markwidth, markwidth)], (-i+0.4, -0.8), facecolor=color)
-            # else:
-                # ax.broken_barh([(c, markwidth)], (-i+0.4, -0.8), facecolor=color)
-                #
-
-            if i:
-                ax.vlines(prev, y_prev, y2, color='black', linewidth=1.5, linestyle='-')
-
-            y_prev = y1
-            prev = c
-        else:
-            ax.vlines(prev, y_prev, y2, color='black', linewidth=1.5, linestyle='-')
-
-        ax.set_ylim(-len(self.info)+0.5, 0.5)
-        plt.yticks(sorted(range(0, -len(self.info), -1)), reversed(self.info))
-        self.patch_yticklabels()
-
-        self.savefig(suffix+('rel1', ))
-
-        ax.set_xlim(left=chi2.min()-3)
-        self.savefig(suffix+('rel1', ))
-
-        #
-        #
-        #
-        ax=self.figure(r'$\Delta\chi^2$')
-        ax.barh(self.info, shift, color=facecolors)
-        ax.set_ylim(reversed(ax.get_ylim()))
-
-        self.savefig(suffix+('relshift', ))
 
 if __name__ == '__main__':
     from argparse import ArgumentParser, Namespace
@@ -157,6 +178,8 @@ if __name__ == '__main__':
     parser.add_argument('files', nargs='+', help='Yaml file to load', type=loader)
     parser.add_argument('-o', '--output', help='output file')
     parser.add_argument('-s', '--show', action='store_true', help='show figures')
+    parser.add_argument('-p', '--previous', type=int, default=[], nargs=2, action='append', help='previous value to calculate diff')
+    parser.add_argument('-l', '--lines', type=int, nargs='+', default=[], help='add separator lines after values')
 
     plotter=NMOSensPlotter(parser.parse_args())
     plotter.plot()
