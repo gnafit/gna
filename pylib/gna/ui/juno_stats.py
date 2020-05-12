ab4f2a2d8a482fd683254be9a2321bebc02198ed
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-05-12 17:19:16 +0300

    Add new UI module juno-stats
    Print JUNO related stats and draw default figures, latex fiendly

---
+++ pylib/gna/ui/juno_stats.py
@@ -0,0 +1,25 @@
+"""Print JUNO related stats and plot default figures, latex friendly"""
+from __future__ import print_function
+from gna.ui import basecmd
+
+class cmd(basecmd):
+    @classmethod
+    def initparser(cls, parser, env):
+        parser.add_argument('exp', help='JUNO exp instance')
+
+    def init(self):
+        try:
+            self.exp = self.env.parts.exp[self.opts.exp]
+        except Exception:
+            print('Unable to retrieve exp '+self.opts.exp)
+
+        self.namespace = self.exp.namespace
+        self.context   = self.exp.context
+        self.outputs   = self.exp.context.outputs
+        self.observation = self.outputs.observation.AD1
+
+        self.init_variables()
+
+    def init_variables(self):
+        pass
+
