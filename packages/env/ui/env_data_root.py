c0b500801b19cd24b30473bfc07cbb5a5e183613
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-06-16 16:45:29 +0300

    env-data-root UI module to save outputs as ROOT objects
    save-root module to dump a namespace to the ROOT file

--- packages/env/ui/env_data_root.py
+++ packages/env/ui/env_data_root.py
@@ -1,4 +1,4 @@
-"""Save outputs as dictionaries with numbers and meta"""
+"""Save the outputs as ROOT objects: TH1D, TH2D, TGraph"""
 
 from __future__ import print_function
 from gna.ui import basecmd, append_typed, qualified
@@ -9,33 +9,58 @@ from mpl_tools.helpers import savefig
 import numpy as np
 from tools.yaml import yaml_load
 from gna.bindings import common
-from gna.env import PartNotFoundError, env
+from gna.env import env
+from mpl_tools import root2numpy
+from load import ROOT as R
 
-def unpack(output):
+def unpack(output, *args, **kwargs):
     dtype = output.datatype()
+    ndim  = len(dtype.shape)
     if dtype.kind==2:
-        return unpack_hist(output, dtype)
+        if ndim==1:
+            return unpack_hist1(output, dtype, *args, **kwargs)
+        # elif ndim==2:
+            # return unpack_hist2(output, dtype, *args, **kwargs)
+        else:
+            raise ValueError('Invalid histogram dimension '+str(ndim))
     elif dtype.kind==1:
-        return unpack_points(output, dtype)
+        # return unpack_points(output, dtype, *args, **kwargs)
+        raise Exception('Saving array is not implemented')
 
     raise ValueError('Uninitialized output')
 
-def unpack_common(output, dtype, **kwargs):
+def unpack_hist1(output, dtype, kwargs={}):
     dtype = dtype or output.datatype()
+    data = output.data()
 
-    ret = dict(
-        data  = output.data().copy(),
-        shape = np.array(dtype.shape),
-        ndim  = len(dtype.shape)
-        )
-    ret.update(kwargs)
-    return ret
+    edges = np.array(dtype.edgesNd[0], dtype='d')
+    widths = edges[1:]-edges[:-1]
+    rel_offsets = np.fabs(widths-widths[0])/widths.max()
 
-def unpack_hist(output, dtype):
-    return unpack_common(output, dtype, type='hist', edges=[np.array(e) for e in dtype.edgesNd])
+    name  = kwargs.pop('name', '')
+    title = kwargs.pop('label', kwargs.pop('title', ''))
 
-def unpack_points(output, dtype):
-    return unpack_common(output, dtype, type='points')
+    if (rel_offsets<1.e-9).all():
+        # Constant width histogram
+        hist = R.TH1D(name, title, edges.size-1, edges[0], edges[-1])
+    else:
+        hist = R.TH1D(name, title, edges.size-1, edges)
+
+    buffer = root2numpy.get_buffer_hist1(hist)
+    buffer[:] = data
+    hist.SetEntries(data.sum())
+
+    xlabel = kwargs.pop('xlabel', None)
+    if xlabel:
+        hist.GetXaxis().SetTitle(xlabel)
+    ylabel = kwargs.pop('ylabel', None)
+    if ylabel:
+        hist.GetYaxis().SetTitle(ylabel)
+
+    if kwargs:
+        raise Exception('Unparsed options in extra arguments for TH1D')
+
+    return hist
 
 class cmd(basecmd):
     def __init__(self, *args, **kwargs):
@@ -60,11 +85,10 @@ class cmd(basecmd):
             except KeyError:
                 print('Unable to find key:', frm)
 
-            data = unpack(obs)
+            kwargs = yaml_load(extra)
+            kwargs.setdefault('name', to.rsplit('.', 1)[-1])
+            data = unpack(obs, kwargs)
             target[to] = data
-
-            upd = yaml_load(extra)
-            data.update(upd)
 
 def list_get(lst, idx, default):
     return lst[idx] if idx<len(lst) else default
