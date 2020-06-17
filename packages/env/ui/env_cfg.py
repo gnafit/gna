dcd04022f3891a4afc0ed6e3455b7a8674a7426e
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-06-17 13:39:42 +0300

    Add verbose env.future option

---
+++ packages/env/ui/env_cfg.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+"""Global environment configuration"""
+
+from __future__ import print_function
+from gna.ui import basecmd
+from pprint import pprint
+from tools.dictwrapper import DictWrapper
+import yaml
+
+class DictWrapperVerbose(DictWrapper):
+    def __setitem__(self, k, v):
+        if k in self:
+            action='Overwrite'
+        else:
+            action='Set'
+        print('{action} {key}: {value}'.format(action=action, key=k, value=v))
+        DictWrapper.__setitem__(self, k, v)
+
+class cmd(basecmd):
+    @classmethod
+    def initparser(cls, parser, env):
+        parser.add_argument('-v', '--verbose', action='count', help='make environment verbose on set')
+
+    def init(self):
+        if self.opts.verbose:
+            self.env.future = DictWrapperVerbose(self.env.future)
