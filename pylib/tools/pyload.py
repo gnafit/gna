a7c5c55a0ec14c5fbb44d488fb1f4da66803c598
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-06-19 13:01:55 +0300

    Add simple python loader and unit test

---
+++ pylib/tools/pyload.py
@@ -0,0 +1,30 @@
+import runpy
+import inspect
+import numpy
+
+def loaddict(filename, init_globals={}, verbose=False):
+    if verbose:
+        print('Loading dictionary from:', filename)
+
+    dic = runpy.run_path(filename, init_globals)
+    newdic = {}
+    notthis={}
+    for k, v in list(dic.items()):
+        if k.startswith('__'):
+            continue
+
+        if v is init_globals.get(k, notthis):
+            continue
+
+        if inspect.ismodule(v):
+            continue
+
+        if inspect.isfunction(v):
+            continue
+
+        if inspect.ismethod(v):
+            continue
+
+        newdic[k] = v
+
+    return newdic
