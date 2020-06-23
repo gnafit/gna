3ee143c3bde14cbeda48f08f72b97df80c97f24c
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-06-23 16:31:55 +0300

    pydoc for loaders

--- pylib/tools/cfg_load.py
+++ pylib/tools/cfg_load.py
@@ -2,6 +2,7 @@ from tools.yaml import yaml_load_file
 from tools.pydict_load import pydict_load
 
 def cfg_load(filename, verbose=False):
+    """Load *.yaml or *.py file as dictionary"""
     if verbose:
         print('Loading dictionary from:', filename)
 
@@ -13,6 +14,7 @@ def cfg_load(filename, verbose=False):
     raise ValueError('Invalid configuration file type: '+filename)
 
 def cfg_parse(filename_or_dict, verbose=False):
+    """Load *.yaml or *.py file as dictionary. If the argument is dictionary - return it."""
     if isinstance(filename_or_dict, str):
 	return cfg_load(filename_or_dict)
 
