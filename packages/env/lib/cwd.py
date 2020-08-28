d385cd80e460112cff862d03d3062d08d21cc049
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-08-28 12:54:56 +0300

    Update env-cwd and add comment UI:
    - env-cwd now may set prefix, which is used to prepend filename (in case there is no / in it)
    - comment UI simply ignores all the arguments. NOTE: first argument should not start with -
    
    Example:
    ./gna \
        -- cmd-save  \
        -- env-cwd output/test_cwd -p cmd_  \
        -- comment a -t a 'a b' 'abc\ne' quotes -v  \
        -- comment test sldfk jsdf -sdf --sdfsdf asdf  \
        -- cmd-save test.sh

---
+++ packages/env/lib/cwd.py
@@ -0,0 +1,73 @@
+"""Current working dir for GNA"""
+
+from os.path import join, exists, isdir
+from os import makedirs, access, W_OK
+from collections import Iterable
+
+_cwd = ''
+_prefix = ''
+
+def get_cwd():
+    """Return CWD"""
+    return _cwd
+
+def set_cwd(cwd):
+    """Set CWD
+    Create if needed
+    """
+    global _cwd
+    _cwd = cwd
+    _make_cwd()
+
+def get_prefix():
+    """Get prefix"""
+    return _prefix
+
+def set_prefix(prefix):
+    """Set prefix"""
+    global _prefix
+    _prefix=prefix
+
+def get_path(path):
+    """Return the path. Prepend with CWD and prefix
+    NOTE: prefix is applied only if path does not contain /
+    """
+    if _prefix and not '/' in path:
+        path = _prefix + path
+
+    if not _cwd:
+        return path
+
+    return join(_cwd, path)
+
+def update_namespace_cwd(ns, keys):
+    """Update Argparser namespace by prepending all the paths with CWD"""
+    if not _cwd and not _prefix:
+        return
+
+    if isinstance(keys, str):
+        keys = keys,
+
+    for key in keys:
+        val=ns.__dict__[key]
+
+        if val is None:
+            continue
+
+        if isinstance(val, str):
+            ns.__dict__[key] = get_path(val)
+        elif isinstance(val, Iterable):
+            ns.__dict__[key] = map(get_path, val)
+        else:
+            raise Exception('Unexpected value {!s} type: {}. Should be string or list of strings.'.format(val, type(val).__name__))
+
+def _make_cwd():
+    if exists(_cwd):
+        if not isdir(_cwd):
+            raise Exception('Unable to use {} as CWD: already exists, not a directory'.format(_cwd))
+    else:
+        print('Create CWD:', _cwd)
+        makedirs(_cwd)
+
+    if not access(_cwd, W_OK):
+        raise Exception('CWD {} is not writable'.format(_cwd))
--- packages/env/ui/env_cwd.py
+++ packages/env/ui/env_cwd.py
@@ -4,13 +4,18 @@
 
 from __future__ import print_function
 from gna.ui import basecmd
-from packages.env.lib.cwd import set_cwd
+from packages.env.lib.cwd import set_cwd, set_prefix
 
 class cmd(basecmd):
     @classmethod
     def initparser(cls, parser, env):
         parser.add_argument('cwd', help='CWD to set')
+        parser.add_argument('-p', '--prefix', help='prefix')
 
     def init(self):
-	set_cwd(self.opts.cwd)
-	print('CWD:', self.opts.cwd)
+	if self.opts.cwd:
+	    set_cwd(self.opts.cwd)
+	    print('CWD:', self.opts.cwd)
+	if self.opts.prefix:
+	    set_prefix(self.opts.prefix)
+	    print('Prefix:', self.opts.prefix)
