ff6fbd0d3066da3f9c4b611b6913ab7e89335128
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-10-15 15:26:33 +0300

    minor

--- tests/nonlinearity/test_nonlinearity_matrix_2.py
+++ tests/nonlinearity/test_nonlinearity_matrix_2.py
@@ -54,10 +54,6 @@ print('Edges original:', orig)
 print('Edges original projected:', orig_proj)
 print('Edges modified projected back:', mod_proj)
 
-# matp_a = rescale_to_matrix_a( edges, edges_m, roundto=3 )
-# matp_b = rescale_to_matrix_b( edges, centers_m, roundto=3 )
-# matp_c = rescale_to_matrix_c( edges, edges_m, centers_m, roundto=3 )
-
 Orig = C.Points(orig)
 Orig_proj = C.Points(orig_proj)
 Mod_proj = C.Points(mod_proj)
@@ -72,78 +68,18 @@ mat_b = nlB.matrix.FakeMatrix.data()
 
 print( 'Mat B and its sum' )
 print( mat_b )
-print( mat_b.sum( axis=0 ) )
+matbsum = mat_b.sum( axis=0 )
+print( matbsum )
+assert np.allclose(matbsum[1:-1], 1, rtol=0, atol=1.e-16)
 
-# print( 'Mat C and its sum' )
-# print( matp_c )
-# print( matp_c.sum( axis=0 ) )
-
-# cmp(mat_a, matp_a)
-
-# ntrue.hist.hist.data()
-# tflag1a = ntrue.hist.tainted()
-# tflag2a = nlA.matrix.tainted()
-# ntrue.hist.taint()
-# tflag1b = ntrue.hist.tainted()
-# tflag2b = nlA.matrix.tainted()
-# print('taints', tflag1a, tflag2a, tflag1b, tflag2b)
-# assert not tflag2b
-
-# def plot_edges_a(edges, centers, edges_m, centers_m):
-    # fig = plt.figure()
-    # ax = plt.subplot(111, xlabel='', ylabel='', title='Edges A')
-    # ax.set_xlim(left=-2.0, right=10.0)
-    # ax.set_ylim(0.0, 3.0)
-
-    # ax.vlines(edges,   0.0, 5.0, linestyle='solid',  color='black', alpha=0.4, linewidth=0.2)
-    # ax.vlines(edges,   0.0, 1.0, linestyle='solid',  color='blue', label='Original')
-    # ax.vlines(centers, 0.0, 1.0, linestyle='dashed', color='blue')
-
-    # ax.vlines(edges_m,   2.0, 3.0, linestyle='solid',  color='red', label='Modified')
-    # ax.vlines(centers_m, 2.0, 3.0, linestyle='dashed', color='red')
-
-    # for a, b in zip(edges, edges_m):
-        # ax.plot( (a, b), (1.0, 2.0), color='gray' )
-
-    # for i, c in enumerate(centers):
-        # ax.text(c, 1.33, str(i), va='center', ha='center')
-
-    # for i, c in enumerate(centers_m):
-        # ax.text(c, 1.66, str(i), va='center', ha='center')
-
-    # ax.legend(loc='upper left')
-
-    # savefig(opts.output, suffix='edges_a')
-
-# def plot_edges_b(edges, centers, edges_m, centers_m):
-    # fig = plt.figure()
-    # ax = plt.subplot(111, xlabel='', ylabel='', title='Edges B')
-    # ax.set_xlim(left=-2.0, right=10.0)
-    # ax.set_ylim(0.0, 3.0)
-
-    # ax.vlines(edges,   0.0, 5.0, linestyle='solid',  color='black', alpha=0.4, linewidth=0.2)
-    # ax.vlines(edges,   0.0, 1.0, linestyle='solid',  color='blue', label='Original')
-    # ax.vlines(centers, 0.0, 1.0, linestyle='dashed', color='blue')
-
-    # hwidths = 0.5*(edges[1:]-edges[:-1])
-    # edges_m1 = centers_m-hwidths
-    # edges_m2 = centers_m+hwidths
-    # ax.vlines(edges_m1,  2.0, 2.5, linestyle='solid',  color='red', label='Left')
-    # ax.vlines(edges_m2,  2.5, 3.0, linestyle='solid',  color='magenta', label='Right')
-    # ax.vlines(centers_m, 2.0, 3.0, linestyle='dashed', color='red')
-
-    # for a, b in zip(edges, edges_m):
-        # ax.plot( (a, b), (1.0, 2.0), color='gray' )
-
-    # for i, c in enumerate(centers):
-        # ax.text(c, 1.33, str(i), va='center', ha='center')
-
-    # for i, c in enumerate(centers_m):
-        # ax.text(c, 1.66, str(i), va='center', ha='center')
-
-    # ax.legend(loc='upper left')
-
-    # savefig(opts.output, suffix='edges_b')
+ntrue.hist.hist.data()
+tflag1a = ntrue.hist.tainted()
+tflag2a = nlB.matrix.tainted()
+ntrue.hist.taint()
+tflag1b = ntrue.hist.tainted()
+tflag2b = nlB.matrix.tainted()
+print('taints', tflag1a, tflag2a, tflag1b, tflag2b)
+assert not tflag2b
 
 def plot_matrix(mat):
     fig = plt.figure()
@@ -165,29 +101,6 @@ def plot_matrix(mat):
     ax.plot(xfine, fcn_direct(xfine), '--', color='white')
     savefig(opts.output, suffix='matrix_1')
 
-# def plot_edges():
-    # fig = plt.figure()
-    # ax = plt.subplot(111, xlabel='', ylabel='', title='')
-    # ax.minorticks_on()
-    # ax.grid()
-
-    # ax.plot(edges[1:],      edges_m[1:], 'o-', label='direct')
-    # ax.plot(edges_back[1:], edges[1:],   'o-', label='inverse')
-    # ax.plot(edges[1:],      edges_back[1:], 'o-', label='inverse')
-
-    # ax.legend()
-
-    # fig = plt.figure()
-    # ax = plt.subplot(111, xlabel='', ylabel='', title='')
-    # ax.minorticks_on()
-    # # ax.grid()
-
-    # ax.vlines(edges[1:],      0.0, 1.0, linestyle='solid', label='Original')
-    # ax.vlines(edges_m[1:],    1.0, 2.0, linestyle='solid', label='Modified')
-    # ax.vlines(edges_back[1:], 2.0, 3.0, linestyle='solid', label='Modified')
-
-    # ax.legend()
-
 def plot_curve():
     fig = plt.figure()
     ax = plt.subplot(111, xlabel='X original', ylabel='X modified', title='')
@@ -203,13 +116,7 @@ def plot_curve():
     ax.legend()
 
 plot_curve()
-
-# plot_edges_a(edges, centers, edges_m, centers_m)
-# plot_edges_b(edges, centers, edges_m, centers_m)
-
 plot_matrix(mat_b)
-
-# plot_edges()
 
 if opts.show:
     plt.show()
