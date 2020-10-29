4c2da76a723af1dcf4e833b8a9251e8b69f2236b
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-10-29 11:54:44 +0300

    Add independent extrapolation methods

--- transformations/interpolation/InterpLinear.cc
+++ transformations/interpolation/InterpLinear.cc
@@ -4,6 +4,14 @@ double InterpLinear::interpolation_formula(double x, double y, double k, double 
     return k * (point - x) + y;
 }
 
+double InterpLinear::interpolation_formula_below(double x, double y, double k, double point) const noexcept {
+    return k * (point - x) + y;
+}
+
+double InterpLinear::interpolation_formula_above(double x, double y, double k, double point) const noexcept {
+    return k * (point - x) + y;
+}
+
 Eigen::ArrayXd InterpLinear::compute_weights(const Eigen::ArrayXd& xs, const Eigen::ArrayXd& ys, const Eigen::ArrayXd& widths, size_t nseg) const noexcept {
     return (ys.tail(nseg)-ys.head(nseg))/widths;          /// k coefficient
 }
