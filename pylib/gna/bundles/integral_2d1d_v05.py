9bf7a2497e475a900e1cce403f240667819226d6
theinz <tobias.heinz@uni-tuebingen.de>
2020-11-06 10:30:48 +0100

    Add mode option for rectangular inetgration

--- pylib/gna/bundles/integral_2d1d_v05.py
+++ pylib/gna/bundles/integral_2d1d_v05.py
@@ -1,6 +1,5 @@
 # Reimplementation of integral_2d1d_v01 for up to date bundles
 # Reimplementation of integral_2d1d_v02 with Integrator21GL instead of legacy GaussLegendre
-# Reimplementation of integral_2d1d_v03 with option for Integrator2Rect
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
@@ -8,40 +7,8 @@ from load import ROOT as R
 import numpy as N
 import gna.constructors as C
 from gna.bundle import TransformationBundle
-from gna.configurator import NestedDict
 
-class integral_2d1d_v05(TransformationBundle):
-    """2d integral based on Integrator21GL v04 and Integrator2Rect
-
-    Changes since integal_2d1d_v04:
-        -Added 'mode' option for rectangular integrator
-
-    Changes since integral_2d1d_v03:
-        - Added 'instances' option
-
-    Configuration options:
-        - instances   - dictionary of (name, label) pairs for instances
-                        { 'name1': 'label1', ... }
-                        alternativetly, a a dictionary with extra options may be passed instead of label:
-                        { 'name1': {'label': 'label1', 'noindex': True}, ... }
-                        Extra options:
-                            * 'noindex' - disable indices
-        - edges     - array with output histogram edges
-        - xorders   - number of points in each bin on axis X, integer or array or integers.
-        - yorder    - number of points in bin on axis Y, integer.
-        - variables - variable names (array of 2 strings).
-        - mode      - integrator mode (GL, rect, rect_left, rect_right)
-
-    Predefined names:
-        - variables[0] - array with points to integrate over variable[0]
-        - variables[1] - array with points to integrate over variable[1]
-        - variables[0]+'_edges' - array with bin edges for variable[0]
-        - variables[0]+'_centers' - array with bin centers for variable[0]
-        - variables[0]+'_hist' - histogram with bin edges for variable[0]
-        - variables[i]+'_mesh' - 2d mesh for variable[i] (as in numpy.meshgrid)
-
-        (may be configured via 'names' option of a bundle)
-    """
+class integral_2d1d_v03(TransformationBundle):
     def __init__(self, *args, **kwargs):
         TransformationBundle.__init__( self, *args, **kwargs )
         self.check_cfg()
@@ -49,29 +16,27 @@ class integral_2d1d_v05(TransformationBundle):
     @staticmethod
     def _provides(cfg):
         var0, var1 = cfg.variables
-        variables = (var0, var1, var0+'_centers', var0+'_edges', var0+'_hist', var0+'_mesh', var1+'_mesh')
-        names = tuple(cfg['instances'].keys())
-        return (), names+variables
+        return (), ('integral', var0, var1, var0+'_centers', var0+'_edges', var0+'_hist', var0+'_mesh', var1+'_mesh')
 
     def check_cfg(self):
         if not 'name' in self.cfg:
             pkey = self.cfg.parent_key()
             if not pkey:
-                raise self.exception('"name" option is not provided')
+                raise Exception('"name" option is not provided for integral_1d_v01')
             self.cfg.name = pkey
 
         try:
             self.edges = N.ascontiguousarray(self.cfg.edges, dtype='d')
         except:
-            raise self.exception('Invalid binning definition: {!r}'.format(self.cfg.edges))
+            raise Exception('Invalid binning definition: {!r}'.format(self.cfg.edges))
 
         try:
             self.xorders = N.ascontiguousarray(self.cfg.xorders, dtype='P')
         except:
-            raise self.exception('Invalid xorders definition: {!r}'.format(self.cfg.xorders))
+            raise Exception('Invalid xorders definition: {!r}'.format(self.cfg.xorders))
 
         if len(self.cfg.variables)!=2:
-            raise self.exception('Two vairables should be provided')
+            raise Exception('Two vairables should be provided')
 
         if not self.cfg.mode:
             self.mode = 'GL'
@@ -116,33 +81,13 @@ class integral_2d1d_v05(TransformationBundle):
         self.set_output(self.cfg.variables[1],            None, self.integrator.points.y)
 
         hist = self.integrator.hist
+        for i, it in enumerate(self.nidx):
+            if i:
+                hist = self.integrator.add_transformation()
+            hist.setLabel(it.current_format('{name} {autoindex} (GL)', name='Integral'))
 
-        instances = self.cfg['instances']
-        needadd = False
-        for name, label in instances.items():
-            noindex = False
-            if isinstance(label, (dict, NestedDict)):
-                noindex = label.get('noindex')
-                label = label.get('label')
-
-            if label is None:
-                label = '{name} {autoindex} (GL)'
-
-            for it in self.nidx:
-                if needadd:
-                    hist = self.integrator.add_transformation()
-                needadd=True
-
-                if noindex:
-                    hist.setLabel(label)
-                    self.set_input(name, None, self.integrator.add_input(), argument_number=0)
-                    self.set_output(name, None, hist.outputs.back())
-                    break
-
-                hist.setLabel(it.current_format(label, name='Integral'))
-
-                self.set_input(name, it, self.integrator.add_input(), argument_number=0)
-                self.set_output(name, it, hist.outputs.back())
+            self.set_input('integral', it, self.integrator.add_input(), argument_number=0)
+            self.set_output('integral', it, hist.outputs.back())
 
     def define_variables(self):
         pass
