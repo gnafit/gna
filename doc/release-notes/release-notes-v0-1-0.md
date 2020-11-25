f36695ff83f5accb3dd7f069546d313bd8cc2588
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-11-25 18:09:46 +0300

    Update release notes

--- doc/release-notes/release-notes-v0-1-0.md
+++ doc/release-notes/release-notes-v0-1-0.md
@@ -659,6 +659,50 @@ Initialize a dataset 'nuisance' with a constrained parameters of 'peak_f':
 
 #### Module analysis-v1
 
+Creates a named analysis, i.e. a triplet of theory, data and covariance matrix. The covariance matrix
+may be diagonal and contain only statistical uncertainties or contain a systematic part as well.
+
+The `analysis-v1` required a name and a few of datasets after `-d` option.
+
+Initialize an analysis 'analysis' with a dataset 'peak':
+```sh
+./gna \
+    -- gaussianpeak --name peak_MC --nbins 50 \
+    -- gaussianpeak --name peak_f  --nbins 50 \
+    -- ns --name peak_MC --print \
+          --set E0             values=2    fixed \
+          --set Width          values=0.5  fixed \
+          --set Mu             values=2000 fixed \
+          --set BackgroundRate values=1000 fixed \
+    -- ns --name peak_f --print \
+          --set E0             values=2.5  relsigma=0.2 \
+          --set Width          values=0.3  relsigma=0.2 \
+          --set Mu             values=1500 relsigma=0.25 \
+          --set BackgroundRate values=1100 relsigma=0.25 \
+    -- dataset-v1  --name peak --theory-data peak_f.spectrum peak_MC.spectrum -v \
+    -- analysis-v1 --name analysis --datasets peak -v
+```
+
+Initialize an analysis 'analysis' with a dataset 'peak' and covariance matrix based on constrained parameters:
+```sh
+./gna \
+    -- gaussianpeak --name peak_MC --nbins 50 \
+    -- gaussianpeak --name peak_f  --nbins 50 \
+    -- ns --name peak_MC --print \
+          --set E0             values=2    fixed \
+          --set Width          values=0.5  fixed \
+          --set Mu             values=2000 fixed \
+          --set BackgroundRate values=1000 fixed \
+    -- ns --name peak_f --print \
+          --set E0             values=2.5  relsigma=0.2 \
+          --set Width          values=0.3  relsigma=0.2 \
+          --set Mu             values=1500 relsigma=0.25 \
+          --set BackgroundRate values=1100 relsigma=0.25 \
+    -- dataset-v1 peak --theory-data peak_f.spectrum peak_MC.spectrum -v \
+    -- pargroup covpars peak_f -m constrained \
+    -- analysis-v1  analysis --datasets peak -p covpars -v
+```
+
 ### Package minimize
 
 The package `minimize` contains modifications of the already existing `minimizer` and `fit` UI
--- packages/dataset/ui/analysis_v1.py
+++ packages/dataset/ui/analysis_v1.py
@@ -124,21 +124,21 @@ class cmd(basecmd):
 
                 \033[32mInitialize an analysis 'analysis' with a dataset 'peak' and covariance matrix based on constrained parameters:
                 ```sh
-                ./gna \\ 
-                    -- gaussianpeak --name peak_MC --nbins 50 \\ 
-                    -- gaussianpeak --name peak_f  --nbins 50 \\ 
-                    -- ns --name peak_MC --print \\ 
-                          --set E0             values=2    fixed \\ 
-                          --set Width          values=0.5  fixed \\ 
-                          --set Mu             values=2000 fixed \\ 
-                          --set BackgroundRate values=1000 fixed \\ 
-                    -- ns --name peak_f --print \\ 
-                          --set E0             values=2.5  relsigma=0.2 \\ 
-                          --set Width          values=0.3  relsigma=0.2 \\ 
-                          --set Mu             values=1500 relsigma=0.25 \\ 
-                          --set BackgroundRate values=1100 relsigma=0.25 \\ 
-                    -- dataset-v1 peak --theory-data peak_f.spectrum peak_MC.spectrum -v \\ 
-                    -- pargroup covpars peak_f -m constrained \\ 
+                ./gna \\
+                    -- gaussianpeak --name peak_MC --nbins 50 \\
+                    -- gaussianpeak --name peak_f  --nbins 50 \\
+                    -- ns --name peak_MC --print \\
+                          --set E0             values=2    fixed \\
+                          --set Width          values=0.5  fixed \\
+                          --set Mu             values=2000 fixed \\
+                          --set BackgroundRate values=1000 fixed \\
+                    -- ns --name peak_f --print \\
+                          --set E0             values=2.5  relsigma=0.2 \\
+                          --set Width          values=0.3  relsigma=0.2 \\
+                          --set Mu             values=1500 relsigma=0.25 \\
+                          --set BackgroundRate values=1100 relsigma=0.25 \\
+                    -- dataset-v1 peak --theory-data peak_f.spectrum peak_MC.spectrum -v \\
+                    -- pargroup covpars peak_f -m constrained \\
                     -- analysis-v1  analysis --datasets peak -p covpars -v
                 ```
                 """
